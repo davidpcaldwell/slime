27bdd262b7c20e39ef7695d276238b9e72618b3f
David P. Caldwell <david@davidpcaldwell.com>
2010-10-20 00:21:38 -0400

    improved error handling when subprocess fails: if IOException occurs, execute callback, otherwise throw RuntimeException

--- rhino/system/java/inonit/system/Command.java
+++ rhino/system/java/inonit/system/Command.java
@@ -175,10 +175,9 @@ public class Command {
 		try {
 			p = Runtime.getRuntime().exec( (String[])args.toArray(new String[0]), env, parameters.getWorkingDirectory() );
 		} catch (IOException e) {
-			e.printStackTrace();
 			listener.threw(e);
 		} catch (Throwable t) {
-			t.printStackTrace();
+			throw new RuntimeException(t);
 		}
 		if (p != null) {
 			InputStream out = p.getInputStream();
