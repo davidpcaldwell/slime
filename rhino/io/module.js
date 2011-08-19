3a432bda2eb1e224ecc1c1109c64eacb9e620948
David P. Caldwell <david@davidpcaldwell.com>
2011-08-19 18:14:03 -0400

    fix issue with split method of stdout / stderr

--- rhino/io/module.js
+++ rhino/io/module.js
@@ -245,6 +245,10 @@ var Streams = new function() {
 			return new Packages.java.io.OutputStreamWriter($context.stdio.$err);
 		}
 
+		this.split = function(other) {
+			return new OutputStream($context.stdio.$err).split(other);
+		}
+
 		this.close = function() {}
 	}
 
@@ -255,6 +259,10 @@ var Streams = new function() {
 
 		this.$getWriter = function() {
 			return new Packages.java.io.OutputStreamWriter($context.stdio.$out);
+		}
+		
+		this.split = function(other) {
+			return new OutputStream($context.stdio.$out).split(other);
 		}
 
 		this.close = function() {}
