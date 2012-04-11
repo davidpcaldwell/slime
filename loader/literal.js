49b0892f61b5242c91b32d35f5ad62e1cf090b38
David P. Caldwell <david@davidpcaldwell.com>
2012-04-11 13:12:14 +0100

    improved literal syntax

--- loader/literal.js
+++ loader/literal.js
@@ -13,333 +13,336 @@
 //	Contributor(s):
 //	END LICENSE
 
-new function() {
-	var $platform = (function() {
-		var $exports = {};
-		$exports.Object = {};
-		if (Object.defineProperty) {
-			$exports.Object.defineProperty = { ecma: true };
-		}
-		if (Object.prototype.__defineGetter__) {
-			if (!$exports.Object.defineProperty) $exports.Object.defineProperty = {};
-			$exports.Object.defineProperty.accessor = true;
-		}
+(function() {
+	return new function() {
+		var $platform = (function() {
+			var $exports = {};
+			$exports.Object = {};
+			if (Object.defineProperty) {
+				$exports.Object.defineProperty = { ecma: true };
+			}
+			if (Object.prototype.__defineGetter__) {
+				if (!$exports.Object.defineProperty) $exports.Object.defineProperty = {};
+				$exports.Object.defineProperty.accessor = true;
+			}
 
-		(function() {
-			var getJavaClass = function(name) {
-				try {
-					if (typeof(Packages) == "undefined") return null;
-					var rv = Packages[name];
-					if (typeof(rv) == "function") {
-						//	In the Firefox Java plugin, JavaPackage objects have typeof() == "function". They also have the
-						//	following format for their String values
-						try {
-							var prefix = "[Java Package";
-							if (String(rv).substring(0, prefix.length) == prefix) {
-								return null;
+			(function() {
+				var getJavaClass = function(name) {
+					try {
+						if (typeof(Packages) == "undefined") return null;
+						var rv = Packages[name];
+						if (typeof(rv) == "function") {
+							//	In the Firefox Java plugin, JavaPackage objects have typeof() == "function". They also have the
+							//	following format for their String values
+							try {
+								var prefix = "[Java Package";
+								if (String(rv).substring(0, prefix.length) == prefix) {
+									return null;
+								}
+							} catch (e) {
+								//	The string value of Packages.java.lang.Object and Packages.java.lang.Number throws a string (the
+								//	below) if you attempt to evaluate it.
+								if (e == "java.lang.NullPointerException") {
+									return rv;
+								}
 							}
-						} catch (e) {
-							//	The string value of Packages.java.lang.Object and Packages.java.lang.Number throws a string (the
-							//	below) if you attempt to evaluate it.
-							if (e == "java.lang.NullPointerException") {
-								return rv;
-							}
+							return rv;
 						}
-						return rv;
+						return null;
+					} catch (e) {
+						return null;
 					}
-					return null;
-				} catch (e) {
-					return null;
 				}
-			}
 
-			if (getJavaClass("java.lang.Object")) {
-				this.java = new function() {
-					this.getClass = function(name) {
-						return getJavaClass(name);
+				if (getJavaClass("java.lang.Object")) {
+					this.java = new function() {
+						this.getClass = function(name) {
+							return getJavaClass(name);
+						}
+					};
+				}
+
+				try {
+					if (typeof($engine) != "undefined") {
+						this.Object.defineProperty.setReadOnly = $engine.Object.defineProperty.setReadOnly;
 					}
-				};
-			}
+				} catch (e) {
+				}
+			}).call($exports);
 
 			try {
 				if (typeof($engine) != "undefined") {
-					this.Object.defineProperty.setReadOnly = $engine.Object.defineProperty.setReadOnly;
+					if ($engine.MetaObject) {
+						$exports.MetaObject = $engine.MetaObject;
+					}
 				}
 			} catch (e) {
 			}
-		}).call($exports);
 
-		try {
-			if (typeof($engine) != "undefined") {
-				if ($engine.MetaObject) {
-					$exports.MetaObject = $engine.MetaObject;
-				}
-			}
-		} catch (e) {
-		}
+			return $exports;
+		})();
 
-		return $exports;
-	})();
+		var $api = (function() {
+			var $exports = {};
 
-	var $api = (function() {
-		var $exports = {};
+			var flag = function() {
+				var rv = function(object,property) {
+					var reason = arguments.callee;
 
-		var flag = function() {
-			var rv = function(object,property) {
-				var reason = arguments.callee;
-
-				if ( (typeof(object) == "object" || typeof(object) == "function") && typeof(property) == "string" ) {
-				} else if (typeof(object) == "function" && typeof(property) == "undefined") {
-				} else {
-					if (reason.warning) {
-						reason.warning({ flag: arguments });
+					if ( (typeof(object) == "object" || typeof(object) == "function") && typeof(property) == "string" ) {
+					} else if (typeof(object) == "function" && typeof(property) == "undefined") {
 					} else {
-						debugger;
-					}
-					if (arguments.length == 1) {
-						//	assume first argument is supposed to be function
-						return function() {
-							throw new TypeError("Attempt to invoke deprecated non-existent function.");
+						if (reason.warning) {
+							reason.warning({ flag: arguments });
+						} else {
+							debugger;
 						}
-					} else {
-						return function(){}();
-					}
-				}
-
-				var warning = function(o) {
-					if (reason.warning) {
-						reason.warning(o);
-					}
-				}
-
-				//	TODO	This is experimental: document it if it stays
-				if (typeof(object) == "function" && arguments.length == 1) {
-					var deprecateFunction = function(f) {
-						return function() {
-							warning({ "function": f, call: arguments, reason: reason });
-							return f.apply(this,arguments);
+						if (arguments.length == 1) {
+							//	assume first argument is supposed to be function
+							return function() {
+								throw new TypeError("Attempt to invoke deprecated non-existent function.");
+							}
+						} else {
+							return function(){}();
 						}
 					}
-					return deprecateFunction(arguments[0]);
-				}
 
-				//	TODO	Even in environments without accessors, we could deprecate all the functions on an object using this
-				//			technique
-				if (typeof(object[property]) == "function") {
-					var deprecateMethod = function(f) {
-						var rv = function() {
-							warning({ target: object, name: property, call: arguments, reason: reason });
-							//	TODO	Add regression to cover previous mistake of not returning this value (but invoking and then
-							//			returning undefined)
-							return f.apply(this,arguments);
-						};
-						for (var x in f) {
-							rv[x] = f[x];
-						}
-						return rv;
-					}
-					object[property] = deprecateMethod(object[property]);
-					return function(){}();
-				}
-
-				//	We only execute the rest of this method if the accessor form of defineProperty is present
-				//	TODO	Make compatible with ECMA method of get/set
-				if (!$platform.Object.defineProperty || !$platform.Object.defineProperty.accessor) return function(){}();
-
-				//	If object has neither getter nor setter, we create both with versions that cooperate with one another
-				//	If object has custom getter and/or custom setter, we overwrite both with versions that wrap them with warnings
-				if (!object.__lookupGetter__(property) && !object.__lookupSetter__(property)) {
-					var values = new function() {
-						var value = object[property];
-
-						this.set = function(v) {
-							warning({ target: object, name: property, set: v, reason: reason });
-							value = v;
-						}
-
-						this.get = function() {
-							warning({ target: object, name: property, get: value, reason: reason });
-							return value;
+					var warning = function(o) {
+						if (reason.warning) {
+							reason.warning(o);
 						}
 					}
-					object.__defineGetter__(property,values.get);
-					object.__defineSetter__(property,values.set);
-				} else {
-					var wrapSetter = function(f) {
-						return function(value) {
-							warning({ target: object, name: property, set: value, reason: reason });
-							if (f) {
-								f.apply(this, [ value ]);
+
+					//	TODO	This is experimental: document it if it stays
+					if (typeof(object) == "function" && arguments.length == 1) {
+						var deprecateFunction = function(f) {
+							return function() {
+								warning({ "function": f, call: arguments, reason: reason });
+								return f.apply(this,arguments);
 							}
 						}
+						return deprecateFunction(arguments[0]);
 					}
 
-					var wrapGetter = function(f) {
-						return function() {
-							var rv;
-							if (f) {
-								rv = f.apply(this, []);
+					//	TODO	Even in environments without accessors, we could deprecate all the functions on an object using this
+					//			technique
+					if (typeof(object[property]) == "function") {
+						var deprecateMethod = function(f) {
+							var rv = function() {
+								warning({ target: object, name: property, call: arguments, reason: reason });
+								//	TODO	Add regression to cover previous mistake of not returning this value (but invoking and then
+								//			returning undefined)
+								return f.apply(this,arguments);
+							};
+							for (var x in f) {
+								rv[x] = f[x];
 							}
-							warning({ target: object, name: property, get: rv, reason: reason });
 							return rv;
 						}
+						object[property] = deprecateMethod(object[property]);
+						return function(){}();
 					}
 
-					object.__defineGetter__(property,wrapGetter(object.__lookupGetter__(property)));
-					object.__defineSetter__(property,wrapSetter(object.__lookupSetter__(property)));
-				}
-				return function(){}();
-			}
-			return rv;
-		}
+					//	We only execute the rest of this method if the accessor form of defineProperty is present
+					//	TODO	Make compatible with ECMA method of get/set
+					if (!$platform.Object.defineProperty || !$platform.Object.defineProperty.accessor) return function(){}();
 
-		var deprecate = flag();
-		var experimental = flag();
+					//	If object has neither getter nor setter, we create both with versions that cooperate with one another
+					//	If object has custom getter and/or custom setter, we overwrite both with versions that wrap them with warnings
+					if (!object.__lookupGetter__(property) && !object.__lookupSetter__(property)) {
+						var values = new function() {
+							var value = object[property];
 
-		$exports.deprecate = deprecate;
-		$exports.experimental = experimental;
-
-		return $exports;
-	})();
-
-	var runInScope = function(code,scope,target) {
-		var run = function(/*code,scope,this*/) {
-			//	TODO	check to understand exactly what leaks into namespace. Does 'runners' for example? 'runScope'? ModuleLoader?
-			//	TODO	putting $exports: true as a property of 'this' is designed to allow older modules to know they are being
-			//			loaded by the new loader, and should go away when all modules are converted
-			return (function() {
-				//	$platform is in scope because of the above
-				//	$api is also in scope
-				with( arguments[1] ) {
-					eval(arguments[0]);
-				}
-			}).apply(
-				(arguments[2]) ? arguments[2] : {},
-				arguments
-			);
-		};
-
-		var runScope = function(initial) {
-			var rv = {};
-			rv.$platform = $platform;
-			rv.$api = $api;
-			for (var x in initial) {
-				rv[x] = initial[x];
-			}
-			return rv;
-		}
-
-		var fixed = runScope(scope);
-
-		if (typeof(code) == "function") {
-			//	it is a function that can execute the code given a scope and target object
-			code(fixed,target);
-		} else if (typeof(code) == "string") {
-			run(code,fixed,target);
-		} else {
-			throw "Unimplemented: typeof(code) = " + typeof(code);
-		}
-	}
-
-	var file = function(code,$context) {
-		var scope = {
-			$exports: {}
-		};
-		scope.$context = ($context) ? $context : {};
-		runInScope(code,scope,{});
-		return scope.$exports;
-	}
-
-	var ModuleLoader = function(format) {
-		//	format.getCode: function(path), returns string containing the code contained at that path
-		//	format.main: string, path to module file
-
-		var Callee = arguments.callee;
-
-		this.load = function(p) {
-			var scope = {
-				$context: (p && p.$context) ? p.$context : {},
-				$exports: (p && p.$exports) ? p.$exports : {},
-				$loader: new function() {
-					this.run = function(path,scope,target) {
-						runInScope(format.getCode(path),scope,target);
-					}
-
-					this.file = function(path,context) {
-						return file(format.getCode(path),context);
-					}
-
-					this.script = $api.deprecate(this.file);
-
-					this.module = function(path,context) {
-						var tokens = path.split("/");
-						var prefix = (tokens.length > 1) ? tokens.slice(0,tokens.length-1).join("/") + "/" : "";
-						var main = tokens[tokens.length-1];
-						var loader = new Callee({
-							main: main,
-							getCode: function(path) {
-								return format.getCode(prefix+path);
+							this.set = function(v) {
+								warning({ target: object, name: property, set: v, reason: reason });
+								value = v;
 							}
-						});
-						return loader.load({ $context: context });
-					}
 
-					//	TODO	should this return a value and allow wholesale replacement?
-					if (format.decorateLoader) {
-						format.decorateLoader(this);
+							this.get = function() {
+								warning({ target: object, name: property, get: value, reason: reason });
+								return value;
+							}
+						}
+						object.__defineGetter__(property,values.get);
+						object.__defineSetter__(property,values.set);
+					} else {
+						var wrapSetter = function(f) {
+							return function(value) {
+								warning({ target: object, name: property, set: value, reason: reason });
+								if (f) {
+									f.apply(this, [ value ]);
+								}
+							}
+						}
+
+						var wrapGetter = function(f) {
+							return function() {
+								var rv;
+								if (f) {
+									rv = f.apply(this, []);
+								}
+								warning({ target: object, name: property, get: rv, reason: reason });
+								return rv;
+							}
+						}
+
+						object.__defineGetter__(property,wrapGetter(object.__lookupGetter__(property)));
+						object.__defineSetter__(property,wrapSetter(object.__lookupSetter__(property)));
 					}
-				}()
+					return function(){}();
+				}
+				return rv;
+			}
+
+			var deprecate = flag();
+			var experimental = flag();
+
+			$exports.deprecate = deprecate;
+			$exports.experimental = experimental;
+
+			return $exports;
+		})();
+
+		var runInScope = function(code,scope,target) {
+			var run = function(/*code,scope,this*/) {
+				//	TODO	check to understand exactly what leaks into namespace. Does 'runners' for example? 'runScope'? ModuleLoader?
+				//	TODO	putting $exports: true as a property of 'this' is designed to allow older modules to know they are being
+				//			loaded by the new loader, and should go away when all modules are converted
+				return (function() {
+					//	$platform is in scope because of the above
+					//	$api is also in scope
+					with( arguments[1] ) {
+						eval(arguments[0]);
+					}
+				}).apply(
+					(arguments[2]) ? arguments[2] : {},
+					arguments
+				);
 			};
 
-			runInScope(format.getCode(format.main),scope);
-			return scope.$exports;
-		}
-	}
-
-	this.run = function(code,scope,target) {
-		runInScope(code,scope,target);
-	};
-
-	//	TODO	For file and module, what should we do about 'this' and why?
-
-	this.file = function(code,$context) {
-		return file(code,$context);
-	};
-
-	this.module = function(format,scope) {
-		var loader = new ModuleLoader(format);
-		return loader.load(scope);
-	};
-
-	this.namespace = function(string) {
-		//	This construct returns the top-level global object, e.g., window in the browser
-		var global = function() {
-			return this;
-		}();
-
-		var scope = global;
-		if (string) {
-			var tokens = string.split(".");
-			for (var i=0; i<tokens.length; i++) {
-				if (typeof(scope[tokens[i]]) == "undefined") {
-					scope[tokens[i]] = {};
+			var runScope = function(initial) {
+				var rv = {};
+				rv.$platform = $platform;
+				rv.$api = $api;
+				for (var x in initial) {
+					rv[x] = initial[x];
 				}
-				scope = scope[tokens[i]];
+				return rv;
+			}
+
+			var fixed = runScope(scope);
+
+			if (typeof(code) == "function") {
+				//	it is a function that can execute the code given a scope and target object
+				code(fixed,target);
+			} else if (typeof(code) == "string") {
+				run(code,fixed,target);
+			} else {
+				throw "Unimplemented: typeof(code) = " + typeof(code);
 			}
 		}
-		return scope;
-	}
 
-	if ($platform.java) {
-		this.java = $platform.java;
-	}
+		var file = function(code,$context) {
+			var scope = {
+				$exports: {}
+			};
+			scope.$context = ($context) ? $context : {};
+			runInScope(code,scope,{});
+			return scope.$exports;
+		}
 
-	//	TODO	The following properties must be exposed to the Rhino loader so that it can supply them to jsh/unit jsapi via jsh.js
+		var ModuleLoader = function(format) {
+			//	format.getCode: function(path), returns string containing the code contained at that path
+			//	format.main: string, path to module file
 
-	//	TODO	also used by client.html unit tests
-	this.$platform = $platform;
+			var Callee = arguments.callee;
 
-	//	TODO	also used by client.html unit tests
-	//	used to allow setting warnings for deprecate and experimental
-	this.$api = $api;
-}
+			this.load = function(p) {
+				var scope = {
+					$context: (p && p.$context) ? p.$context : {},
+					$exports: (p && p.$exports) ? p.$exports : {},
+					$loader: new function() {
+						this.run = function(path,scope,target) {
+							runInScope(format.getCode(path),scope,target);
+						}
+
+						this.file = function(path,context) {
+							return file(format.getCode(path),context);
+						}
+
+						this.script = $api.deprecate(this.file);
+
+						this.module = function(path,context) {
+							var tokens = path.split("/");
+							var prefix = (tokens.length > 1) ? tokens.slice(0,tokens.length-1).join("/") + "/" : "";
+							var main = tokens[tokens.length-1];
+							var loader = new Callee({
+								main: main,
+								getCode: function(path) {
+									return format.getCode(prefix+path);
+								}
+							});
+							return loader.load({ $context: context });
+						}
+
+						//	TODO	should this return a value and allow wholesale replacement?
+						if (format.decorateLoader) {
+							format.decorateLoader(this);
+						}
+					}()
+				};
+
+				runInScope(format.getCode(format.main),scope);
+				return scope.$exports;
+			}
+		}
+
+		this.run = function(code,scope,target) {
+			runInScope(code,scope,target);
+		};
+
+		//	TODO	For file and module, what should we do about 'this' and why?
+
+		this.file = function(code,$context) {
+			return file(code,$context);
+		};
+
+		this.module = function(format,scope) {
+			var loader = new ModuleLoader(format);
+			return loader.load(scope);
+		};
+
+		this.namespace = function(string) {
+			//	This construct returns the top-level global object, e.g., window in the browser
+			var global = function() {
+				return this;
+			}();
+
+			var scope = global;
+			if (string) {
+				var tokens = string.split(".");
+				for (var i=0; i<tokens.length; i++) {
+					if (typeof(scope[tokens[i]]) == "undefined") {
+						scope[tokens[i]] = {};
+					}
+					scope = scope[tokens[i]];
+				}
+			}
+			return scope;
+		}
+
+		if ($platform.java) {
+			this.java = $platform.java;
+		}
+
+		//	TODO	The following properties must be exposed to the Rhino loader so that it can supply them to jsh/unit jsapi via
+		//			jsh.js
+
+		//	TODO	also used by client.html unit tests
+		this.$platform = $platform;
+
+		//	TODO	also used by client.html unit tests
+		//	used to allow implementations to set warnings for deprecate and experimental
+		this.$api = $api;
+	};
+})()
--- loader/rhino/literal.js
+++ loader/rhino/literal.js
@@ -13,91 +13,91 @@
 //	Contributor(s):
 //	END LICENSE
 
-new function() {
-	var loader = (function() {
-		var $engine = {
-			Object: {
-				defineProperty: {}
-			}
-		};
-		(function() {
-			var $objects = new Packages.inonit.script.rhino.Objects();
-			$engine.Object.defineProperty.setReadOnly = function(object,name,value) {
-				$objects.setReadOnly(object,name,value);
-			}
-
-			$engine.MetaObject = function(p) {
-				var delegate = (p.delegate) ? p.delegate : {};
-				var get = (p.get) ? p.get : function(){};
-				var set = (p.set) ? p.set : function(){};
-				return Packages.inonit.script.rhino.MetaObject.create(delegate,get,set);
-			}
-		})();
-		return eval($loader.code);
-	})();
-
-	var getCode = function(code) {
-		//	TODO	maybe should only be with debugging on? Although this way name will be used in stack traces
-		if (typeof(code) == "object" && code.name && code.$in) {
-			if ($loader.script) {
-				return function() { $loader.script(code.name,code.$in,arguments[0],arguments[1]); };
-			} else {
-				return String(
-					new Packages.inonit.script.runtime.io.Streams().readString(code.$in)
-				);
-			}
-		} else if (typeof(code) == "string") {
-			return code;
-		} else {
-			throw "Unimplemented: code = " + code;
-		}
-	}
-
-	this.run = function(code,scope,target) {
-		loader.run(getCode(code),scope,target);
-	}
-
-	this.file = function(code,$context) {
-		return loader.file(getCode(code),$context);
-	}
-
-	var engineModuleCodeLoader = function($engine_module) {
-		return new function() {
-			this.main = String($engine_module.getMainScriptPath());
-
-			this.getCode = function(path) {
-				//	TODO	maybe should only be with debugging on?
-				var $in = $engine_module.read(new Packages.java.lang.String(path));
-				if (!$in) throw "Missing module file: " + path + " in " + $engine_module;
-				return getCode({
-					name: String($engine_module) + ":" + path,
-					$in: $in
-				});
+(function() {
+	return new function() {
+		var loader = (function() {
+			var $engine = {
+				Object: {
+					defineProperty: {}
+				}
 			};
+			(function() {
+				var $objects = new Packages.inonit.script.rhino.Objects();
+				$engine.Object.defineProperty.setReadOnly = function(object,name,value) {
+					$objects.setReadOnly(object,name,value);
+				}
+
+				$engine.MetaObject = function(p) {
+					var delegate = (p.delegate) ? p.delegate : {};
+					var get = (p.get) ? p.get : function(){};
+					var set = (p.set) ? p.set : function(){};
+					return Packages.inonit.script.rhino.MetaObject.create(delegate,get,set);
+				}
+			})();
+			return eval($loader.code);
+		})();
+
+		var getCode = function(code) {
+			if (typeof(code) == "object" && code.name && code.$in) {
+				if ($loader.script) {
+					return function() { $loader.script(code.name,code.$in,arguments[0],arguments[1]); };
+				} else {
+					return String(
+						new Packages.inonit.script.runtime.io.Streams().readString(code.$in)
+					);
+				}
+			} else if (typeof(code) == "string") {
+				return code;
+			} else {
+				throw "Unimplemented: code = " + code;
+			}
+		}
+
+		this.run = function(code,scope,target) {
+			loader.run(getCode(code),scope,target);
+		}
+
+		this.file = function(code,$context) {
+			return loader.file(getCode(code),$context);
+		}
+
+		var engineModuleCodeLoader = function($engine_module) {
+			return new function() {
+				this.main = String($engine_module.getMainScriptPath());
+
+				this.getCode = function(path) {
+					var $in = $engine_module.read(new Packages.java.lang.String(path));
+					if (!$in) throw "Missing module file: " + path + " in " + $engine_module;
+					return getCode({
+						name: String($engine_module) + ":" + path,
+						$in: $in
+					});
+				};
 
 
-			this.decorateLoader = function($loader) {
-				$loader.java = new function() {
-					this.read = function(path) {
-						return $engine_module.read(new Packages.java.lang.String(path));
+				this.decorateLoader = function($loader) {
+					$loader.java = new function() {
+						this.read = function(path) {
+							return $engine_module.read(new Packages.java.lang.String(path));
+						}
 					}
 				}
 			}
 		}
-	}
 
-	this.module = function($module,p) {
-		return loader.module(engineModuleCodeLoader($module),p);
-	}
+		this.module = function($module,p) {
+			return loader.module(engineModuleCodeLoader($module),p);
+		}
 
-	this.namespace = function(name) {
-		return loader.namespace(name);
-	}
+		this.namespace = function(name) {
+			return loader.namespace(name);
+		}
 
-	//	currently only used by jsapi in jsh/unit via jsh.js, so undocumented
-	this.$platform = loader.$platform;
+		//	currently only used by jsapi in jsh/unit via jsh.js, so undocumented
+		this.$platform = loader.$platform;
 
-	//	currently used to set deprecation warning in jsh.js
-	//	currently used by jsapi in jsh/unit via jsh.js
-	this.$api = loader.$api;
-}
+		//	currently used to set deprecation warning in jsh.js
+		//	currently used by jsapi in jsh/unit via jsh.js
+		this.$api = loader.$api;
+	};
+})()
