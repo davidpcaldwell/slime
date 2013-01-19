5e10033c06c5429cb412a7a03210a70a03b49415
David P. Caldwell <david@davidpcaldwell.com>
2013-01-19 17:07:58 -0500

    add hotspots to profiling

--- rhino/tools/profiler/viewer/viewer.js
+++ rhino/tools/profiler/viewer/viewer.js
@@ -44,20 +44,22 @@ var render = function(profiles,settings) {
 		heading.innerHTML = "Tree";
 		div_tree.appendChild(heading);
 		div_profile.appendChild(div_tree);
+		
+		var nodeName = function(node) {
+			if (node.code.className && node.code.methodName) {
+				return node.code.className + " " + node.code.methodName + " " + node.code.signature;
+			} else if (node.code.sourceName && node.code.lineNumbers) {
+				return node.code.sourceName + " [" + node.code.lineNumbers[0] + "-" + node.code.lineNumbers[node.code.lineNumbers.length-1] + "]";
+			} else {
+				return "(top)";
+			}
+		}
 
 		var renderNode = function(node) {
 			var top = document.createElement("div");
 			top.className = "node";
 			var total = div("total", top);
-			var name = (function() {
-				if (node.code.className && node.code.methodName) {
-					return node.code.className + " " + node.code.methodName + " " + node.code.signature;
-				} else if (node.code.sourceName && node.code.lineNumbers) {
-					return node.code.sourceName + " [" + node.code.lineNumbers[0] + "-" + node.code.lineNumbers[node.code.lineNumbers.length-1] + "]";
-				} else {
-					return "(top)";
-				}
-			})();
+			var name = nodeName(node);
 			total.innerHTML = (node.statistics.elapsed/1000).toFixed(3) + " " + node.statistics.count + " " + name.replace(/\</g, "&lt;");
 			node.children.filter(function(child) {
 				var children = 0;
@@ -75,6 +77,44 @@ var render = function(profiles,settings) {
 		}
 
 		div_tree.appendChild(renderNode(profile.timing.root));
+		
+		var div_hotspots = div("hotspots", div_profile);
+		var heading = document.createElement("h2");
+		heading.innerHTML = "Hot Spots";
+		div_hotspots.appendChild(heading);
+		
+		var map = {};
+		
+		var addToHotspots = function(node) {
+			var key = nodeName(node);
+			if (!map[key]) {
+				map[key] = {
+					count: 0,
+					elapsed: 0
+				};
+			}
+			map[key].count += node.statistics.count;
+			map[key].elapsed += node.statistics.elapsed;
+			node.children.forEach(function(child) {
+				addToHotspots(child);
+			});
+		};
+		
+		addToHotspots(profile.timing.root);
+		
+		var list = [];
+		for (var x in map) {
+			list.push({ key: x, count: map[x].count, elapsed: map[x].elapsed });
+		}
+		list.sort(function(a,b) {
+			return b.elapsed - a.elapsed;
+		});
+		list.forEach(function(item) {
+			if ( item.elapsed > settings.threshold ) {
+				var hotspotdiv = div("hotspot", div_hotspots);
+				hotspotdiv.innerHTML = (item.elapsed / 1000).toFixed(3) + " " + item.count + " " + item.key.replace(/\</g, "&lt;");
+			}
+		})
 	});
 }
 
