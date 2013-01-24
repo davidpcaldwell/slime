7d379b29cc711739089581e0b9ce9f53e6375da5
David P. Caldwell <david@davidpcaldwell.com>
2013-01-24 05:41:15 -0500

    Update copyright dates

--- loader/api/api.js
+++ loader/api/api.js
@@ -5,7 +5,7 @@
 //	The Original Code is the SLIME loader infrastructure.
 //
 //	The Initial Developer of the Original Code is David P. Caldwell <david@davidpcaldwell.com>.
-//	Portions created by the Initial Developer are Copyright (C) 2010 the Initial Developer. All Rights Reserved.
+//	Portions created by the Initial Developer are Copyright (C) 2010-2013 the Initial Developer. All Rights Reserved.
 //
 //	Contributor(s):
 //	END LICENSE
