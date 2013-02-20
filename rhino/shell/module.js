7971d3765ea6c9dbdb16d36d6e307c7236a7f154
David P. Caldwell <david@davidpcaldwell.com>
2013-02-20 05:51:40 -0500

    Automated pre-commit code formatting

--- rhino/shell/module.js
+++ rhino/shell/module.js
@@ -118,7 +118,7 @@ $exports.environment = (function() {
 			return value;
 		};
 	};
-	
+
 	var isCaseInsensitive = (function() {
 		var jenv = Packages.java.lang.System.getenv();
 		var i = jenv.keySet().iterator();
