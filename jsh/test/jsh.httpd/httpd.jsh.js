6bed9b1900c400f2e467c1b7561889d0687c962b
David P. Caldwell <david@davidpcaldwell.com>
2013-02-20 05:47:58 -0500

    Do not attempt to run inside-Tomcat tests if Bourne shell is not present. Completes the process of adding automated tests that exercise the servlet framework. (Fixes issue {Bitbucket issue: [Document] Add documentation/automated testing for the servlet plugin}.)

--- jsh/test/jsh.httpd/httpd.jsh.js
+++ jsh/test/jsh.httpd/httpd.jsh.js
@@ -151,87 +151,89 @@ var plugin = new function() {
 	}
 };
 
-var server = new function() {
+var server = (function() {
 	if (!jsh.file.Pathname("/bin/sh").file) {
-		jsh.shell.echo("No Bourne shell");
+		jsh.shell.echo("No Bourne shell; cannot run automated inside-Tomcat tests");
 		return;
 	}
 	if (!jsh.shell.environment.CATALINA_HOME) {
-		jsh.shell.echo("No $CATALINA_HOME");
+		jsh.shell.echo("No $CATALINA_HOME; cannot run automated inside-Tomcat tests");
 		return;
 	}
-	var environment = {
-		CATALINA_HOME: jsh.file.Pathname(jsh.shell.environment.CATALINA_HOME).directory,
-		CATALINA_BASE: (function() {
-			if (parameters.options["tomcat.base"]) {
-				return parameters.options["tomcat.base"].directory;
-			} else {
-				return jsh.shell.TMPDIR.createTemporary({ directory: true });
-			}
-		})()
-	};
-	jsh.shell.echo("CATALINA_HOME: " + environment.CATALINA_HOME);
-	jsh.shell.echo("CATALINA_BASE: " + environment.CATALINA_BASE);
+	return new function() {
+		var environment = {
+			CATALINA_HOME: jsh.file.Pathname(jsh.shell.environment.CATALINA_HOME).directory,
+			CATALINA_BASE: (function() {
+				if (parameters.options["tomcat.base"]) {
+					return parameters.options["tomcat.base"].directory;
+				} else {
+					return jsh.shell.TMPDIR.createTemporary({ directory: true });
+				}
+			})()
+		};
+		jsh.shell.echo("CATALINA_HOME: " + environment.CATALINA_HOME);
+		jsh.shell.echo("CATALINA_BASE: " + environment.CATALINA_BASE);
 
-	var tomcat = jsh.script.loader.file("httpd.tomcat.js");
-	var installation = new tomcat.Tomcat({
-		home: environment.CATALINA_HOME
-	});
-	var server = new installation.Base({
-		base: environment.CATALINA_BASE,
-		configuration: environment.CATALINA_HOME.getFile("conf/server.xml")
-	});
+		var tomcat = jsh.script.loader.file("httpd.tomcat.js");
+		var installation = new tomcat.Tomcat({
+			home: environment.CATALINA_HOME
+		});
+		var server = new installation.Base({
+			base: environment.CATALINA_BASE,
+			configuration: environment.CATALINA_HOME.getFile("conf/server.xml")
+		});
 
-	var build = function() {
-		jsh.shell.echo("Building webapps ...");
+		var build = function() {
+			jsh.shell.echo("Building webapps ...");
 
-		var buildWebapp = function(urlpath,servletpath) {
-			//	TODO	may want to move this to httpd.tomcat.js, although it would need to somehow be aware of location of
-			//			webapp.jsh.js
-			jsh.shell.jsh(
-				jsh.script.getRelativePath("../../../rhino/http/servlet/tools/webapp.jsh.js"),
-				[
-					"-to", environment.CATALINA_BASE.getSubdirectory("webapps").getRelativePath(urlpath),
-					"-servletapi", environment.CATALINA_HOME.getRelativePath("lib/servlet-api.jar"),
-					"-resources", jsh.script.getRelativePath("httpd.resources.js"),
-					"-servlet", servletpath
-				],
-				{
-					onExit: function(result) {
-						jsh.shell.echo("Command: " + [result.command].concat(result.arguments).join(" "));
-						jsh.shell.echo("Status: " + result.status);
-						if (result.status) {
-							throw new Error("Exit status: " + result.status);
+			var buildWebapp = function(urlpath,servletpath) {
+				//	TODO	may want to move this to httpd.tomcat.js, although it would need to somehow be aware of location of
+				//			webapp.jsh.js
+				jsh.shell.jsh(
+					jsh.script.getRelativePath("../../../rhino/http/servlet/tools/webapp.jsh.js"),
+					[
+						"-to", environment.CATALINA_BASE.getSubdirectory("webapps").getRelativePath(urlpath),
+						"-servletapi", environment.CATALINA_HOME.getRelativePath("lib/servlet-api.jar"),
+						"-resources", jsh.script.getRelativePath("httpd.resources.js"),
+						"-servlet", servletpath
+					],
+					{
+						onExit: function(result) {
+							jsh.shell.echo("Command: " + [result.command].concat(result.arguments).join(" "));
+							jsh.shell.echo("Status: " + result.status);
+							if (result.status) {
+								throw new Error("Exit status: " + result.status);
+							}
 						}
 					}
+				);
+			};
+			buildWebapp("slime.hello", "test/hello.servlet.js");
+			buildWebapp("slime.file", "test/file.servlet.js");
+			buildWebapp("slime.api", "test/api.servlet.js");
+		}
+
+		this.start = function() {
+			build();
+			jsh.shell.echo("Invoking Tomcat start script ...");
+			server.start({
+				debug: {
+					script: parameters.options["debug:server"]
 				}
-			);
-		};
-		buildWebapp("slime.hello", "test/hello.servlet.js");
-		buildWebapp("slime.file", "test/file.servlet.js");
-		buildWebapp("slime.api", "test/api.servlet.js");
-	}
+			});
+			//	TODO	horrifying synchronization strategy
+			jsh.shell.echo("Invoked Tomcat start script ...");
+			debugger;
+			jsh.shell.echo("Pausing to let Tomcat start ...");
+			Packages.java.lang.Thread.sleep(5000);
+			jsh.shell.echo("Continuing, assuming Tomcat has started.");
+		}
 
-	this.start = function() {
-		build();
-		jsh.shell.echo("Invoking Tomcat start script ...");
-		server.start({
-			debug: {
-				script: parameters.options["debug:server"]
-			}
-		});
-		//	TODO	horrifying synchronization strategy
-		jsh.shell.echo("Invoked Tomcat start script ...");
-		debugger;
-		jsh.shell.echo("Pausing to let Tomcat start ...");
-		Packages.java.lang.Thread.sleep(5000);
-		jsh.shell.echo("Continuing, assuming Tomcat has started.");
-	}
-
-	this.stop = function() {
-		server.stop();
-	}
-}
+		this.stop = function() {
+			server.stop();
+		}
+	};
+})();
 
 var suites = {
 	plugin: function() {
@@ -249,7 +251,9 @@ var suites = {
 	},
 	all: function() {
 		this.plugin();
-		this.server();
+		if (server) {
+			this.server();
+		}
 	}
 };
 
