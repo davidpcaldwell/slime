b10a43282e954a60ce23e126f1d1cb7c8e9bd8bc
David P. Caldwell <david@davidpcaldwell.com>
2013-03-09 04:53:19 -0500

    add checking for third argument to Type constructor

--- js/mime/module.js
+++ js/mime/module.js
@@ -1,7 +1,12 @@
 $exports.Type = $api.Function({
 	before: [
 		$api.Function.arguments.isString({ index: 0, name: "media" }),
-		$api.Function.arguments.isString({ index: 1, name: "subtype" })
+		$api.Function.arguments.isString({ index: 1, name: "subtype" }),
+		function(p) {
+			if (typeof(p.arguments[2]) != "object" && typeof(p.arguments[2]) != "undefined") {
+				throw new TypeError("arguments[2] (parameters) must be undefined or object");
+			}
+		}
 	],
 	call: function(media,subtype,parameters) {
 		this.getMedia = function() {
