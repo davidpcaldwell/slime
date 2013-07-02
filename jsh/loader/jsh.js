ef88978895d6ffcc13a66f41df3d9bb1f15a3191
David P. Caldwell <david@davidpcaldwell.com>
2013-07-02 16:23:08 -0400

    remove dead code

--- jsh/loader/jsh.js
+++ jsh/loader/jsh.js
@@ -283,34 +283,6 @@ this.jsh = new function() {
 		);
 	})();
 
-//	jsh.shell = (function() {
-//		var context = {};
-//		context.api = {
-//			js: js,
-//			java: java,
-//			shell: $shell,
-//			io: jsh.io,
-//			file: jsh.file
-//		}
-//		context.stdio = new function() {
-//			this["in"] = jsh.io.java.adapt($host.getStdio().getStandardInput());
-//			this["out"] = jsh.io.java.adapt($host.getStdio().getStandardOutput());
-//			this["err"] = jsh.io.java.adapt($host.getStdio().getStandardOutput());
-//		}
-//		context.getSystemProperty = function(name) {
-//			var rv = $host.getSystemProperties().getProperty(name);
-//			if (rv == null) return null;
-//			return String(rv);
-//		};
-//		context._getSystemProperties = function() {
-//			return $host.getSystemProperties();
-//		};
-//		context.exit = function(code) {
-//			$host.exit(code);
-//		}
-//		return loader.bootstrap(context,"jsh/shell");
-//	})();
-
 	jsh.$jsapi = {
 		$platform: loader.$platform,
 		$api: loader.$api,
