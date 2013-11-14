19fcd5b4a10615522f1faad2545ecb4601dce004
David P. Caldwell <david@davidpcaldwell.com>
2013-11-14 08:13:31 -0500

    add filtering for Java classes to profiler

--- jsh/loader/jsh.js
+++ jsh/loader/jsh.js
@@ -377,91 +377,93 @@ this.jsh = new function() {
 					var tokens = declaration.split("=");
 					options[tokens[0]] = tokens[1];
 				});
-				Packages.inonit.tools.Profiler.javaagent().addListener(new JavaAdapter(
-					Packages.inonit.tools.Profiler.Listener,
-					new function() {
-						var Code = function(_peer) {
-							if (_peer && _peer.getSourceName && _peer.getLineNumbers && _peer.getFunctionName) {
-								this.sourceName = String(_peer.getSourceName());
-								this.lineNumbers = jsh.java.toJsArray(_peer.getLineNumbers(), function(v) {
-									return v;
-								});
-								if (_peer.getFunctionName()) {
-									this.functionName = String(_peer.getFunctionName());
+				if (options.listener || options.output) {
+					Packages.inonit.tools.Profiler.javaagent().addListener(new JavaAdapter(
+						Packages.inonit.tools.Profiler.Listener,
+						new function() {
+							var Code = function(_peer) {
+								if (_peer && _peer.getSourceName && _peer.getLineNumbers && _peer.getFunctionName) {
+									this.sourceName = String(_peer.getSourceName());
+									this.lineNumbers = jsh.java.toJsArray(_peer.getLineNumbers(), function(v) {
+										return v;
+									});
+									if (_peer.getFunctionName()) {
+										this.functionName = String(_peer.getFunctionName());
+									}
+								} else if (_peer && _peer.getClassName && _peer.getMethodName && _peer.getSignature) {
+									this.className = String(_peer.getClassName());
+									this.methodName = String(_peer.getMethodName());
+									this.signature = String(_peer.getSignature());
+								} else {
 								}
-							} else if (_peer && _peer.getClassName && _peer.getMethodName && _peer.getSignature) {
-								this.className = String(_peer.getClassName());
-								this.methodName = String(_peer.getMethodName());
-								this.signature = String(_peer.getSignature());
-							} else {
 							}
-						}
 
-						var Statistics = function(_peer) {
-							this.count = _peer.getCount();
-							this.elapsed = _peer.getElapsed();
-						}
+							var Statistics = function(_peer) {
+								this.count = _peer.getCount();
+								this.elapsed = _peer.getElapsed();
+							}
 
-						var Node = function(_peer) {
-							var Constructor = arguments.callee;
+							var Node = function(_peer) {
+								var Constructor = arguments.callee;
 
-							this.code = new Code(_peer.getCode());
-							this.statistics = new Statistics(_peer.getStatistics());
-							this.children = jsh.java.toJsArray(_peer.getChildren(), function(_child) {
-								return new Constructor(_child);
-							});
-						}
+								this.code = new Code(_peer.getCode());
+								this.statistics = new Statistics(_peer.getStatistics());
+								this.children = jsh.java.toJsArray(_peer.getChildren(), function(_child) {
+									return new Constructor(_child);
+								});
+							}
 
-						var Timing = function(_peer) {
-							this.root = new Node(_peer.getRoot());
-						}
+							var Timing = function(_peer) {
+								this.root = new Node(_peer.getRoot());
+							}
 
-						var Profile = function(_peer) {
-							this.thread = {
-								name: String(_peer.getThread().getName())
+							var Profile = function(_peer) {
+								this.thread = {
+									name: String(_peer.getThread().getName())
+								};
+
+								this.timing = new Timing(_peer.getTiming());
 							};
 
-							this.timing = new Timing(_peer.getTiming());
-						};
-
-						this.onExit = function(_profiles) {
-							var profiles = jsh.java.toJsArray(_profiles, function(_profile) {
-								return new Profile(_profile);
-							});
-
-							if (options.listener) {
-								var _listener = new Packages.java.io.File(options.listener);
-								var pathname = jsh.file.filesystems.os.Pathname(String(_listener.getCanonicalPath()));
-								jsh.loader.run(pathname, {
-									$loader: new loader.Loader({ _source: Packages.inonit.script.rhino.Code.Source.create(_listener.getParentFile()) }),
-									jsh: jsh,
-									profiles: profiles
+							this.onExit = function(_profiles) {
+								var profiles = jsh.java.toJsArray(_profiles, function(_profile) {
+									return new Profile(_profile);
 								});
-							} else if (options.output && /\.html$/.test(options.output) && jsh.shell.jsh.home) {
-								var pathname = jsh.shell.jsh.home.getRelativePath("tools/profiler/viewer/module.js");
-								//	TODO	the below would not work because when jsh.loader.module loads the module, it does not
-								//			provide the module with a $loader which has been decorated with the jsh.io stuff
-								//			(that is, the .resource() method). That's a bug and a redesign may be needed.
-								//
-								//			The good news is that we can now use the same scope strategy to send profiles (not
-								//			putting it within $context).
-								if (false) {
-									jsh.loader.module(pathname, {
-										profiles: profiles,
-										to: jsh.file.filesystems.os.Pathname(String(new Packages.java.io.File(options.output).getCanonicalPath()))
-									});
-								} else {
+
+								if (options.listener) {
+									var _listener = new Packages.java.io.File(options.listener);
+									var pathname = jsh.file.filesystems.os.Pathname(String(_listener.getCanonicalPath()));
 									jsh.loader.run(pathname, {
-										$loader: new loader.Loader({ _source: Packages.inonit.script.rhino.Code.Source.create(pathname.parent.java.adapt()) }),
+										$loader: new loader.Loader({ _source: Packages.inonit.script.rhino.Code.Source.create(_listener.getParentFile()) }),
 										jsh: jsh,
-										profiles: profiles,
-										to: jsh.file.filesystems.os.Pathname(String(new Packages.java.io.File(options.output).getCanonicalPath()))
+										profiles: profiles
 									});
+								} else if (options.output && /\.html$/.test(options.output) && jsh.shell.jsh.home) {
+									var pathname = jsh.shell.jsh.home.getRelativePath("tools/profiler/viewer/module.js");
+									//	TODO	the below would not work because when jsh.loader.module loads the module, it does not
+									//			provide the module with a $loader which has been decorated with the jsh.io stuff
+									//			(that is, the .resource() method). That's a bug and a redesign may be needed.
+									//
+									//			The good news is that we can now use the same scope strategy to send profiles (not
+									//			putting it within $context).
+									if (false) {
+										jsh.loader.module(pathname, {
+											profiles: profiles,
+											to: jsh.file.filesystems.os.Pathname(String(new Packages.java.io.File(options.output).getCanonicalPath()))
+										});
+									} else {
+										jsh.loader.run(pathname, {
+											$loader: new loader.Loader({ _source: Packages.inonit.script.rhino.Code.Source.create(pathname.parent.java.adapt()) }),
+											jsh: jsh,
+											profiles: profiles,
+											to: jsh.file.filesystems.os.Pathname(String(new Packages.java.io.File(options.output).getCanonicalPath()))
+										});
+									}
 								}
 							}
 						}
-					}
-				));
+					));
+				}
 			}
 		})();
 		//	TODO	test for the existence of this class as well?
