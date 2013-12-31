768965f473c6980e462644c18b0fba9d29952f8e
David P. Caldwell <david@davidpcaldwell.com>
2013-12-31 15:03:24 -0500

    allow the addition of custom properties to our custom errors

--- js/object/Error.js
+++ js/object/Error.js
@@ -1,13 +1,16 @@
 $exports.Error = {};
 
 $exports.Error.Type = function(name) {
-	var rv = function(message) {
+	var rv = function(message,properties) {
 		if (this instanceof arguments.callee) {
 			this.name = name;
 			this.message = message;
 			var template = new Error();
 			if (template.stack) {
 				this.stack = template.stack;
+			}
+			for (var x in properties) {
+				this[x] = properties[x];
 			}
 		} else {
 			return new arguments.callee(message);
