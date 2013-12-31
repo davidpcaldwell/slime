e0dfc9c43150da5847b42ce33c27f7414fdadaca
David P. Caldwell <david@davidpcaldwell.com>
2013-12-31 14:58:04 -0500

    fix case of error type called as function rather than constructor

--- js/object/Error.js
+++ js/object/Error.js
@@ -2,11 +2,15 @@ $exports.Error = {};
 
 $exports.Error.Type = function(name) {
 	var rv = function(message) {
-		this.name = name;
-		this.message = message;
-		var template = new Error();
-		if (template.stack) {
-			this.stack = template.stack;
+		if (this instanceof arguments.callee) {
+			this.name = name;
+			this.message = message;
+			var template = new Error();
+			if (template.stack) {
+				this.stack = template.stack;
+			}
+		} else {
+			return new arguments.callee(message);
 		}
 	};
 	rv.prototype = new Error();
