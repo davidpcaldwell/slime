54dce150cc12cf94c125ac83d884ab28687b4a76
David P. Caldwell <david@davidpcaldwell.com>
2014-03-07 07:06:40 -0500

    working on URL parsing

--- js/web/module.js
+++ js/web/module.js
@@ -39,7 +39,7 @@ $exports.Url = function(o) {
 	}
 };
 $exports.Url.parse = function(string) {
-	var matcher = /(?:(.*)\:\/\/)([\w\.]+)(?:\:(\d+))?(\/.*)\?(.*)\#(.*)/;
+	var matcher = /(?:([^\:\/\?\#]+)\:\/\/)([\w\.]+)(?:\:(\d+))?(\/.*)\?(.*)\#(.*)/;
 	var match = matcher.exec(string);
 	return new $exports.Url({
 		scheme: match[1],
