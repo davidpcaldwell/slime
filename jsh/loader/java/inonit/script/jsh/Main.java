bad3eba0e71eba96df2c3c3638741ac016544798
David P. Caldwell <david@davidpcaldwell.com>
2014-04-25 07:30:27 -0400

    Refactor jsh launcher to run loader in Nashorn if available

--- jsh/loader/java/inonit/script/jsh/Main.java
+++ jsh/loader/java/inonit/script/jsh/Main.java
@@ -115,52 +115,7 @@ public class Main {
 			invocation = Invocation.create(args.toArray(new String[0]));
 		}
 
-		final Shell.Configuration configuration = new Shell.Configuration() {
-			private InputStream stdin = new Logging.InputStream(System.in);
-			//	We assume that as long as we have separate launcher and loader processes, we should immediately flush stdout
-			//	whenever it is written to (by default it only flushes on newlines). This way the launcher process can handle
-			//	ultimately buffering the stdout to the console or other ultimate destination.
-			private OutputStream stdout = new Logging.OutputStream(inonit.script.runtime.io.Streams.Bytes.Flusher.ALWAYS.decorate(System.out), "stdout");
-			//	We do not make the same assumption for stderr because we assume it will always be written to a console-like
-			//	device and bytes will never need to be immediately available
-			private OutputStream stderr = new PrintStream(new Logging.OutputStream(System.err, "stderr"));
-
-			public ClassLoader getClassLoader() {
-				return ClassLoader.getSystemClassLoader();
-			}
-
-			public Properties getSystemProperties() {
-				return System.getProperties();
-			}
-
-			public OperatingSystem.Environment getEnvironment() {
-				return OperatingSystem.Environment.SYSTEM;
-			}
-
-			public Stdio getStdio() {
-				return new Stdio() {
-					public InputStream getStandardInput() {
-						return stdin;
-					}
-
-					public OutputStream getStandardOutput() {
-						return stdout;
-					}
-
-					public OutputStream getStandardError() {
-						return stderr;
-					}
-				};
-			}
-
-			@Override public Code.Source getPackagedCode() {
-				if (System.getProperty("jsh.launcher.packaged") != null) {
-					return Code.Source.system("$packaged/");
-				} else {
-					return null;
-				}
-			}
-		};
+		final Shell.Configuration configuration = Shell.Configuration.main();
 		
 		this.rhino = new Configuration() {
 			public int getOptimizationLevel() {
