b2a34565a259489ffc5b28b1cc610774a2cc3962
David P. Caldwell <david@davidpcaldwell.com>
2014-05-06 16:40:38 -0400

    move engine-specific features into rhino loader

--- jsh/loader/rhino.js
+++ jsh/loader/rhino.js
@@ -1,42 +1,4 @@
 var $engine = new function() {
-//	this.script = function(name,code,scope,target) {
-//		return $rhino.script(name,code,scope,target);
-//	};
-//	
-//	this.getClasspath = function() {
-//		return $rhino.getClasspath();
-//	};
-//	
-//	this.getDebugger = function() {
-//		return $rhino.getDebugger();
-//	};
-	
-	//	toScope apparently not necessary
-	
-//	this.exit = function(status) {
-//		return $rhino.exit(status);
-//	};
-//	
-//	this.jsh = function(configuration,invocation) {
-//		return $rhino.jsh(configuration,invocation);
-//	};
-//	
-//	this.setReadOnly = function(object,name,value) {
-//		if (!arguments.callee.objects) {
-//			arguments.callee.objects = new Packages.inonit.script.rhino.Objects();
-//		}
-//		arguments.callee.objects.setReadOnly(object,name,value);							
-//	};
-//	
-//	this.MetaObject = function(p) {
-//		var delegate = (p.delegate) ? p.delegate : {};
-//		var get = (p.get) ? p.get : function(){};
-//		var set = (p.set) ? p.set : function(){};
-//		return Packages.inonit.script.rhino.MetaObject.create(delegate,get,set);
-//	};
-	
-	//	TODO	addFinalizer
-	//	TODO	destroy
 };
 $engine.$javaloader = (function() {
 	var _streams = new Packages.inonit.script.runtime.io.Streams();
