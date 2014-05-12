b48161451461e4792aa4bf44eef5d39a65ed499a
David P. Caldwell <david@davidpcaldwell.com>
2014-05-12 17:30:23 -0400

    move toward Nashorn version of servlet

--- rhino/http/servlet/java/inonit/script/servlet/NashornServlet.java
+++ rhino/http/servlet/java/inonit/script/servlet/NashornServlet.java
@@ -2,35 +2,55 @@ package inonit.script.servlet;
 
 import java.io.*;
 
+import javax.script.*;
+
+import inonit.script.engine.*;
 import inonit.script.nashorn.*;
 
 public class NashornServlet extends Servlet {
 	private static class Nashorn extends ScriptContainer {
-//		private Host host = new Host();
+		private Servlet servlet;
+		private Host host;
 		
-		@Override
-		void initialize(Servlet servlet) {
-			throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		@Override void initialize(Servlet servlet) {
+			this.servlet = servlet;
+			this.host = Host.create(new Classes.Configuration() {
+				@Override public boolean canCreateClassLoaders() {
+					return true;
+				}
+
+				@Override public ClassLoader getApplicationClassLoader() {
+					return NashornServlet.class.getClassLoader();
+				}				
+			});
 		}
 
-		@Override
-		Host getServletHostObject() {
-			throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		@Override Servlet.HostObject getServletHostObject() {
+			return new HostObject(servlet);
 		}
 
-		@Override
-		void setVariable(String name, Object value) {
-			throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		@Override void setVariable(String name, Object value) {
+			host.set(name, value);
 		}
 
-		@Override
-		void addScript(String name, InputStream stream) {
-			throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		@Override void addScript(final String name, final InputStream stream) {
+			host.add(new Code.Source.File() {
+				@Override public String getSourceName() {
+					return name;
+				}
+
+				@Override public Reader getReader() {
+					return new InputStreamReader(stream);
+				}
+			});
 		}
 
-		@Override
-		void execute() {
-			throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		@Override void execute() {
+			try {
+				host.run();
+			} catch (ScriptException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 	
