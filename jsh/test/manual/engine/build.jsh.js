9991f2d389f327edb1b3a4c7a77c2202b3692ce6
David P. Caldwell <david@davidpcaldwell.com>
2014-05-15 13:59:07 -0400

    add some scripts to enable easier testing of JDK/script engine combinations

--- jsh/test/manual/engine/build.jsh.js
+++ jsh/test/manual/engine/build.jsh.js
@@ -9,37 +9,52 @@ var SRC = jsh.script.file.parent.parent.parent.parent.parent;
 
 jsh.shell.echo(SRC);
 
-var builds = {};
-builds.rhino = function() {
+var build = function(p) {
 	var environment = {
 		//	TODO	One unit test as of this writing requires the PATH environment variable to be defined
 		PATH: jsh.shell.environment.PATH,
-		JSH_ENGINE: "rhino",
+		JSH_ENGINE: p.engine,
 		JSH_BUILD_TOMCAT_HOME: parameters.options.tomcat
 	};
 	var TMP = jsh.shell.TMPDIR.createTemporary({ directory: true });
+	var command = p.run({
+		script: SRC.getRelativePath("jsh/etc/unbuilt.rhino.js"),
+		arguments: ["build",TMP]		
+	});
 	jsh.shell.run({
-		command: jsh.shell.java.launcher,
-		arguments: ["-jar", parameters.options.rhino, "-opt", "-1", SRC.getRelativePath("jsh/etc/unbuilt.rhino.js"), "build", TMP],
+		command: command.command,
+		arguments: command.arguments,
 		environment: environment
 	});
+	jsh.shell.echo("Successfully built engine: " + p.engine);
 }
+
+var builders = {};
+if (parameters.options.rhino) {
+	builders.rhino = {
+		engine: "rhino",
+		run: function(p) {
+			return {
+				command: jsh.shell.java.launcher,
+				arguments: ["-jar", parameters.options.rhino, "-opt", "-1", p.script].concat(p.arguments)
+			};
+		}
+	};
+}
+
 var javaCommands = jsh.file.Searchpath([jsh.shell.java.home.getRelativePath("bin")]);
 var jjs = javaCommands.getCommand("jjs");
 if (jjs) {
-	builds.nashorn = function() {
-		var environment = {
-			PATH: jsh.shell.environment.PATH,
-			JSH_ENGINE: "nashorn",
-			JSH_BUILD_TOMCAT_HOME: parameters.options.tomcat
-		};
-		var TMP = jsh.shell.TMPDIR.createTemporary({ directory: true });
-		jsh.shell.run({
-			command: jjs,
-			arguments: [SRC.getRelativePath("jsh/etc/unbuilt.rhino.js"), "--", "build", TMP],
-			environment: environment
-		})
-	}
+	builders.nashorn = {
+		engine: "nashorn",
+		run: function(p) {
+			return {
+				command: jjs,
+				arguments: [p.script,"--"].concat(p.arguments)
+			};
+		}
+	};
 }
-builds.rhino();
-if (builds.nashorn) builds.nashorn();
+
+if (builders.rhino) build(builders.rhino);
+if (builders.nashorn) build(builders.nashorn);
