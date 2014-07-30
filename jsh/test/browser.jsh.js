bc126d8e545242477b7c7223cc8a24d625904a1a
David P. Caldwell <david@davidpcaldwell.com>
2014-07-30 11:58:35 -0400

    refactor browser tests to support Internet Explorer

--- jsh/test/browser.jsh.js
+++ jsh/test/browser.jsh.js
@@ -2,10 +2,16 @@ var parameters = jsh.script.getopts({
 	options: {
 		interactive: false,
 		chrome: jsh.file.Pathname,
+		ie: jsh.file.Pathname,
 		coffeescript: jsh.file.Pathname
 	},
 	unhandled: jsh.script.getopts.UNEXPECTED_OPTION_PARSER.SKIP
 });
+
+if (!jsh.httpd || !jsh.httpd.Tomcat) {
+	jsh.shell.stderr.write("Cannot run browser tests; Tomcat not present.\n");
+	jsh.shell.exit(1);
+}
 
 var all = [
 	"loader/test/data/a/", "loader/test/data/b/", "loader/test/data/c/main.js",
@@ -19,6 +25,122 @@ jsh.shell.echo("Running browser unit tests ...");
 var modules = (parameters.arguments.length == 0) ? all : parameters.arguments.map(function(path) { return { path: path } });
 
 var slimepath = "";
+
+var browseTestPage = function(p) {
+	var opened = this.browse(p.tomcat.url(p.url));
+	if (p.success) {
+		var response = p.client.request({
+			url: p.tomcat.url(p.success)
+		});
+		if (opened && opened.close) {
+			opened.close();
+		}
+		var string = response.body.stream.character().asString();
+		if (string == "false") return false;
+		if (string == "true") return true;
+		if (string == "null") return null;
+		return string;
+	}
+};
+
+var browserTest = function(p) {
+	var startServer = function(p) {
+		var tomcat = new jsh.httpd.Tomcat({});
+		var parameters = {
+			delegate: p.servlet,
+			url: p.success
+		};
+		for (var x in p.parameters) {
+			parameters[x] = p.parameters[x];
+		}
+		jsh.shell.echo("Starting server ...");
+		tomcat.map({
+			path: "",
+			servlets: {
+				//	alternative would be to allow an actual servlet, rather than file, to be specified using servlet plugin
+				//	and then somehow control threading from here: servlet could be defined above and could receive browser
+				//	callback and release lock allowing loop to proceed
+				//	TODO	should we enhance the servlet plugin interface to allow something other than a file?
+				"/*": {
+					file: jsh.script.file.getRelativePath("browser.servlet.js").file,
+					parameters: parameters
+				}
+			},
+			resources: p.resources
+		});
+		tomcat.start();
+		jsh.shell.echo("Tomcat base directory: " + tomcat.base);
+		return new function() {
+			this.port = tomcat.port;
+
+			this.url = function(url) {
+				return "http://127.0.0.1:" + tomcat.port + "/" + url;
+			};
+
+			this.run = function() {
+				tomcat.run();
+			}
+		};
+	};
+	
+	var tomcat = startServer(p);
+	var result = this.browseTestPage(jsh.js.Object.set({}, { tomcat: tomcat, client: new jsh.http.Client() }, p));
+	if (!p.success) {
+		tomcat.run();
+	} else {
+		if (result === false) {
+			throw new Error("Browser tests failed." + ((p.message) ? (" " + p.message) : ""));
+		} else if (result === true) {
+			jsh.shell.echo("Browser tests succeeded." + ((p.message) ? (" " + p.message) : ""));
+		} else if (result === null) {
+			throw new Error("Browser tests errored." + ((p.message) ? (" " + p.message) : ""));			
+		} else {
+			throw new Error("Error launching browser tests: " + result);
+		}
+	}
+};
+
+var Browser = function(p) {
+	var on = {
+		start: function(p) {
+			new lock.Waiter({
+				until: function() {
+					return true;
+				},
+				then: function() {
+					opened = new function() {
+						this.close = function() {
+							p.kill();
+						}
+					}										
+				}
+			})();
+		}
+	};
+	
+	this.browse = function(uri) {
+		var lock = new jsh.java.Thread.Monitor();
+		var opened;
+		jsh.shell.echo("Starting browser thread...");
+		jsh.java.Thread.start({
+			call: function() {
+				p.open(on)(uri);
+			}
+		});
+		var returner = new lock.Waiter({
+			until: function() {
+				return Boolean(opened);
+			},
+			then: function() {
+				return opened;
+			}
+		});
+		return returner();		
+	};
+	
+	this.browseTestPage = browseTestPage;
+	this.browserTest = browserTest;
+}
 
 var chrome;
 if (parameters.options.chrome) {
@@ -66,83 +188,48 @@ if (parameters.options.chrome) {
 			});
 			return returner();
 		};
-	}
-}
-
-var browseTestPage = function(p) {
-	var opened = chrome.browse(p.tomcat.url(p.url));
-	if (p.success) {
-		var response = p.client.request({
-			url: p.tomcat.url(p.success)
-		});
-		if (opened && opened.close) {
-			opened.close();
-		}
-		var string = response.body.stream.character().asString();
-		if (string == "false") return false;
-		if (string == "true") return true;
-		if (string == "null") return null;
-		return string;
-	}
-};
-
-var startServer = function(p) {
-	if (!jsh.httpd || !jsh.httpd.Tomcat) throw new Error("Cannot run browser tests; Tomcat not present.");
-	var tomcat = new jsh.httpd.Tomcat({});
-	var parameters = {
-		delegate: p.servlet,
-		url: p.success
-	};
-	for (var x in p.parameters) {
-		parameters[x] = p.parameters[x];
-	}
-	jsh.shell.echo("Starting server ...");
-	tomcat.map({
-		path: "",
-		servlets: {
-			//	alternative would be to allow an actual servlet, rather than file, to be specified using servlet plugin
-			//	and then somehow control threading from here: servlet could be defined above and could receive browser
-			//	callback and release lock allowing loop to proceed
-			//	TODO	should we enhance the servlet plugin interface to allow something other than a file?
-			"/*": {
-				file: jsh.script.file.getRelativePath("browser.servlet.js").file,
-				parameters: parameters
-			}
-		},
-		resources: p.resources
-	});
-	tomcat.start();
-	jsh.shell.echo("Tomcat base directory: " + tomcat.base);
-	return new function() {
-		this.port = tomcat.port;
-
-		this.url = function(url) {
-			return "http://127.0.0.1:" + tomcat.port + "/" + url;
-		};
-
-		this.run = function() {
-			tomcat.run();
-		}
-	};
-};
 	
-var browserTest = function(p) {
-	var tomcat = startServer(p);
-	var result = browseTestPage(jsh.js.Object.set({}, { tomcat: tomcat, client: new jsh.http.Client() }, p));
-	if (!p.success) {
-		tomcat.run();
-	} else {
-		if (result === false) {
-			throw new Error("Browser tests failed." + ((p.message) ? (" " + p.message) : ""));
-		} else if (result === true) {
-			jsh.shell.echo("Browser tests succeeded." + ((p.message) ? (" " + p.message) : ""));
-		} else if (result === null) {
-			throw new Error("Browser tests errored." + ((p.message) ? (" " + p.message) : ""));			
-		} else {
-			throw new Error("Error launching browser tests: " + result);
-		}
+		this.browseTestPage = browseTestPage;
+		this.browserTest = browserTest;
 	}
 }
+var ie;
+if (parameters.options.ie) {
+	ie = new Browser({
+		open: function(on) {
+			return function(uri) {
+				jsh.shell.echo("Opening IE ...");
+				jsh.shell.echo("Command: " + parameters.options.ie.file);
+				jsh.shell.echo("Arguments: " + uri);
+				jsh.shell.run({
+					command: parameters.options.ie.file,
+					arguments: [
+						uri
+					],
+					on: on
+				});
+			};
+		}
+	})
+}
+
+//var browserTest = function(p) {
+//	var tomcat = startServer(p);
+//	var result = browseTestPage(jsh.js.Object.set({}, { tomcat: tomcat, client: new jsh.http.Client() }, p));
+//	if (!p.success) {
+//		tomcat.run();
+//	} else {
+//		if (result === false) {
+//			throw new Error("Browser tests failed." + ((p.message) ? (" " + p.message) : ""));
+//		} else if (result === true) {
+//			jsh.shell.echo("Browser tests succeeded." + ((p.message) ? (" " + p.message) : ""));
+//		} else if (result === null) {
+//			throw new Error("Browser tests errored." + ((p.message) ? (" " + p.message) : ""));			
+//		} else {
+//			throw new Error("Error launching browser tests: " + result);
+//		}
+//	}
+//}
 		
 var getBrowserTestRequest = function(modules) {
 	jsh.shell.echo("Testing modules ...");
@@ -177,17 +264,22 @@ if (modules) {
 		request.parameters.push({ name: "callback", value: "server" });					
 	}
 	jsh.shell.echo("fullurl = " + request.build());
+	var browsers = [];
+	if (chrome) browsers.push(chrome);
+	if (ie) browsers.push(ie);
 	try {
-		browserTest(jsh.js.Object.set({}, {
-			resources: (function() {
-				var rv = new jsh.httpd.Resources();
-				rv.map("", jsh.script.file.getRelativePath("../../"));
-				return rv;
-			})(),
-			servlet: "jsh/test/browser.modules.js",
-			url: request.build(),
-			success: (parameters.options.interactive) ? null : slimepath + "loader/browser/test/success"
-		}, (parameters.options.coffeescript) ? { parameters: { coffeescript: parameters.options.coffeescript } } : {}));
+		browsers.forEach(function(browser) {
+			browser.browserTest(jsh.js.Object.set({}, {
+				resources: (function() {
+					var rv = new jsh.httpd.Resources();
+					rv.map("", jsh.script.file.getRelativePath("../../"));
+					return rv;
+				})(),
+				servlet: "jsh/test/browser.modules.js",
+				url: (browser != ie) ? request.build() : getBrowserTestRequest([]).build(),
+				success: (parameters.options.interactive) ? null : slimepath + "loader/browser/test/success"
+			}, (parameters.options.coffeescript) ? { parameters: { coffeescript: parameters.options.coffeescript } } : {}));			
+		})
 	} catch (e) {
 		if (e.rhinoException) {
 			e.rhinoException.printStackTrace();
