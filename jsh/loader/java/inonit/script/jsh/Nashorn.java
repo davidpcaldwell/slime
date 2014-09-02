f0639b92de6110435fe495f817d53826a1fd35d0
David P. Caldwell <david@davidpcaldwell.com>
2014-09-02 17:23:15 -0400

    dump stack traces in Nashorn shell more thoroughly

--- jsh/loader/java/inonit/script/jsh/Nashorn.java
+++ jsh/loader/java/inonit/script/jsh/Nashorn.java
@@ -133,6 +133,23 @@ public class Nashorn {
 	}
 
 	public static void main(final String[] args) throws Invocation.CheckedException {
+		Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+			public void uncaughtException(Thread t, Throwable e) {
+				Throwable error = e;
+				java.io.PrintWriter writer = new java.io.PrintWriter(System.err,true);
+				while(error != null) {
+					writer.println(error.getClass().getName() + ": " + error.getMessage());
+					StackTraceElement[] trace = error.getStackTrace();
+					for (StackTraceElement line : trace) {
+						writer.println("\t" + line);
+					}
+					error = error.getCause();
+					if (error != null) {
+						writer.print("Caused by: ");
+					}
+				}
+			}
+		});
 		main(Shell.main(args));
 	}
 }
