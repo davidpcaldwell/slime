01b3d8e76d029befaf5e09ecef7a8c72252a6a2a
David P. Caldwell <david@davidpcaldwell.com>
2014-10-16 08:05:28 -0400

    get profiler test suite working

--- rhino/tools/profiler/build.jsh.js
+++ rhino/tools/profiler/build.jsh.js
@@ -13,22 +13,13 @@
 var parameters = jsh.script.getopts({
 	options: {
 		to: jsh.file.Pathname,
-		javassist: jsh.file.Pathname,
-		test: String,
-		"test:debug": false,
-		"test:logging": jsh.file.Pathname,
-		"test:engine": String,
-		"test:internal": false,
-		"test:agentlib": String,
-		"test:output": jsh.file.Pathname
+		javassist: jsh.file.Pathname
 	}
 });
 
 if (!parameters.options.to) {
-	parameters.options.to = jsh.shell.TMPDIR.createTemporary({ suffix: ".jar" });
+	parameters.options.to = jsh.shell.TMPDIR.createTemporary({ prefix: "profiler-", suffix: ".jar" });
 }
-
-var tmp = jsh.shell.TMPDIR.createTemporary({ directory: true });
 
 var javassist;
 
@@ -39,6 +30,8 @@ if (!parameters.options.javassist) {
 	javassist = parameters.options.javassist;
 }
 
+var tmp = jsh.shell.TMPDIR.createTemporary({ directory: true });
+
 jsh.java.tools.javac({
 	destination: tmp.getRelativePath("classes"),
 	classpath: jsh.file.Searchpath([javassist]),
@@ -47,17 +40,6 @@ jsh.java.tools.javac({
 	]),
 	arguments: [
 		jsh.script.file.getRelativePath("java/inonit/tools/Profiler.java")
-	]
-});
-
-jsh.java.tools.javac({
-	destination: tmp.getRelativePath("test"),
-	classpath: jsh.file.Searchpath([]),
-	sourcepath: jsh.file.Searchpath([
-		jsh.script.file.getRelativePath("java")
-	]),
-	arguments: [
-		jsh.script.file.getRelativePath("java/inonit/tools/Test.java")
 	]
 });
 
@@ -95,108 +77,3 @@ jsh.shell.shell(
 );
 
 jsh.shell.echo("Created JAR at " + parameters.options.to);
-
-if (parameters.options.test) {
-	if (parameters.options.test == "java" || parameters.options.test == "all") {
-		jsh.shell.shell(
-			jsh.shell.java.home.getFile("bin/java"),
-			[
-				"-javaagent:" + parameters.options.to,
-				"-classpath", tmp.getRelativePath("test"),
-				"inonit.tools.Test"
-			],
-			{
-				onExit: function(result) {
-					jsh.shell.echo(result.command + " " + result.arguments.join(" "));
-					jsh.shell.echo("Exit status: "  + result.status);
-				}
-			}
-		)
-	}
-	if (parameters.options.test == "script" || parameters.options.test == "all") {
-		//	TODO	would be better to use jsh.shell.jsh, but would need to guarantee forking, which upcoming version may not
-		var evaluate = function(result) {
-			jsh.shell.echo(result.command + " " + result.arguments.join(" "));
-			jsh.shell.echo("Exit status: "  + result.status);
-		};
-		var agentlib = (parameters.options["test:agentlib"]) ? ["-agentlib:" + parameters.options["test:agentlib"]] : [];
-		var pairs = [];
-		if (parameters.options["test:debug"]) {
-			pairs.push({
-				name: "debug",
-				value: "true"
-			});
-		}
-		if (parameters.options["test:output"]) {
-			pairs.push({
-				name: "output",
-				value: parameters.options["test:output"].toString()
-			});
-		}
-		var profilerSettings = pairs.map(function(pair) { return pair.name + "=" + pair.value; }).join(",");
-		var vmarguments = ["-javaagent:" + parameters.options.to + "=" + profilerSettings].concat(agentlib).concat((parameters.options["test:debug"]) ? ["-verbose:class"] : []);
-		if (parameters.options["test:logging"]) {
-			vmarguments.push("-Djava.util.logging.config.file=" + parameters.options["test:logging"]);
-		}
-		var extdirs = String(Packages.java.lang.System.getProperty("java.ext.dirs"));
-		extdirs += ":" + parameters.options.to.parent.toString();
-		vmarguments.push("-Djava.ext.dirs=" + extdirs);
-		jsh.shell.echo("VM arguments: " + vmarguments);
-		if (true) {
-			jsh.shell.java({
-				vmarguments: (parameters.options["test:internal"]) ? vmarguments : [],
-				jar: jsh.shell.jsh.home.getRelativePath("jsh.jar"),
-				arguments: [
-					jsh.script.file.getRelativePath("../../../jsh/test/jsh.shell/properties.jsh.js")
-				],
-				environment: jsh.js.Object.set({}, jsh.shell.environment, {
-					JSH_LAUNCHER_INTERNAL: (parameters.options["test:internal"]) ? "true" : "",
-					JSH_SCRIPT_DEBUGGER: "profiler" + ((profilerSettings) ? ":" + profilerSettings : ""),
-//					JSH_LAUNCHER_CONSOLE_DEBUG: "true",
-//					JSH_JVM_OPTIONS: (!parameters.options["test:internal"]) ? vmarguments.join(" ") : "",
-					JSH_ENGINE: (parameters.options["test:engine"]) ? parameters.options["test:engine"] : ""
-				}),
-				evaluate: evaluate
-			});
-		} else if (true) {
-			jsh.shell.java({
-				vmarguments: [
-					"-javaagent:" + parameters.options.to
-				],
-				jar: jsh.shell.jsh.home.getRelativePath("jsh.jar"),
-				arguments: [
-					jsh.script.file.getRelativePath("../../../jsh/test/jsh.shell/properties.jsh.js")
-				],
-				environment: jsh.js.Object.set({}, jsh.shell.environment, {
-					JSH_LAUNCHER_INTERNAL: "true",
-					JSH_SCRIPT_DEBUGGER: profiler,
-					JSH_LAUNCHER_CONSOLE_DEBUG: "true",
-					JSH_ENGINE: "nashorn"
-				}),
-				evaluate: function(result) {
-					jsh.shell.echo(result.command + " " + result.arguments.join(" "));
-					jsh.shell.echo("Exit status: "  + result.status);
-				}
-			});
-		} else {
-			jsh.shell.shell(
-				jsh.shell.java.home.getFile("bin/java"),
-				[
-					"-classpath", jsh.shell.jsh.home.getRelativePath("jsh.jar"),
-					"inonit.script.jsh.launcher.Main",
-					jsh.script.file.getRelativePath("test.jsh.js")
-				],
-				{
-					environment: jsh.js.Object.set({}, jsh.shell.environment, {
-						JSH_JVM_OPTIONS: "-javaagent:" + parameters.options.to,
-						JSH_SCRIPT_DEBUGGER: "profiler"
-					}),
-					onExit: function(result) {
-						jsh.shell.echo(result.command + " " + result.arguments.join(" "));
-						jsh.shell.echo("Exit status: "  + result.status);
-					}
-				}
-			)
-		}
-	}
-}
---
+++ rhino/tools/profiler/test/suite.jsh.js
@@ -0,0 +1,155 @@
+//	LICENSE
+//	This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+//	distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//
+//	The Original Code is the SLIME JDK interface.
+//
+//	The Initial Developer of the Original Code is David P. Caldwell <david@davidpcaldwell.com>.
+//	Portions created by the Initial Developer are Copyright (C) 2014 the Initial Developer. All Rights Reserved.
+//
+//	Contributor(s):
+//	END LICENSE
+
+var parameters = jsh.script.getopts({
+	options: {
+		"build:javassist": jsh.file.Pathname,
+		build: String,
+		tests: "script",
+		jar: jsh.file.Pathname,
+		engine: jsh.script.getopts.ARRAY(String),
+		launcher: jsh.script.getopts.ARRAY(String),
+		agentlib: String,
+		output: jsh.file.Pathname,
+		debug: false,
+		logging: jsh.file.Pathname
+	}
+});
+
+if (!parameters.options.launcher.length) {
+	parameters.options.launcher = ["classloader","fork"];
+}
+if (!parameters.options.engine.length) {
+	parameters.options.engine = ["rhino","nashorn"];
+}
+
+if (!parameters.options.jar) {
+	var tmpdir = jsh.shell.TMPDIR.createTemporary({ directory: true });
+	parameters.options.jar = tmpdir.createTemporary({ suffix: ".jar" });
+}
+
+if (parameters.options.build == "shell" || parameters.options.build == "all") {
+	throw new Error("Need to implement standard way to build shell from within jsh");
+}
+if (parameters.options.build == "profiler" || parameters.options.build == "all") {
+	jsh.shell.jsh({
+		script: jsh.script.file.getRelativePath("../build.jsh.js").file,
+		arguments: [
+			"-javassist", parameters.options["build:javassist"],
+			"-to", parameters.options.jar
+		]
+	});
+}
+
+var tmp = jsh.shell.TMPDIR.createTemporary({ directory: true });
+
+jsh.java.tools.javac({
+	destination: tmp.getRelativePath("test"),
+	classpath: jsh.file.Searchpath([]),
+	sourcepath: jsh.file.Searchpath([
+		jsh.script.file.getRelativePath("java")
+	]),
+	arguments: [
+		jsh.script.file.getRelativePath("java/inonit/tools/Test.java")
+	]
+});
+jsh.shell.echo("Created test classes at " + tmp.getRelativePath("test"));
+
+if (parameters.options.tests == "java" || parameters.options.tests == "all") {
+	jsh.shell.shell(
+		jsh.shell.java.home.getFile("bin/java"),
+		[
+			"-javaagent:" + parameters.options.jar,
+			"-classpath", tmp.getRelativePath("test"),
+			"inonit.tools.Test"
+		],
+		{
+			onExit: function(result) {
+				jsh.shell.echo(result.command + " " + result.arguments.join(" "));
+				jsh.shell.echo("Exit status: "  + result.status);
+			}
+		}
+	)
+}
+
+if (parameters.options.tests == "script" || parameters.options.tests == "all") {
+	var getOutput = function(engine,launcher) {
+		if (parameters.options.engine.length > 1 || parameters.options.launcher.length > 1 || !/\.html$/.test(parameters.options.output)) {
+			//	pathname is directory
+			if (!arguments.callee.created) {
+				if (parameters.options.output.directory) {
+					parameters.options.output.directory.remove();
+				}
+				parameters.options.output.createDirectory();
+				arguments.callee.created = true;
+			}
+			return parameters.options.output.directory.getRelativePath("profile." + engine + "." + launcher + ".html");
+		} else {
+			return parameters.options.output;
+		}
+	}
+
+	var test = function(engine,launcher) {
+		var evaluate = function(result) {
+			jsh.shell.echo(result.command + " " + result.arguments.join(" "));
+			jsh.shell.echo("Exit status: "  + result.status);
+		};
+
+		//	TODO	would be better to use jsh.shell.jsh, but would need to guarantee forking, which upcoming version may not
+		var agentlib = (parameters.options.agentlib) ? ["-agentlib:" + parameters.options.agentlib] : [];
+		var pairs = [];
+		if (parameters.options.debug) {
+			pairs.push({
+				name: "debug",
+				value: "true"
+			});
+		}
+		if (parameters.options.output) {
+			pairs.push({
+				name: "output",
+				value: getOutput(engine,launcher).toString()
+			});
+		}
+		var profilerSettings = pairs.map(function(pair) { return pair.name + "=" + pair.value; }).join(",");
+		var vmarguments = (engine == "rhino" && launcher == "fork") ? [] : ["-javaagent:" + parameters.options.jar + "=" + profilerSettings];
+		var vmarguments = vmarguments.concat(agentlib).concat((parameters.options.debug) ? ["-verbose:class"] : []);
+		if (parameters.options.logging) {
+			vmarguments.push("-Djava.util.logging.config.file=" + parameters.options.logging);
+		}
+		var extdirs = String(Packages.java.lang.System.getProperty("java.ext.dirs"));
+		extdirs += ":" + parameters.options.jar.parent.toString();
+		vmarguments.push("-Djava.ext.dirs=" + extdirs);
+		jsh.shell.echo("VM arguments: " + vmarguments);
+		jsh.shell.java({
+			vmarguments: (launcher == "classloader") ? vmarguments : [],
+			jar: jsh.shell.jsh.home.getRelativePath("jsh.jar"),
+			arguments: [
+				jsh.script.file.getRelativePath("../../../../jsh/test/jsh.shell/properties.jsh.js")
+			],
+			environment: jsh.js.Object.set({}, jsh.shell.environment, {
+				JSH_LAUNCHER_INTERNAL: (launcher == "classloader") ? "true" : "",
+				JSH_SCRIPT_DEBUGGER: "profiler" + ((profilerSettings) ? ":" + profilerSettings : ""),
+//					JSH_LAUNCHER_CONSOLE_DEBUG: "true",
+				JSH_JVM_OPTIONS: (launcher == "fork") ? vmarguments.join(" ") : "",
+				JSH_ENGINE: (engine) ? engine : ""
+			}),
+			evaluate: evaluate
+		});
+	};
+
+	parameters.options.engine.forEach(function(engine) {
+		parameters.options.launcher.forEach(function(launcher) {
+			test(engine,launcher);
+		})
+	});
+}
