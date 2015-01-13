3b57303f34c3fac5fdc61cbda0857f472fcc6649
David P. Caldwell <david@davidpcaldwell.com>
2015-01-13 16:42:59 -0500

    code formatting, license

--- rhino/shell/apple.js
+++ rhino/shell/apple.js
@@ -1,3 +1,16 @@
+//	LICENSE
+//	This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+//	distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//
+//	The Original Code is the rhino/shell SLIME module.
+//
+//	The Initial Developer of the Original Code is David P. Caldwell <david@davidpcaldwell.com>.
+//	Portions created by the Initial Developer are Copyright (C) 2015 the Initial Developer. All Rights Reserved.
+//
+//	Contributor(s):
+//	END LICENSE
+
 if (!$context.api || !$context.api.document) {
 	throw new Error("Required: $context.api.document");
 }
@@ -10,7 +23,7 @@ var isArray = function(object) {
 }
 
 $exports.Plist = function(v) {
-	//	https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/PropertyLists/AboutPropertyLists/AboutPropertyLists.html	
+	//	https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/PropertyLists/AboutPropertyLists/AboutPropertyLists.html
 	var Value = function(v) {
 		if (typeof(v) == "object") {
 			if (isArray(v)) {
@@ -25,7 +38,7 @@ $exports.Plist = function(v) {
 					if (typeof(v[x]) == "undefined") {
 						//	TODO	what happens? Looks like the property should be omitted.
 					} else if (v[x] === null) {
-						//	TODO	what happens? Looks like the property should be omitted.						
+						//	TODO	what happens? Looks like the property should be omitted.
 					} else if (typeof(v[x]) == "string") {
 						var key = new $context.api.document.Element({ type: { name: "key" } });
 						//	TODO	document use of new Text() in slime/js/document/api.html
@@ -44,7 +57,7 @@ $exports.Plist = function(v) {
 			throw new TypeError("v = " + v + " typeof(v) = " + typeof(v));
 		}
 	};
-	
+
 	var document = new $context.api.document.Document();
 	var root = new $context.api.document.Element({
 		type: {
@@ -54,7 +67,7 @@ $exports.Plist = function(v) {
 	document.children.push(root);
 	root.element.attributes.set("version", "1.0");
 	root.children.push(new Value(v));
-	
+
 	this.serialize = function() {
 		return document.serialize.apply(document,arguments);
 	}
@@ -65,14 +78,14 @@ $exports.Plist = function(v) {
 $exports.bundle = {};
 $exports.bundle.osx = function(p) {
 	var info = $context.api.js.Object.set({}, p.info);
-	
+
 	if (p.command) {
 		this.command = p.command;
 		if (!info.CFBundleExecutable) {
 			info.CFBundleExecutable = "script";
 		}
 	}
-	
+
 	this.write = function(to) {
 		var contents = to.getRelativePath("Contents").createDirectory({
 			ifExists: function(dir) {
@@ -98,5 +111,4 @@ $exports.bundle.osx = function(p) {
 		}
 	}
 };
-
 
