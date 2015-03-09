7771c0a01004420155bb150c0b540345daa6c72c
David P. Caldwell <david@davidpcaldwell.com>
2015-03-09 18:04:21 -0400

    get subprocess version of test runner working correctly

--- jsh/unit/console.stdio.js
+++ jsh/unit/console.stdio.js
@@ -39,7 +39,20 @@ $exports.Parent = function(p) {
 		return " " + Packages.java.lang.Thread.currentThread().getName();
 	}
 	
-	var Scenario = function(json) {
+	var queued = [];
+
+	var queue = function(json) {
+		lock.Waiter({
+			until: function() {
+				return true;
+			},
+			then: function() {
+				queued.push(json);
+			}
+		})();
+	}
+		
+	var Scenario = function(json,top) {
 		var ended = false;
 		
 		stack.push(this);
@@ -50,43 +63,106 @@ $exports.Parent = function(p) {
 		
 		this.name = json.name;
 		
-		this.end = function() {
+		var self = this;
+		
+		var done = false;
+		
+		this.done = function() {
 			lock.Waiter({
 				until: function() {
 					return true;
 				},
 				then: function() {
-					ended = true;
+					done = true;
 				}
 			})();
 		}
 		
 		this.execute = function(scope) {
-			jsh.shell.echo("Executing a scenario: " + this + t());
-			var self = this;
 			lock.Waiter({
 				until: function() {
 					return true;
 				},
 				then: function() {
-					jsh.shell.echo("Setting scope for " + this + t());
 					self.scope = scope;
 				}
 			})();
-			jsh.shell.echo("Waiting until end received: " + this + t());
-			lock.Waiter({
-				until: function() {
-					return ended;
-				},
-				then: function() {					
-				}
-			})();
+			var more = true;
+			while(more) {
+				lock.Waiter({
+					until: function() {
+						return Boolean(queued.length) || done;
+					},
+					then: function() {
+						if (done) {
+							more = false;
+							return;
+						}
+						var action = queued.shift();
+						if (action.start) {
+							var scenario = new Scenario(action.start.scenario);
+							scope.scenario(scenario);
+						} else if (action.test) {
+							scope.test(action.test);
+						} else if (action.end) {
+							more = false;
+						}
+					}
+				})();
+			}
+//			lock.Waiter({
+//				until: function() {
+//					if (queued.length && stack[stack.length-1] == self) {
+//						var action = queued.shift();
+//						if (action.start) {
+//							jsh.shell.echo("Processing start: " + self + t());
+//							var scenario = new Scenario(action.start.scenario);
+////							stack.push(scenario);
+//							scope.scenario(scenario);							
+//						} else if (action.test) {
+//							jsh.shell.echo("Processing test: " + self + t());
+//							scope.test(action.test);
+//						} else if (action.end) {
+//							jsh.shell.echo("Processing end: " + self + t());
+//							ended = true;
+//						} else {
+//							jsh.shell.echo("Action: " + JSON.stringify(action));
+//						}
+//					} else if (!ended) {
+//						if (!queued.length) {
+//							jsh.shell.echo("Nothing queued: " + self + t());
+//						} else {
+//							jsh.shell.echo("Not target: " + self + t() + " target is " + stack[stack.length-1]);
+//						}
+//					} else {
+//						jsh.shell.echo("Ended already: " + self);
+//					}
+//					return ended;
+//				},
+//				then: function() {
+//					var last = stack.pop();
+//					jsh.shell.echo("Popped " + last + " off stack; self = " + self);
+//					jsh.shell.echo("New last = " + stack[stack.length-1]);
+//					if (last != self) {
+//						jsh.shell.echo("Warning: last " + last + " self " + self);
+//					}
+//				}
+//			})();
+//			lock.Waiter({
+//				until: function() {
+//					return true;
+//				},
+//				then: function() {
+//					
+//				}
+//			})();
+//			jsh.shell.echo("ended: " + this + t() + " with top of stack " + stack[stack.length-1]);
 		}
 	};
 	
 	var top = new Scenario({
 		name: p.name
-	});
+	},true);
 	
 	this.top = top;
 	
@@ -94,33 +170,15 @@ $exports.Parent = function(p) {
 		p.stream.character().readLines(function(line) {
 			lock.Waiter({
 				until: function() {
-					jsh.shell.echo("Trying to proceed with line" + t());
+//					jsh.shell.echo("Trying to proceed with line" + t());
 					return Boolean(top.scope);
 				},
 				then: function() {
-					jsh.shell.echo("Got line: " + line + t());
+//					jsh.shell.echo("Got line: " + line + t());
 					try {
 						if (line.substring(0,1) == "{") {
 							var json = JSON.parse(line);
-							var last = (stack.length) ? stack[stack.length-1] : null;
-							if (json.start) {
-								jsh.shell.echo("start" + t());
-								if (last) {
-									var scenario = new Scenario(json.start.scenario);
-									stack.push(scenario);
-									throw new Error("This has to be in separate thread, you idiot")
-									last.scope.scenario(scenario);
-								} else {
-									stack.push(top);
-								}
-								jsh.shell.echo("processed start" + t())
-							} else if (json.end) {
-								jsh.shell.echo("end");
-								stack.pop().end();
-							} else if (json.test) {
-								jsh.shell.echo("test");
-								last.scope.test(json.test);
-							}
+							queue(json);
 						}
 					} catch (e) {
 						Packages.java.lang.System.err.println(e);
@@ -129,5 +187,13 @@ $exports.Parent = function(p) {
 				}
 			})();
 		});
+		lock.Waiter({
+			until: function() {
+				return true;
+			},
+			then: function() {
+				top.done();
+			}
+		})();
 	});
 }
