83be890befe9ac5a957d6cf1cb0d33b40aaa7ea0
David P. Caldwell <david@davidpcaldwell.com>
2015-08-18 09:19:06 -0400

    Improve documentation presentation by using client-side manipulation

--- jsh/etc/documentation.jsh.js
+++ jsh/etc/documentation.jsh.js
@@ -11,13 +11,27 @@
 //	Contributor(s):
 //	END LICENSE
 
-var tomcat = jsh.httpd.Tomcat.serve({
-	directory: jsh.script.file.parent.parent.parent
+var parameters = jsh.script.getopts({
+	options: {
+		"chrome:profile": jsh.file.Pathname,
+		port: Number
+	}
 });
+
+var tomcat = jsh.httpd.Tomcat.serve({
+	directory: jsh.script.file.parent.parent.parent,
+	port: parameters.options.port
+});
+
 if (jsh.shell.browser.chrome) {
-	var tmp = jsh.shell.TMPDIR.createTemporary({ directory: true });
-	tmp.getRelativePath("First Run").write("", { append: false });
-	var user = new jsh.shell.browser.chrome.User({ directory: tmp });
+	var profile = (parameters.options["chrome:profile"]) ? parameters.options["chrome:profile"].createDirectory({
+		ifExists: function() {
+			return false;
+		}
+	}) : jsh.shell.TMPDIR.createTemporary({ directory: true });
+	jsh.shell.echo("Using Chrome profile " + profile + " ...");
+	profile.getRelativePath("First Run").write("", { append: false });
+	var user = new jsh.shell.browser.chrome.User({ directory: profile });
 	user.run({ uri: "http://127.0.0.1:" + tomcat.port + "/jsh/etc/api.html" });
 } else {
 	jsh.shell.echo("Tomcat started on port " + tomcat.port + " ...");
