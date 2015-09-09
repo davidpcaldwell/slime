0f28d4e44224c2b2691d4d88b72aaa8d63b8968b
David P. Caldwell <david@davidpcaldwell.com>
2015-09-09 18:45:17 -0400

    More thoroughly implement GIT_ASKPASS behaviors

---
+++ rhino/tools/git/askpass.jsh.js
@@ -0,0 +1,56 @@
+//	TODO	come up with a test suite and documentation for this
+var parameters = jsh.script.getopts({
+	options: {
+		//	Determines which of the two modes of operation will be used for this script (see below)
+		child: false,
+		
+		//	Without the -child argument, the -script argument is used to specify the location of an executable script which will be
+		//	produced and will be suitable for use as the value of GIT_ASKPASS
+		script: jsh.file.Pathname,
+		
+		//	With the -child argument, the -prompt argument is expected, which provides a prompt that will be shown to the user
+		prompt: String
+	},
+	unhandled: jsh.script.getopts.UNEXPECTED_OPTION_PARSER.SKIP
+});
+
+if (!parameters.options.child) {
+	var INVOKE_JSH = (function() {
+		if (jsh.shell.jsh.home) {
+			return [
+				jsh.shell.java.jrunscript,
+				jsh.shell.jsh.home.getRelativePath("jsh.js")
+			];
+		}
+		return [
+			jsh.shell.java.jrunscript,
+			jsh.shell.jsh.src.getRelativePath("rhino/jrunscript/api.js"),
+			"jsh"
+		];
+	})();
+	var script = jsh.script.file.parent.getFile("askpass.bash").read(String);
+	script = script.replace(/\#INVOKE_JSH\=/, "INVOKE_JSH=\"" + INVOKE_JSH.join(" ") + "\"");
+	script = script.replace(/\#ASKPASS_JSH_PATH\=/, "ASKPASS_JSH_PATH=\"" + jsh.script.file + "\"");
+	var lines = script.split("\n");
+	var SCRIPT = lines.join("\n");
+	//	TODO	do not hard-code bash path
+	lines.shift("#!/bin/bash");
+	parameters.options.script.write(SCRIPT, { append: false });
+	jsh.shell.run({
+		command: "chmod",
+		arguments: ["+x",parameters.options.script]
+	});
+} else {
+	var api = jsh.loader.file(jsh.script.file.parent.parent.getRelativePath("askpass.js"), {
+		api: {
+			java: jsh.java
+		}
+	});
+	var nomask = /^Username/.test(parameters.options.prompt);
+	var typed = api.gui({
+		nomask: nomask,
+		prompt: parameters.options.prompt
+	});
+	jsh.shell.stdout.write(typed + "\n");
+	jsh.shell.exit(0);
+}
