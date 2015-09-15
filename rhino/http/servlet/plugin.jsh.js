e7037802bdbf7bf526f11daef121f5e5d1e8e030
David P. Caldwell <david@davidpcaldwell.com>
2015-09-15 16:24:10 -0400

    Reorganize jsh servlet plugin

--- rhino/http/servlet/plugin.jsh.js
+++ rhino/http/servlet/plugin.jsh.js
@@ -20,9 +20,15 @@ plugin({
 			jsh.httpd = {};
 		}
 
-		var getMimeType = $loader.file("jsh.mime.js", {
-			jsh: jsh
-		}).getMimeType;
+		var getMimeType = function(file) {
+			var type = jsh.io.mime.Type.guess({
+				name: file.pathname.basename
+			});
+			if (!type && /\.js$/.test(file.pathname.basename)) {
+				type = new jsh.io.mime.Type("application", "javascript");
+			}
+			return type;
+		};
 
 		jsh.httpd.nugget = {};
 		jsh.httpd.nugget.getMimeType = getMimeType;
@@ -37,13 +43,173 @@ plugin({
 		})();
 
 		//	TODO	allow system property in addition to environment variable?
-		$loader.module("jsh.tomcat.js", {
-			$java: $jsh,
-			jsh: jsh,
-			CATALINA_HOME: CATALINA_HOME
-		});
+		var TOMCAT_CLASS = (function() {
+			var TOMCAT_CLASS = jsh.java.getClass("org.apache.catalina.startup.Tomcat");
+			if (!TOMCAT_CLASS && CATALINA_HOME) {
+				[
+					"bin/tomcat-juli.jar", "lib/servlet-api.jar", "lib/tomcat-util.jar", "lib/tomcat-api.jar", "lib/tomcat-coyote.jar",
+					"lib/catalina.jar"
+					,"lib/annotations-api.jar"
+				].forEach(function(path) {
+					jsh.loader.java.add(CATALINA_HOME.getRelativePath(path));
+				});
+				TOMCAT_CLASS = jsh.java.getClass("org.apache.catalina.startup.Tomcat");
+			}
+			return TOMCAT_CLASS;
+		})();
+		
+		jsh.java.log.named("jsh.httpd").CONFIG("When trying to load Tomcat: class = %s CATALINA_HOME = %s", TOMCAT_CLASS, CATALINA_HOME);
+		
+		if (TOMCAT_CLASS) {
+			jsh.httpd.Tomcat = function(p) {
+				var tomcat = new Packages.org.apache.catalina.startup.Tomcat();
 
-		if (jsh.httpd.Tomcat) {
+				var base = (p.base) ? p.base : jsh.shell.TMPDIR.createTemporary({ directory: true, prefix: "tomcat" });
+
+				this.base = base;
+
+				var port = (p.port) ? p.port : (function() {
+					var address = new Packages.java.net.ServerSocket(0);
+					var rv = address.getLocalPort();
+					address.close();
+					return rv;
+				})();
+
+				this.port = port;
+
+				tomcat.setBaseDir(base);
+				tomcat.setPort(port);
+
+				var api = {
+					js: jsh.js,
+					java: jsh.java,
+					io: jsh.io
+				}
+				var server = $loader.file("server.js", {
+					api: api
+				});
+
+				this.map = function(m) {
+					if (typeof(m.path) == "string" && m.servlets) {
+						var context = tomcat.addContext(m.path, base.pathname.java.adapt().getCanonicalPath());
+						var id = 0;
+						for (var pattern in m.servlets) {
+							//	TODO	below may not work if more than one servlet; value changes during loop and may be picked up
+							//			by servlets earlier in loop
+							var servletDeclaration = m.servlets[pattern];
+		//					if (!servletDeclaration.file) {
+		//						throw new Error("Incorrect launch.");
+		//					}
+							var servletName = "slime" + String(id++);
+							var servlet = Packages.org.apache.catalina.startup.Tomcat.addServlet(context,servletName,new JavaAdapter(
+								Packages.javax.servlet.http.HttpServlet,
+								new function() {
+									//	TODO	could use jsh.io here
+									var servlet;
+
+									this.init = function() {
+										var apiScope = {
+											$host: new function() {
+												this.parameters = (servletDeclaration.parameters) ? servletDeclaration.parameters : {};
+
+												if (m.resources) {
+													if (!m.resources.loader) throw new Error("No m.resources.loader");
+												}
+
+												var script = (function() {
+													if (m.resources) {
+														if (servletDeclaration.$loader) {
+															return servletDeclaration.$loader;
+														} else if (servletDeclaration.file) {
+															return new jsh.file.Loader({
+																directory: servletDeclaration.file.parent,
+																type: getMimeType
+															});
+														} else {
+															throw new Error("Unimplemented: servletDeclaration without file property");
+														}
+													}
+													if (servletDeclaration.file) {
+														return new jsh.file.Loader({ directory: servletDeclaration.file.parent });
+													}
+												})();
+
+												this.loaders = {
+													script: script,
+													container: (m.resources) ? m.resources.loader : null
+												};
+
+												this.getCode = function(scope) {
+													if (servletDeclaration.load) {
+														servletDeclaration.load(scope);
+													} else {
+														jsh.loader.run(servletDeclaration.file.pathname, scope);
+													}
+												}
+
+												this.$java = $jsh;
+
+												this.$exports = {};
+												this.server = server;
+												this.api = api;
+											}
+										};
+										$loader.run("api.js", apiScope);
+										servlet = apiScope.$host.$exports.servlet;
+									};
+
+									this.service = function(_request,_response) {
+										servlet.service(_request,_response);
+									}
+
+									this.destroy = function() {
+										servlet.destroy();
+									}
+								}
+							));
+							context.addServletMapping(pattern,servletName);
+						}
+					} else if (typeof(m.path) == "string" && m.webapp) {
+						tomcat.getEngine().setParentClassLoader(tomcat.getEngine().getClass().getClassLoader());
+						var context = tomcat.addWebapp(m.path, m.webapp.java.adapt().getCanonicalPath());
+						jsh.shell.echo("Added " + context);
+					}
+				}
+
+				//	TODO	are both start() and run() needed?
+
+				var started = false;
+
+				this.start = function() {
+					if (!started) {
+						tomcat.start();
+						started = true;
+					}
+				}
+
+				this.run = function() {
+					if (!started) {
+						tomcat.start();
+						started = true;
+					}
+					var run = function() {
+						tomcat.getServer().await();
+						started = false;
+					};
+					var fork = false;
+					if (fork) {
+						jsh.java.Thread.start({ call: run });
+					} else {
+						run();
+					}
+				}
+
+				this.stop = function() {
+					tomcat.stop();
+					started = false;
+				}
+			};
+			
 			jsh.httpd.Tomcat.serve = function(p) {
 				var loader = new jsh.file.Loader({ directory: p.directory });
 				var getMimeType = function(path) {
