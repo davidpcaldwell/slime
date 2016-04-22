e983cb3bcf90709592fe352747721935ddcb8593
David P. Caldwell <david@davidpcaldwell.com>
2016-04-22 10:19:31 -0400

    Load browser unit testing framework as a module

--- loader/browser/test/module.js
+++ loader/browser/test/module.js
@@ -172,337 +172,334 @@ var Tests = function() {
 	};
 };
 
-var unit = new function() {
-	var target;
-	var global = new Tests();
+var target;
+var global = new Tests();
+
+$exports.target = function(page) {
+	global.target(page);
+	target = page;
+};
 	
-	this.target = function(page) {
-		global.target(page);
-		target = page;
-	}
-	
-	var suite;
-	
-	this.Tests = function() {
-		Tests.call(this);
+var suite;
+
+$exports.Tests = function() {
+	Tests.call(this);
+};
+
+$exports.Scenario = function() {
+	Scenario.call(this,{ target: target });
+}
+
+var getStructure = function(part) {
+	var rv = {
+		id: part.id,
+		name: part.name
 	};
-
-	this.Scenario = function() {
-		Scenario.call(this,{ target: target });
+	if (part.parts) {
+		var parts = part.parts;
+		rv.parts = {};
+		for (var x in parts) {
+			rv.parts[x] = getStructure(parts[x]);
+		}
 	}
+	return rv;
+};
 
-	var getStructure = function(part) {
-		var rv = {
-			id: part.id,
-			name: part.name
+$exports.structure = function() {
+	if (!suite) {
+		return {
+			name: "Tests",
+			old: true
 		};
-		if (part.parts) {
-			var parts = part.parts;
-			rv.parts = {};
-			for (var x in parts) {
-				rv.parts[x] = getStructure(parts[x]);
-			}
-		}
-		return rv;
-	};
-
-	this.structure = function() {
-		if (!suite) {
-			return {
-				name: "Tests",
-				old: true
-			};
-		}
-		return getStructure(suite);
 	}
+	return getStructure(suite);
+}
 
-	this.suite = function() {
-		suite = arguments[0];
-	}
-	
-	this.test = function(test) {
-		global.test(test);
-	}
+$exports.suite = function() {
+	suite = arguments[0];
+}
 
-	//	TODO	this is used by the unit test button to run the tests; could encapsulate
-	this.run = function(_callbacks) {
-		if (suite) {
-			suite.listeners.add("scenario", function(e) {
+$exports.test = function(test) {
+	global.test(test);
+}
+
+//	TODO	this is used by the unit test button to run the tests; could encapsulate
+$exports.run = function(_callbacks) {
+	if (suite) {
+		suite.listeners.add("scenario", function(e) {
 //				_callbacks.fire(e);
-				console.log("scenario", Object.keys(e.detail).join(","), e);
-				_callbacks.event(e);
-			});
-			suite.listeners.add("test", function(e) {
-				console.log("test", Object.keys(e.detail).join(","), e.detail.message, e);
+			console.log("scenario", Object.keys(e.detail).join(","), e);
+			_callbacks.event(e);
+		});
+		suite.listeners.add("test", function(e) {
+			console.log("test", Object.keys(e.detail).join(","), e.detail.message, e);
 //				_callbacks.fire(e);
-				_callbacks.event(e);
-			});
-			suite.run({},function(success) {
-				console.log("success = " + success);
-				_callbacks.end(success);
-			});
-		} else {
-			if (!_callbacks) throw new Error("Missing callbacks!");
-			global.listeners.add("console", function(e) {
-				window.console.log(e.detail);
-			});
-			global.listeners.add("test", function(e) {
-				if (_callbacks.event) _callbacks.event(e);
-				_callbacks.log(e.detail.success, e.detail.message);
-			});
-			global.listeners.add("end", function(e) {
-				_callbacks.end(e.detail);
-				if (_callbacks.after) {
-					//	TODO	deprecated; should combine after() with end(success)
-					debugger;
-					_callbacks.after();
-				}
-			});
-			global.listeners.add("log", function(e) {
-				_callbacks.log(e.detail.success, e.detail.message);
-			});
-			global.run();
+			_callbacks.event(e);
+		});
+		suite.run({},function(success) {
+			console.log("success = " + success);
+			_callbacks.end(success);
+		});
+	} else {
+		if (!_callbacks) throw new Error("Missing callbacks!");
+		global.listeners.add("console", function(e) {
+			window.console.log(e.detail);
+		});
+		global.listeners.add("test", function(e) {
+			if (_callbacks.event) _callbacks.event(e);
+			_callbacks.log(e.detail.success, e.detail.message);
+		});
+		global.listeners.add("end", function(e) {
+			_callbacks.end(e.detail);
+			if (_callbacks.after) {
+				//	TODO	deprecated; should combine after() with end(success)
+				debugger;
+				_callbacks.after();
+			}
+		});
+		global.listeners.add("log", function(e) {
+			_callbacks.log(e.detail.success, e.detail.message);
+		});
+		global.run();
+	}
+};
+
+$exports.nugget = new function() {
+	var addLoadHook;
+	if (window.addEventListener) {
+		addLoadHook = function(f) {
+			window.addEventListener("load", f, false);
 		}
-	};
+	} else if (window.attachEvent) {
+		addLoadHook = function(f) {
+			window.attachEvent("onload", f);
+		}
+	}
 
-	this.nugget = new function() {
-		var addLoadHook;
-		if (window.addEventListener) {
-			addLoadHook = function(f) {
-				window.addEventListener("load", f, false);
-			}
-		} else if (window.attachEvent) {
-			addLoadHook = function(f) {
-				window.attachEvent("onload", f);
+	this.addLoadHook = addLoadHook;		
+};
+
+$exports.fire = new function() {
+	var Event = function(name,canBubble,cancelable) {
+		this.name = name;
+		this.canBubble = canBubble;
+		this.cancelable = cancelable;
+
+		this.set = function(p) {
+			if (!p) return;
+			for (var x in p) {
+				this[x] = p[x];
 			}
 		}
 
-		this.addLoadHook = addLoadHook;		
-	};
+		this.create = function() {
+			var rv = document.createEvent("Event");
+			var v = this;
+			rv.initEvent(v.name,v.canBubble,v.cancelable);
+			return rv;
+		}
+	}
 
-	this.fire = new function() {
-		var Event = function(name,canBubble,cancelable) {
-			this.name = name;
-			this.canBubble = canBubble;
-			this.cancelable = cancelable;
+	var UIEvent = function(name,canBubble,cancelable) {
+		Event.call(this,name,canBubble,cancelable);
+		this.view = window;
+		this.detail = null;		
+	}
 
-			this.set = function(p) {
-				if (!p) return;
-				for (var x in p) {
-					this[x] = p[x];
-				}
+	var MouseEvent = function(name,canBubble,cancelable) {
+		UIEvent.call(this,name,canBubble,cancelable)
+		this.screenX = 0;
+		this.screenY = 0;
+		this.clientX = 0;
+		this.clientY = 0;
+		this.ctrlKey = false;
+		this.altKey = false;
+		this.shiftKey = false;
+		this.metaKey = false;
+		this.button = 0;
+		this.relatedTarget = null;
+
+		this.set = function(p) {
+			if (!p) return;
+			for (var x in p) {
+				this[x] = p[x];
 			}
+		}
 
-			this.create = function() {
-				var rv = document.createEvent("Event");
+		this.create = function() {
+			if (document.createEvent) {
+				var rv = document.createEvent("MouseEvent");
 				var v = this;
-				rv.initEvent(v.name,v.canBubble,v.cancelable);
+				rv.initMouseEvent(
+					v.name,v.canBubble,v.cancelable,v.view,v.detail,v.screenX,v.screenY,v.clientX,v.clientY,v.ctrlKey,v.altKey,
+					v.shiftKey,v.metaKey,v.button,v.relatedTarget
+				);
+				return rv;
+			} else {
+				var rv = document.createEventObject();
+				if (false) {
+					for (var x in this) {
+						if (typeof(x) != "function") {
+							rv[x] = this[x];
+						}
+					}
+				}
 				return rv;
 			}
 		}
-
-		var UIEvent = function(name,canBubble,cancelable) {
-			Event.call(this,name,canBubble,cancelable);
-			this.view = window;
-			this.detail = null;		
-		}
-
-		var MouseEvent = function(name,canBubble,cancelable) {
-			UIEvent.call(this,name,canBubble,cancelable)
-			this.screenX = 0;
-			this.screenY = 0;
-			this.clientX = 0;
-			this.clientY = 0;
-			this.ctrlKey = false;
-			this.altKey = false;
-			this.shiftKey = false;
-			this.metaKey = false;
-			this.button = 0;
-			this.relatedTarget = null;
-
-			this.set = function(p) {
-				if (!p) return;
-				for (var x in p) {
-					this[x] = p[x];
-				}
-			}
-
-			this.create = function() {
-				if (document.createEvent) {
-					var rv = document.createEvent("MouseEvent");
-					var v = this;
-					rv.initMouseEvent(
-						v.name,v.canBubble,v.cancelable,v.view,v.detail,v.screenX,v.screenY,v.clientX,v.clientY,v.ctrlKey,v.altKey,
-						v.shiftKey,v.metaKey,v.button,v.relatedTarget
-					);
-					return rv;
-				} else {
-					var rv = document.createEventObject();
-					if (false) {
-						for (var x in this) {
-							if (typeof(x) != "function") {
-								rv[x] = this[x];
-							}
-						}
-					}
-					return rv;
-				}
-			}
-		}
-
-		var KeyEvent = function(name,canBubble,cancelable) {
-			UIEvent.call(this,name,canBubble,cancelable);
-			this["char"] = null;
-			this.key = null;
-			this.location = 0;
-			this.ctrlKey = false;
-			this.altKey = false;
-			this.shiftKey = false;
-			this.metaKey = false;
-			this.repeat = false;
-			this.locale = null;
-
-			this.create = function() {
-				if (document.createEvent) {
-					var rv = document.createEvent("KeyboardEvent");
-					var v = this;
-					var modifiers = [];
-					if (this.ctrlKey) modifiers.push("Control");
-					if (this.altKey) modifiers.push("Alt");
-					if (this.shiftKey) modifiers.push("Shift");
-					if (this.metaKey) modifiers.push("Meta");
-					rv.initKeyboardEvent(
-						v.name,v.canBubble,v.cancelable,v.view,v["char"],v.key,v.location,modifiers.join(" "),v.repeat,v.locale
-					);
-					return rv;
-				} else {
-					throw new Error("Unimplemented: browser lacks createEvent for KeyboardEvent");
-				}
-			}
-		};
-
-		var eventFunction = function(name,constructor,properties) {
-			return function(element,p) {
-				if (!element) throw new Error("Cannot dispatch " + name + " because specified element is " + element);
-				if (element.disabled) debugger;
-				var v = new constructor(name, properties.bubbles, properties.cancelable);
-				for (var x in properties) {
-					v[x] = properties[x];
-				}
-				v.set(p);
-				if (element.dispatchEvent) {
-					element.dispatchEvent(v.create());
-				} else if (element.fireEvent) {
-					element.fireEvent("on" + name, v.create());
-				}			
-			}
-		};
-
-		this.click = eventFunction("click",MouseEvent,{
-			bubbles: true,
-			cancelable: true,
-			//	TODO	should detail be 1?
-			detail: 0
-		});
-
-		this.mousedown = function(element,p) {
-			if (element.disabled) debugger;
-			var v = new MouseEvent("mousedown", true, true);
-			v.detail = 0;
-			v.set(p);
-			element.dispatchEvent(v.create());
-		}
-
-		this.mouseup = function(element,p) {
-			if (element.disabled) debugger;
-			var v = new MouseEvent("mouseup", true, true);
-			v.detail = 0;
-			v.set(p);
-			element.dispatchEvent(v.create());
-		}
-
-		this.change = function(element,p) {
-			if (element.disabled) debugger;
-			var v = new Event("change", true, false);
-			v.set(p);
-			element.dispatchEvent(v.create());
-		};
-		
-		this.input = function(element,p) {
-			if (element.disabled) debugger;
-			var v = new Event("input", true, false);
-			v.set(p);
-			element.dispatchEvent(v.create());
-		}
-
-		this.keydown = function(element,p) {
-			if (element.disabled) debugger;
-			var v = new KeyEvent("keydown",true,true);
-			v.set(p);
-			element.dispatchEvent(v.create());
-		};
-
-		this.keypress = function(element,p) {
-			if (element.disabled) debugger;
-			var v = new KeyEvent("keypress",true,true);
-			v.set(p);
-			element.dispatchEvent(v.create());
-		};
-	};
-		
-	this.ui = function() {
-		var unit = this;
-
-		var loadUnitTests = function() {
-			var testing = document.createElement("div");
-			var heading = document.createElement("h1");
-			heading.appendChild(document.createTextNode("Unit Tests"));
-			testing.appendChild(heading);
-			var results = document.createElement("table");
-			testing.appendChild(results);
-			var resultsHead = results.createTHead();
-			var resultsHeadRow = resultsHead.insertRow(-1);
-			var resultsResultTh = resultsHeadRow.insertCell(-1);
-			resultsResultTh.appendChild(document.createTextNode("Result"));
-			var resultsMessageTh = resultsHeadRow.insertCell(-1);
-			resultsMessageTh.appendChild(document.createTextNode("Message"));
-
-			var unitButton = document.createElement("input");
-			unitButton.setAttribute("type", "button");
-			unitButton.setAttribute("value", "Unit tests");
-			document.body.insertBefore(unitButton,document.body.firstChild);
-			unitButton.onclick = function(e) {
-				document.body.insertBefore(testing,document.body.firstChild);
-				unit.run(new function() {
-					var addRow = function(b,message) {
-						var tbody = results.tBodies[0];
-						if (!tbody) {
-							results.appendChild(document.createElement("tbody"));
-							tbody = results.tBodies[0];
-						}
-						var index = (tbody.rows.length == 0) ? 0 : tbody.rows.length-1;
-						var row = tbody.insertRow(index);
-						var result = row.insertCell(-1);
-						result.appendChild(document.createTextNode( (b) ? "Success" : "Failure" ));
-						var messageTd = row.insertCell(-1);
-						messageTd.appendChild(document.createTextNode( message ));
-						return row;
-					}
-					var last = addRow(false,"Incomplete");
-					this.log = function(b,message) {
-						addRow(b,message);
-					}
-					//	TODO	combine these two methods
-					this.end = function(b) {
-						addRow(b,"OVERALL");
-						results.deleteRow(results.rows.length-1);
-						unitButton.parentNode.removeChild(unitButton);
-					}
-				});
-			}
-		};
-
-		loadUnitTests();
 	}
+
+	var KeyEvent = function(name,canBubble,cancelable) {
+		UIEvent.call(this,name,canBubble,cancelable);
+		this["char"] = null;
+		this.key = null;
+		this.location = 0;
+		this.ctrlKey = false;
+		this.altKey = false;
+		this.shiftKey = false;
+		this.metaKey = false;
+		this.repeat = false;
+		this.locale = null;
+
+		this.create = function() {
+			if (document.createEvent) {
+				var rv = document.createEvent("KeyboardEvent");
+				var v = this;
+				var modifiers = [];
+				if (this.ctrlKey) modifiers.push("Control");
+				if (this.altKey) modifiers.push("Alt");
+				if (this.shiftKey) modifiers.push("Shift");
+				if (this.metaKey) modifiers.push("Meta");
+				rv.initKeyboardEvent(
+					v.name,v.canBubble,v.cancelable,v.view,v["char"],v.key,v.location,modifiers.join(" "),v.repeat,v.locale
+				);
+				return rv;
+			} else {
+				throw new Error("Unimplemented: browser lacks createEvent for KeyboardEvent");
+			}
+		}
+	};
+
+	var eventFunction = function(name,constructor,properties) {
+		return function(element,p) {
+			if (!element) throw new Error("Cannot dispatch " + name + " because specified element is " + element);
+			if (element.disabled) debugger;
+			var v = new constructor(name, properties.bubbles, properties.cancelable);
+			for (var x in properties) {
+				v[x] = properties[x];
+			}
+			v.set(p);
+			if (element.dispatchEvent) {
+				element.dispatchEvent(v.create());
+			} else if (element.fireEvent) {
+				element.fireEvent("on" + name, v.create());
+			}			
+		}
+	};
+
+	this.click = eventFunction("click",MouseEvent,{
+		bubbles: true,
+		cancelable: true,
+		//	TODO	should detail be 1?
+		detail: 0
+	});
+
+	this.mousedown = function(element,p) {
+		if (element.disabled) debugger;
+		var v = new MouseEvent("mousedown", true, true);
+		v.detail = 0;
+		v.set(p);
+		element.dispatchEvent(v.create());
+	}
+
+	this.mouseup = function(element,p) {
+		if (element.disabled) debugger;
+		var v = new MouseEvent("mouseup", true, true);
+		v.detail = 0;
+		v.set(p);
+		element.dispatchEvent(v.create());
+	}
+
+	this.change = function(element,p) {
+		if (element.disabled) debugger;
+		var v = new Event("change", true, false);
+		v.set(p);
+		element.dispatchEvent(v.create());
+	};
+
+	this.input = function(element,p) {
+		if (element.disabled) debugger;
+		var v = new Event("input", true, false);
+		v.set(p);
+		element.dispatchEvent(v.create());
+	}
+
+	this.keydown = function(element,p) {
+		if (element.disabled) debugger;
+		var v = new KeyEvent("keydown",true,true);
+		v.set(p);
+		element.dispatchEvent(v.create());
+	};
+
+	this.keypress = function(element,p) {
+		if (element.disabled) debugger;
+		var v = new KeyEvent("keypress",true,true);
+		v.set(p);
+		element.dispatchEvent(v.create());
+	};
 };
-$set(unit);
+
+$exports.ui = function() {
+	var unit = this;
+
+	var loadUnitTests = function() {
+		var testing = document.createElement("div");
+		var heading = document.createElement("h1");
+		heading.appendChild(document.createTextNode("Unit Tests"));
+		testing.appendChild(heading);
+		var results = document.createElement("table");
+		testing.appendChild(results);
+		var resultsHead = results.createTHead();
+		var resultsHeadRow = resultsHead.insertRow(-1);
+		var resultsResultTh = resultsHeadRow.insertCell(-1);
+		resultsResultTh.appendChild(document.createTextNode("Result"));
+		var resultsMessageTh = resultsHeadRow.insertCell(-1);
+		resultsMessageTh.appendChild(document.createTextNode("Message"));
+
+		var unitButton = document.createElement("input");
+		unitButton.setAttribute("type", "button");
+		unitButton.setAttribute("value", "Unit tests");
+		document.body.insertBefore(unitButton,document.body.firstChild);
+		unitButton.onclick = function(e) {
+			document.body.insertBefore(testing,document.body.firstChild);
+			unit.run(new function() {
+				var addRow = function(b,message) {
+					var tbody = results.tBodies[0];
+					if (!tbody) {
+						results.appendChild(document.createElement("tbody"));
+						tbody = results.tBodies[0];
+					}
+					var index = (tbody.rows.length == 0) ? 0 : tbody.rows.length-1;
+					var row = tbody.insertRow(index);
+					var result = row.insertCell(-1);
+					result.appendChild(document.createTextNode( (b) ? "Success" : "Failure" ));
+					var messageTd = row.insertCell(-1);
+					messageTd.appendChild(document.createTextNode( message ));
+					return row;
+				}
+				var last = addRow(false,"Incomplete");
+				this.log = function(b,message) {
+					addRow(b,message);
+				}
+				//	TODO	combine these two methods
+				this.end = function(b) {
+					addRow(b,"OVERALL");
+					results.deleteRow(results.rows.length-1);
+					unitButton.parentNode.removeChild(unitButton);
+				}
+			});
+		}
+	};
+
+	loadUnitTests();
+}
