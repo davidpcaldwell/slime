9c2aa70ed37b6bdfa6ab77abc8a5727cf49674ab
David P. Caldwell <david@davidpcaldwell.com>
2018-05-04 21:33:01 -0400

    Remove "local" directory from shell and httpd war builds

--- rhino/http/servlet/test/resource/1.hg.js
+++ rhino/http/servlet/test/resource/1.hg.js
@@ -11,4 +11,34 @@
 //	Contributor(s):
 //	END LICENSE
 
-map("WEB-INF/slime/", $mapping.getRelativePath("../../../../.."));
+var slime = new jsh.file.Loader({ 
+	directory: $mapping.getRelativePath("../../../../..").directory
+});
+
+var NoLocalSource = function(was) {
+	this.toString = function() {
+		return "NoLocalSource: [" + was.toString() + "]";
+	};
+
+	this.get = function(path) {
+		return was.get(path);
+	};
+
+	this.list = function(prefix) {
+		var rv = was.list(prefix);
+		if (!prefix) rv = rv.filter(function(item) {
+			return item.path != "local";
+		});
+		rv = rv.filter(function(item) {
+			return item.path != ".hg";
+		});
+		return rv;
+	}
+};
+
+var loader = new jsh.io.Loader(new NoLocalSource(slime.source));
+
+add({
+	prefix: "WEB-INF/slime/", 
+	loader: loader
+});
