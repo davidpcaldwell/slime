bc4d110a942e4c4d5acf4a12e9f8ba2c9c36aed4
David P. Caldwell <david@davidpcaldwell.com>
2018-07-07 04:41:40 -0400

    Add ability to run node binaries

--- rhino/tools/node/module.js
+++ rhino/tools/node/module.js
@@ -3,6 +3,22 @@ $exports.Installation = function(o) {
 		return "Node installation at " + o.directory;
 	};
 	
+	var PATH = (function() {
+		var elements = jsh.shell.PATH.pathnames.slice();
+		elements.push(o.directory.getRelativePath("bin"));
+		return jsh.file.Searchpath(elements);		
+	})();
+	
+	this.run = function(p) {
+		jsh.shell.run({
+			command: p.project.getFile("node_modules/.bin/" + p.command),
+			arguments: p.arguments,
+			environment: jsh.js.Object.set({}, jsh.shell.environment, p.environment, {
+				PATH: PATH.toString()
+			})
+		});
+	}
+	
 	this.npm = function(p) {
 		var elements = jsh.shell.PATH.pathnames.slice();
 		elements.push(o.directory.getRelativePath("bin"));
@@ -14,7 +30,7 @@ $exports.Installation = function(o) {
 				rv.push.apply(rv,p.arguments);
 				return rv;
 			})([]),
-			environment: jsh.js.Object.set({}, jsh.shell.environment, {
+			environment: jsh.js.Object.set({}, jsh.shell.environment, p.environment, {
 				PATH: PATH.toString()
 			}),
 			directory: p.project
