ef2069df3fb22d84bac49032fbef823f35f1b8cc
David P. Caldwell <david@davidpcaldwell.com>
2018-11-25 11:25:38 -0500

    resolves {Bitbucket issue: Integrate jrunscript/io grid tests into test suite} by integrating jsh.io.grid tests into test suite ... adds test to correctly dertermine number of sheets in .xlsx workbook

--- jrunscript/io/grid.js
+++ jrunscript/io/grid.js
@@ -2,20 +2,27 @@ var excel;
 
 var Excel = function() {
 	var Workbook = function(_workbook) {
+		Packages.java.lang.System.err.println("_workbook = " + _workbook);
+		this.sheets = {};
+		
+		Object.defineProperty(this.sheets, "count", {
+			get: function() {
+				return _workbook.getNumberOfSheets();
+			},
+			enumerable: true
+		});
 	}
 	
 	var format = {};
 	
 	format.xls = function(p) {
-		var _workbook = new Packages.org.apache.poi.hssf.usermodel.HSSFWorkbook(
+		return new Packages.org.apache.poi.hssf.usermodel.HSSFWorkbook(
 			p.resource.read($context.Streams.binary).java.adapt()
 		);
-		return new Workbook(_workbook);
 	}
 	
 	format.xlsx = function(p) {
-		var _workbook = new Packages.org.apache.poi.xssf.usermodel.XSSFWorkbook(p.resource.read($context.Streams.binary).java.adapt());
-		return new Workbook(_workbook);
+		return new Packages.org.apache.poi.xssf.usermodel.XSSFWorkbook(p.resource.read($context.Streams.binary).java.adapt());
 	};
 	
 	this.format = format;
--- jrunscript/io/test/grid/suite.bash
+++
@@ -1,4 +0,0 @@
-set -x
-BASE=$(dirname $0)
-SLIME=${BASE}/../../../..
-$SLIME/jsh.bash $BASE/suite.jsh.js "$@"
