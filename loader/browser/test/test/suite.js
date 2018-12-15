c8dec857f28572d454d8a4b5c81dfcc03deefe9f
David P. Caldwell <david@davidpcaldwell.com>
2018-12-15 17:34:03 -0500

    implement new browser testing suite runner which loads suite from URL specified in query string

--- loader/browser/test/test/suite.js
+++ loader/browser/test/test/suite.js
@@ -1,4 +1,29 @@
 window.addEventListener("load", function() {
-	var api = inonit.loader.loader.file("../api.js");
-	api.suite.part("$api", api.getPartDescriptor("../../../../loader/$api.api.html",{},void(0)));
+	var parameters = (function() {
+		if (window.location.search && window.location.search.length > 1) {
+			var web = inonit.loader.loader.module("../../../../js/web/", {
+				// TODO: can this context be standardized in js/web itself?
+				escaper: {
+					encode: window.escape,
+					decode: window.unescape
+				}
+			});
+			var form = new web.Form({ urlencoded: window.location.search.substring(1) });
+			var script = (function(controls) {
+				for (var i=0; i<controls.length; i++) {
+					if (controls[i].name == "suite") return controls[i].value;
+				}
+			})(form.controls);
+			return { form: form, suite: script };
+		}
+	})();
+	if (parameters.suite) {
+		document.getElementById("nosuite").style.display = "none";
+		var api = inonit.loader.loader.file("../api.js");
+		inonit.loader.loader.run(parameters.suite, {
+			parameters: parameters.form,
+			suite: api.suite,
+			getPartDescriptor: api.getPartDescriptor
+		});
+	}
 });
