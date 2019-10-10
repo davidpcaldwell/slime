e53d5d9a5ecf8dcda537a486be9921e34ebd9ec4
David P. Caldwell <david@davidpcaldwell.com>
2019-10-10 09:13:20 -0400

    adds PATCH as allowed method for HTTP client

--- rhino/http/client/plugin.jsh.js
+++ rhino/http/client/plugin.jsh.js
@@ -12,14 +12,16 @@
 
 plugin({
 	isReady: function() {
-		return typeof(jsh.js) != "undefined" && typeof(jsh.io) != "undefined" && typeof(jsh.js.web) != "undefined";
+		return typeof(jsh.js) != "undefined" && typeof(jsh.io) != "undefined" && typeof(jsh.js.web) != "undefined" && jsh.java;
 	},
 	load: function() {
 		jsh.http = $loader.module("module.js", {
+			$slime: $slime,
 			api: {
-				io: jsh.io,
 				js: jsh.js,
-				web: jsh.js.web
+				java: jsh.java,
+				web: jsh.js.web,
+				io: jsh.io
 			}
 		});
 
