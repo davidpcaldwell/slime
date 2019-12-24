594115b00846a6d46fbc0c92852a1abff1a3b853
David P. Caldwell <david@davidpcaldwell.com>
2019-12-24 12:14:54 -0500

    moves toward initial test for mock GitHub by adding real client to test script and continuing to add type data

--- jsh/unit/test/manual/github.jsh.js
+++ jsh/unit/test/manual/github.jsh.js
@@ -1,3 +1,15 @@
 //@ts-check
-var www = new jsh.unit.mock.Web();
-www.add(jsh.unit.mock.Web.github());
+
+var parameters = jsh.script.getopts({
+	options: {
+		real: false
+	}
+});
+
+var client = (function() {
+	if (parameters.options.real) return new jsh.http.Client();
+	var www = new jsh.unit.mock.Web();
+	www.add(jsh.unit.mock.Web.github());
+	return www.client;
+})();
+
