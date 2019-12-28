9f7ac474bbcdd799a128ea1efc86e9afd461de55
David P. Caldwell <david@davidpcaldwell.com>
2019-12-27 22:43:39 -0500

    generalize contributor documentation script ... add JSDoc to jsh/tools/install ... refactors plist toward type safety

--- contributor/documentation.jsh.js
+++ contributor/documentation.jsh.js
@@ -1,3 +1,11 @@
+var parameters = jsh.script.getopts({
+	options: {
+		base: jsh.file.Pathname,
+		path: "api.html",
+		host: "documentation"
+	}
+});
+
 jsh.shell.jsh.require({
 	satisfied: function() {
 		return jsh.httpd.Tomcat;
@@ -7,27 +15,29 @@ jsh.shell.jsh.require({
 	}
 });
 
+var base = parameters.options.base.directory;
+
 var server = jsh.httpd.Tomcat.serve({
-	directory: jsh.shell.jsh.src
+	directory: base
 });
 
 //	Use dedicated Chrome browser if present
 if (jsh.shell.browser.chrome) {
 	var pac = jsh.shell.jsh.src.getFile("rhino/ui/application-hostToPort.pac").read(String)
-		.replace(/__HOST__/g, "slime-documentation")
+		.replace(/__HOST__/g, parameters.options.host)
 		.replace(/__PORT__/g, String(server.port))
 	;
 	var instance = new jsh.shell.browser.chrome.Instance({
-		location: jsh.shell.jsh.src.getRelativePath("local/chrome/documentation"),
+		location: base.getRelativePath("local/chrome/documentation"),
 		proxy: new jsh.shell.browser.ProxyConfiguration({
 			code: pac
 		})
 	});
 	instance.run({
-		uri: "http://slime-documentation/api.html"
+		uri: "http://" + parameters.options.host + "/" + parameters.options.path
 	});
 } else {
 	//	Otherwise, fall back to Java desktop integration and default browser
-	Packages.java.awt.Desktop.getDesktop().browse( new Packages.java.net.URI( "http://127.0.0.1:" + server.port + "/api.html" ) );
+	Packages.java.awt.Desktop.getDesktop().browse( new Packages.java.net.URI( "http://127.0.0.1:" + server.port + "/" + parameters.options.path ) );
 }
 server.run();
