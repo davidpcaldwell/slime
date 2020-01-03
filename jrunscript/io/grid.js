bfa5b0b8c14e3cc38fd0f7f93632a5a709c79ddb
David P. Caldwell <david@davidpcaldwell.com>
2020-01-03 15:13:33 -0500

    Add code to POI jrunscript/io grid implementation, allowing basic spreadsheet parsing

--- jrunscript/io/grid.js
+++ jrunscript/io/grid.js
@@ -1,7 +1,59 @@
 if ($context.getClass("org.apache.poi.hssf.usermodel.HSSFWorkbook")) {
 	$exports.excel = new function() {
+		var Cell = function(_cell) {
+			this.toString = function() {
+				return String(_cell);
+			}
+
+			this.getStringValue = function() {
+				var _string = _cell.getStringCellValue();
+				if (_string === null) return null;
+				return String(_string);
+			}
+		};
+
+		var Row = function(_row) {
+			this.toString = function() {
+				return "(Row)";
+			};
+
+			this.getCells = function() {
+				var rv = [];
+				for (var i=0; i<_row.getLastCellNum(); i++) {
+					var _cell = _row.getCell(i);
+					rv.push( (_cell) ? new Cell(_cell) : null );
+				}
+				return rv;
+			}
+		};
+
+		var Sheet = function(_sheet) {
+			this.toString = function() {
+				return "name = " + _sheet.getSheetName() + " _sheet = " + _sheet;
+			};
+
+			Object.defineProperty(this, "name", {
+				get: function() {
+					return String(_sheet.getSheetName());
+				},
+				enumerable: true
+			});
+
+			this.getRows = function() {
+				var rv = [];
+				for (var i=0; i<_sheet.getLastRowNum(); i++) {
+					var _row = _sheet.getRow(i);
+					rv.push( (_row) ? new Row(_row) : null );
+				}
+				return rv;
+			}
+		}
+
 		var Workbook = function(_workbook) {
-			Packages.java.lang.System.err.println("_workbook = " + _workbook);
+			this.toString = function() {
+				return "_workbook = " + _workbook;
+			};
+
 			this.sheets = {};
 
 			Object.defineProperty(this.sheets, "count", {
@@ -10,6 +62,14 @@ if ($context.getClass("org.apache.poi.hssf.usermodel.HSSFWorkbook")) {
 				},
 				enumerable: true
 			});
+
+			this.sheets.list = function() {
+				var rv = [];
+				for (var i=0; i<this.count; i++) {
+					rv[i] = new Sheet(_workbook.getSheetAt(i));
+				}
+				return rv;
+			}
 		}
 
 		var format = {};
