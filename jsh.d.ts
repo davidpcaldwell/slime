5d309ebf33bac455d623922fb98c6f2b9120da44
David P. Caldwell <david@davidpcaldwell.com>
2020-01-13 12:16:34 -0500

    Update jsh.d.ts to organize jsh interface/namespace

--- jsh.d.ts
+++ jsh.d.ts
@@ -1,24 +1,42 @@
-interface jsh {}
+interface jsh {
+	loader: any;
+	js: any;
+	document: any;
+	file: any;
+
+	shell: {
+		console: (message: string) => void,
+		exit: (code: number) => void,
+		jsh: any,
+		environment: any,
+		echo: Function
+	};
+
+	script: {
+		getopts: Function & { UNEXPECTED_OPTION_PARSER: any },
+		file: slime.jrunscript.file.File,
+		Application: any
+	}
+}
 
 declare namespace jsh {
+	const http: slime.jrunscript.http.client
+
+	//	TODO	Below probably should be defined in terms of SLIME types, like above
+	const js: jsh.js;
+	const document: jsh.document;
+	const shell: jsh.shell;
+	const file: jsh.file;
+
+	//	TODO	investigate whether to define in terms of SLIME
+	const loader: jsh.loader;
+	const tools: {
+		git: any
+	}
 	namespace unit {
 		const mock: slime.jsh.unit.mock
 	}
 
-	const http: slime.jrunscript.http.client
-
-	const script: {
-		getopts: Function,
-		file: slime.jrunscript.file.File
-	}
-
-	const shell: {
-		console: (message: string) => void,
-		exit: (code: number) => void,
-		jsh: any
-	}
-
-	const tools: {
-		git: any
-	}
+	//	jsh-specific
+	const script: jsh.script
 }
