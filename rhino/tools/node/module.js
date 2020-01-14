7d0c1ddc71d2b2d8999e21692f19f7cd5e2e2a8d
David P. Caldwell <david@davidpcaldwell.com>
2020-01-14 05:00:14 -0500

    Update Node.js default version to 12.14.1 ... add additional APIs to Node definition file and type annotations to source code

--- rhino/tools/node/module.js
+++ rhino/tools/node/module.js
@@ -1,161 +1,193 @@
-//	TODO	detach from jsh
-$exports.Installation = function(o) {
-	this.toString = function() {
-		return "Node installation at " + o.directory;
-	};
+//@ts-check
 
-	Object.defineProperty(this, "version", {
-		get: function() {
-			var string = $context.module.shell.run({
-				command: o.directory.getFile("bin/node"),
-				arguments: ["--version"],
-				stdio: {
-					output: String
-				},
-				evaluate: function(result) {
-					return result.stdio.output;
-				}
-			});
-			return {
-				toString: function() {
-					return string;
-				},
-				number: string.substring(1)
+/**
+ *	@typedef {object} slime.jrunscript.node.Context
+ */
+
+/**
+ * @typedef {object} slime.jrunscript.node.Installation
+ */
+
+/**
+ * @typedef {object} slime.jrunscript.node.Exports
+ * @property { new (p: any) => slime.jrunscript.node.Installation } Installation
+ * @property {Function} at
+ * @property {Function} install
+ * @property {Function} Project
+ */
+
+void(0);
+
+(
+	/**
+	 * @param {slime.jrunscript.node.Context} $context
+	 * @param {slime.jrunscript.node.Exports} $exports
+	 */
+	function($context,$exports) {
+		/**
+		 * @constructor
+		 */
+		$exports.Installation = function(o) {
+			this.toString = function() {
+				return "Node installation at " + o.directory;
 			};
-		}
-	})
 
-	var PATH = (function() {
-		var elements = $context.module.shell.PATH.pathnames.slice();
-		elements.push(o.directory.getRelativePath("bin"));
-		return $context.module.file.Searchpath(elements);
-	})();
-
-	this.run = function(p) {
-		var command = (function() {
-			if (p.command) {
-				if (p.project) return p.project.getFile("node_modules/.bin/" + p.command);
-				return o.directory.getFile("bin/" + p.command);
-			}
-			return o.directory.getFile("bin/node");
-		})();
-		return $context.module.shell.run({
-			command: command,
-			arguments: p.arguments,
-			directory: p.directory,
-			environment: function(environment) {
-				//	TODO	check for other types besides object, function, falsy
-				//	TODO	can this be simplified further using mutator concept? Maybe; mutator could allow object and just return it
-				environment.PATH = PATH.toString();
-				var mutating = $api.Function.mutating(p.environment);
-				var result = mutating(environment);
-				if (!result.PATH) result.PATH = PATH.toString();
-			},
-			stdio: p.stdio,
-			evaluate: p.evaluate
-		});
-	}
-
-	var npm = (function(run) {
-		return function(p) {
-			return run($api.Object.compose(p, {
-				command: "npm",
-				arguments: function(list) {
-					list.push(p.command);
-					if (p.global) {
-						list.push("--global");
-					}
-					var mutating = $api.Function.mutating(p.arguments);
-					var npmargs = mutating([]);
-					list.push.apply(list, npmargs);
+			Object.defineProperty(this, "version", {
+				get: function() {
+					var string = $context.module.shell.run({
+						command: o.directory.getFile("bin/node"),
+						arguments: ["--version"],
+						stdio: {
+							output: String
+						},
+						evaluate: function(result) {
+							return result.stdio.output;
+						}
+					});
+					return {
+						toString: function() {
+							return string;
+						},
+						number: string.substring(1)
+					};
 				}
-			}));
-		};
-	})(this.run);
+			})
 
-	this.modules = new function() {
-		var Installed = function() {
-			var node_modules = o.directory.getSubdirectory("lib/node_modules");
-			if (node_modules) {
-				node_modules.list().forEach(function(item) {
-					this[item.pathname.basename] = {};
-				},this);
-			}
-		}
+			var PATH = (function() {
+				var elements = $context.module.shell.PATH.pathnames.slice();
+				elements.push(o.directory.getRelativePath("bin"));
+				return $context.module.file.Searchpath(elements);
+			})();
 
-		this.installed = new Installed();
-
-		this.refresh = function() {
-			this.installed = new Installed();
-		}
-
-		this.install = function(p) {
-			if (p.name) {
-				npm({
-					command: "install",
-					global: true,
-					arguments: [p.name]
+			this.run = function(p) {
+				var command = (function() {
+					if (p.command) {
+						if (p.project) return p.project.getFile("node_modules/.bin/" + p.command);
+						return o.directory.getFile("bin/" + p.command);
+					}
+					return o.directory.getFile("bin/node");
+				})();
+				return $context.module.shell.run({
+					command: command,
+					arguments: p.arguments,
+					directory: p.directory,
+					environment: function(environment) {
+						//	TODO	check for other types besides object, function, falsy
+						//	TODO	can this be simplified further using mutator concept? Maybe; mutator could allow object and just return it
+						environment.PATH = PATH.toString();
+						var mutating = $api.Function.mutating(p.environment);
+						var result = mutating(environment);
+						if (!result.PATH) result.PATH = PATH.toString();
+					},
+					stdio: p.stdio,
+					evaluate: p.evaluate
 				});
-				this.refresh();
 			}
+
+			var npm = (function(run) {
+				return function(p) {
+					return run($api.Object.compose(p, {
+						command: "npm",
+						arguments: function(list) {
+							list.push(p.command);
+							if (p.global) {
+								list.push("--global");
+							}
+							var mutating = $api.Function.mutating(p.arguments);
+							var npmargs = mutating([]);
+							list.push.apply(list, npmargs);
+						}
+					}));
+				};
+			})(this.run);
+
+			this.modules = new function() {
+				var Installed = function() {
+					var node_modules = o.directory.getSubdirectory("lib/node_modules");
+					if (node_modules) {
+						node_modules.list().forEach(function(item) {
+							this[item.pathname.basename] = {};
+						},this);
+					}
+				}
+
+				this.installed = new Installed();
+
+				this.refresh = function() {
+					this.installed = new Installed();
+				}
+
+				this.install = function(p) {
+					if (p.name) {
+						npm({
+							command: "install",
+							global: true,
+							arguments: [p.name]
+						});
+						this.refresh();
+					}
+				};
+
+				this.uninstall = function(p) {
+					if (p.name) {
+						npm({
+							command: "uninstall",
+							global: true,
+							arguments: [p.name]
+						});
+						this.refresh();
+					}
+				};
+			};
+
+			this.npm = new function() {
+				this.run = function(p) {
+					return npm(p);
+				}
+			};
 		};
 
-		this.uninstall = function(p) {
-			if (p.name) {
-				npm({
-					command: "uninstall",
-					global: true,
-					arguments: [p.name]
+		$exports.Project = function(o) {
+			throw new Error();
+		}
+
+		var versions = {
+			"12.13.1": { url: "https://nodejs.org/dist/v12.13.1/node-v12.13.1-darwin-x64.tar.gz" },
+			"12.14.1": { url: "https://nodejs.org/dist/v12.14.1/node-v12.14.1-darwin-x64.tar.gz" }
+		};
+
+		$exports.at = function(p) {
+			if (!p.location) throw new TypeError("Required: 'location' property.");
+			if (!p.location.directory) return null;
+			return new $exports.Installation({
+				directory: p.location.directory
+			})
+		};
+
+		$exports.install = $api.Events.Function(function(p,events) {
+			if (!p) p = {};
+			if (!p.version) p.version = "12.14.1";
+			var existing = $exports.at({ location: p.location });
+			var rv;
+			if (!existing || (existing.version != p.version && p.update)) {
+				if (existing) {
+					p.location.directory.remove();
+				}
+				var version = versions[p.version];
+				$context.library.install.install({
+					url: version.url,
+					to: p.location
 				});
-				this.refresh();
+				rv = new $exports.Installation({
+					directory: p.location.directory
+				});
+				events.fire("console", "Node " + rv.version + " installed.");
+			} else {
+				rv = existing;
+				events.fire("console", "Node " + existing.version + " already installed.");
 			}
-		};
-	};
-
-	this.npm = new function() {
-		this.run = function(p) {
-			return npm(p);
-		}
-	};
-};
-
-$exports.Project = function(o) {
-	throw new Error();
-}
-
-var versions = {
-	"12.13.1": { url: "https://nodejs.org/dist/v12.13.1/node-v12.13.1-darwin-x64.tar.gz" }
-};
-
-$exports.at = function(p) {
-	if (!p.location) throw new TypeError("Required: 'location' property.");
-	if (!p.location.directory) return null;
-	return new $exports.Installation({
-		directory: p.location.directory
-	})
-};
-
-$exports.install = $api.Events.Function(function(p,events) {
-	if (!p) p = {};
-	if (!p.version) p.version = "12.13.1";
-	var existing = $exports.at({ location: p.location });
-	var rv;
-	if (!existing || (existing.version != p.version && p.update)) {
-		if (existing) {
-			p.location.directory.remove();
-		}
-		var version = versions[p.version];
-		$context.library.install.install({
-			url: version.url,
-			to: p.location
+			return rv;
 		});
-		rv = new $exports.Installation({
-			directory: p.location.directory
-		});
-		events.fire("console", "Node " + rv.version + " installed.");
-	} else {
-		rv = existing;
-		events.fire("console", "Node " + existing.version + " already installed.");
 	}
-	return rv;
-});
+	//@ts-ignore
+)($context,$exports)
