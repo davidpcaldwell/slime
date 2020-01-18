9cf267875bef65ef7b9b4fc2257d94db3d6da115
David P. Caldwell <david@davidpcaldwell.com>
2020-01-18 06:36:24 -0500

    Add creation of initial GitHub client and add TypeScript checking to it

--- rhino/tools/github/module.js
+++ rhino/tools/github/module.js
@@ -1,3 +1,79 @@
-$exports.Session = function(o) {
-	throw new Error("Unimplemented.");
-}
+//@ts-check
+
+/**
+ * @typedef {object} slime.jrunscript.tools.github.Context
+ * @property { { http: slime.jrunscript.http.client, shell: jsh.shell } } library
+ */
+
+/**
+ * @typedef {object} slime.jrunscript.tools.github.Exports
+ * @property {any} Session
+ */
+
+//	Work around tsc bug not allowing first expression to be parenthetical
+void(0);
+(
+	/**
+	 * @param {slime.jrunscript.tools.github.Context} $context
+	 * @param {slime.jrunscript.tools.github.Exports} $exports
+	 * @param {*} Packages
+	 */
+	function($context,$exports,Packages) {
+		$exports.Session = function(o) {
+			var apiClient = (function(o) {
+				var client = new $context.library.http.Client({
+					authorization: (o.credentials) ? $context.library.http.Authentication.Basic.Authorization({
+						user: o.credentials.user,
+						password: o.credentials.password
+					}) : void(0)
+				});
+
+				var evaluate = function(response) {
+					if (response.status.code == 404) return null;
+					var string = response.body.stream.character().asString();
+					if (response.status.code != 200) {
+						$context.library.shell.console("Response code: " + response.status.code + " " + response.request.method + " " + response.request.url);
+						$context.library.shell.console(string);
+						$context.library.shell.console("");
+					}
+					var rv = (string) ? JSON.parse(string) : void(0);
+					if (response.status.code == 403 && rv && rv.documentation_url == "https://developer.github.com/v3/#abuse-rate-limits") {
+						return {
+							retry: true
+						}
+					}
+					if (rv && rv.message == "Bad credentials") throw new Error("Bad credentials.");
+					return rv;
+				};
+
+				client.request = (function(was) {
+					return function(p) {
+						var more = true;
+						var retry = 1;
+						var rv;
+						while(more) {
+							rv = was.call(this, $api.Object.compose(p, {
+								url: "https://api.github.com/" + p.url,
+								evaluate: evaluate
+							}));
+							if (rv && rv.retry) {
+								//	TODO	X.X should use Retry-After
+								//			see https://developer.github.com/v3/guides/best-practices-for-integrators/#dealing-with-abuse-rate-limits
+								$context.library.shell.console("Sleeping for " + retry + " seconds...");
+								Packages.java.lang.Thread.sleep(retry * 1000);
+								retry *= 2;
+							} else {
+								return rv;
+							}
+						}
+					}
+				})(client.request);
+
+				return client;
+			})(o);
+
+			throw new Error("Unimplemented.");
+		}
+	}
+//@ts-ignore
+)($context,$exports,Packages)
