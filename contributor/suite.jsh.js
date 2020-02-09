a5a798ccaeee916f5482041962f921e9024f9077
David P. Caldwell <david@davidpcaldwell.com>
2020-02-09 18:39:46 -0500

    Improve contributor documentation regarding testing

--- contributor/suite.jsh.js
+++ contributor/suite.jsh.js
@@ -10,20 +10,11 @@
 //	Contributor(s):
 //	END LICENSE
 
-if (!jsh.httpd.Tomcat) {
-	jsh.shell.console("Installing Tomcat into " + jsh.script.file + " shell ...");
-	jsh.shell.jsh({
-		script: jsh.script.file.parent.parent.getFile("jsh/tools/install/tomcat.jsh.js")
-	});
-	jsh.shell.jsh({
-		fork: true,
-		script: jsh.script.file,
-		arguments: jsh.script.arguments,
-		evaluate: function(result) {
-			jsh.shell.exit(result.status);
-		}
-	});
-}
+jsh.shell.tools.tomcat.require(void(0), {
+	console: function(e) {
+		jsh.shell.console(e.detail);
+	}
+});
 
 var parameters = jsh.script.getopts({
 	options: {
