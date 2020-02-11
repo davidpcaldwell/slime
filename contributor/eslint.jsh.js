2b1920c11b9b2822c312aa1dd62c43438409db4a
David P. Caldwell <david@davidpcaldwell.com>
2020-02-11 09:03:35 -0500

    Set project as ESLint root so that it can be contained in other projects with ESLint configurations ... convert ESLint script to allow it to be run against arbitrary project

--- contributor/eslint.jsh.js
+++ contributor/eslint.jsh.js
@@ -1,10 +1,17 @@
+var parameters = jsh.script.getopts({
+	options: {
+		project: jsh.shell.jsh.src.pathname
+	}
+});
+
 jsh.shell.jsh({
 	script: jsh.shell.jsh.src.getFile("jsh/tools/install/eslint.jsh.js")
 });
+
 jsh.shell.tools.node.run({
 	command: "eslint",
-	arguments: ["."],
-	directory: jsh.shell.jsh.src,
+	arguments: [/*"--debug",*/ "."],
+	directory: parameters.options.project.directory,
 	evaluate: function(result) {
 		jsh.shell.exit(result.status);
 	}
