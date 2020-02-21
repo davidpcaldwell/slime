67e2dbcf706a4e5b7aff987befb6f72daaf0d95a
David P. Caldwell <david@davidpcaldwell.com>
2020-02-21 07:55:22 -0500

    Clean up output from contributor/git.bash remove-merged

--- contributor/git.jsh.js
+++ contributor/git.jsh.js
@@ -10,13 +10,12 @@ jsh.script.Application.run({
 				if (!parameters.global.repository) parameters.global.repository = jsh.script.file.parent.parent.pathname;
 				/** @type { slime.jrunscript.git.LocalRepository } */
 				var repository = new jsh.tools.git.Repository({ directory: parameters.global.repository.directory });
-				jsh.shell.console("Repository: " + repository);
 				repository.fetch({ all: true, prune: true });
 				var branches = repository.branch({ all: true });
-				branches.forEach(function(branch) {
-					jsh.shell.console(branch.name + " " + JSON.stringify(branch));
-					jsh.shell.console("");
-				});
+				// branches.forEach(function(branch) {
+				// 	jsh.shell.console(branch.name + " " + JSON.stringify(branch));
+				// 	jsh.shell.console("");
+				// });
 
 				var remotePattern = /^remotes\/(.+?)\/(.*)$/;
 
@@ -25,33 +24,33 @@ jsh.script.Application.run({
 				}
 
 				branches.forEach(function(branch) {
-					if (branch.name != "master") {
+					if (branch.name != "remotes/origin/master") {
 						//jsh.shell.console(branch.name);
 						var common = repository.mergeBase({
-							commits: ["master", branch.name]
+							commits: ["origin/master", branch.name]
 						});
 						if (common.commit.hash == branch.commit.commit.hash) {
 							var remote = isRemote(branch);
-							jsh.shell.console("Remove: " + branch.name + " remote = " + isRemote(branch));
+							//jsh.shell.console("Remove: " + branch.name + " remote = " + isRemote(branch));
 							if (remote) {
 								var match = remotePattern.exec(branch.name);
-								if (match[2] != "master") {
-									jsh.shell.console("Delete remote: " + match[1] + " name: " + match[2]);
-									repository.push({
-										delete: true,
-										repository: match[1],
-										refspec: match[2]
-									});
-								} else {
-									jsh.shell.console("Not deleting remote: " + match[1] + " name: " + match[2]);
-								}
+								jsh.shell.console("Delete remote: " + match[1] + " name: " + match[2]);
+								repository.push({
+									delete: true,
+									repository: match[1],
+									refspec: match[2]
+								});
 							} else {
-								jsh.shell.console("Delete local: " + branch.name);
-								repository.branch({ delete: branch.name });
+								if (branch.name != "master") {
+									jsh.shell.console("Delete local: " + branch.name);
+									repository.branch({ delete: branch.name });
+								} else {
+									// jsh.shell.console("Not deleting master.");
+								}
 							}
 						} else {
-							jsh.shell.console(Object.keys(branch));
-							jsh.shell.console(Object.keys(branch.commit));
+							// jsh.shell.console(Object.keys(branch));
+							// jsh.shell.console(Object.keys(branch.commit));
 							jsh.shell.console("Unmerged: " + branch.name + " common=" + common.commit.hash + " " + " branch=" + branch.commit.commit.hash);
 						}
 					}
