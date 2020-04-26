96a112d80bca463c704b982bc19dba4940710e5b
David P. Caldwell <david@davidpcaldwell.com>
2020-04-26 08:11:29 -0400

    Improve TypeScript definition for rhino/shell embed()

--- rhino/shell/module.d.ts
+++ rhino/shell/module.d.ts
@@ -4,4 +4,13 @@ namespace slime.jrunscript.shell {
 		output?: any
 		error?: any
 	}
+
+	interface Exports {
+		//	fires started, exception, stdout, stderr
+		embed: (p: {
+			method: Function
+			argument: object
+			started: (p: { output?: string, error?: string }) => boolean
+		}, events: $api.Events.Function.Receiver) => void
+	}
 }
