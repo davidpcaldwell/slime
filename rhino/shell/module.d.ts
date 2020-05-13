f64705717ec3d1677d29f469563923c594c61df5
David P. Caldwell <david@davidpcaldwell.com>
2020-05-12 22:10:04 -0400

    Fix output problems with wf status ... add partial TypeScript definition for jrunscript/shell java

--- rhino/shell/module.d.ts
+++ rhino/shell/module.d.ts
@@ -5,6 +5,19 @@ namespace slime.jrunscript.shell {
 		error?: any
 	}
 
+	interface Result {
+	}
+
+	interface java {
+		(p: {
+
+		}): Result
+
+		version: string
+
+		launcher: slime.jrunscript.file.File
+	}
+
 	interface Exports {
 		//	fires started, exception, stdout, stderr
 		embed: (p: {
@@ -12,5 +25,7 @@ namespace slime.jrunscript.shell {
 			argument: object
 			started: (p: { output?: string, error?: string }) => boolean
 		}, events: $api.Events.Function.Receiver) => void
+
+		java: java
 	}
 }
