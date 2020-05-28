99af2294932d7e8ff453f0b56e8b6f1d7e197de7
David P. Caldwell <david@davidpcaldwell.com>
2020-05-28 00:06:35 -0400

    Rearrange Fifty test runner to be more modular

--- loader/api/test/fifty/test.jsh.js
+++ loader/api/test/fifty/test.jsh.js
@@ -1,38 +1,44 @@
 //@ts-check
 (function() {
-	var tests = {
-		types: {}
-	};
-
 	/** @type { slime.definition.verify.Factory } */
 	var Verify = jsh.loader.file(jsh.shell.jsh.src.getFile("loader/api/verify.js")).Verify;
 
-	/** @type { slime.definition.verify.Scope } */
-	var scope = new function() {
-		this.success = true;
+	var execute = function(file) {
+		/** @type { slime.definition.verify.Scope } */
+		var scope = new function() {
+			this.success = true;
 
-		/** @param { slime.definition.verify.ScopeTest } f */
-		this.test = function(f) {
-			var result = f();
-			if (result.success === false) {
-				this.success = false;
-				jsh.shell.console(result.message);
-			} else if (result.success === true) {
-				//	do nothing
-				jsh.shell.console(result.message);
-			} else {
-				throw new TypeError();
+			/** @param { slime.definition.verify.ScopeTest } f */
+			this.test = function(f) {
+				var result = f();
+				if (result.success === false) {
+					this.success = false;
+					jsh.shell.console(result.message);
+				} else if (result.success === true) {
+					//	do nothing
+					jsh.shell.console(result.message);
+				} else {
+					throw new TypeError();
+				}
 			}
-		}
+		};
+
+		var verify = Verify(scope);
+
+		var loader = new jsh.file.Loader({ directory: file.parent });
+
+		var tests = {
+			types: {}
+		};
+
+		loader.run(file.pathname.basename, {
+			$loader: loader,
+			verify: verify,
+			tests: tests
+		});
+
+		tests.suite();
 	};
 
-	var verify = Verify(scope);
-
-	jsh.script.loader.run("module.d.ts", {
-		$loader: jsh.script.loader,
-		verify: verify,
-		tests: tests
-	});
-
-	tests.suite();
+	execute(jsh.script.file.parent.getFile("module.d.ts"));
 })();
