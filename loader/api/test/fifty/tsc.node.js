7b50d9b908d7c072c61d680f5608f8f1d04f4c2b
David P. Caldwell <david@davidpcaldwell.com>
2020-05-31 06:54:27 -0400

    Get programmatic tsc invocation of project working in preparation for working to use broader compiler APIs rather than AST for documentation

--- loader/api/test/fifty/tsc.node.js
+++ loader/api/test/fifty/tsc.node.js
@@ -2,13 +2,51 @@
 const path = require("path");
 const ts = require("typescript");
 
-console.log(ts.getParsedCommandLineOfConfigFile);
+//	Shims supporting copy-pasted functions
 
-//	Looking at tsc executeCommandLineWorker
+function shouldBePretty(sys, options) {
+	return true;
+}
+
+function enableStatistics() {
+}
+
+function reportStatistics(sys, program) {
+}
+
+//	Copy-pasted from tsc
+
+function createReportErrorSummary(sys, options) {
+	return shouldBePretty(sys, options) ?
+		function (errorCount) { return sys.write(ts.getErrorSummaryText(errorCount, sys.newLine)); } :
+		undefined;
+}
+
+function performCompilation(sys, cb, reportDiagnostic, config) {
+	var fileNames = config.fileNames, options = config.options, projectReferences = config.projectReferences;
+	var host = ts.createCompilerHostWorker(options, undefined, sys);
+	var currentDirectory = host.getCurrentDirectory();
+	var getCanonicalFileName = ts.createGetCanonicalFileName(host.useCaseSensitiveFileNames());
+	ts.changeCompilerHostLikeToUseCache(host, function (fileName) { return ts.toPath(fileName, currentDirectory, getCanonicalFileName); });
+	enableStatistics(sys, options);
+	var programOptions = {
+		rootNames: fileNames,
+		options: options,
+		projectReferences: projectReferences,
+		host: host,
+		configFileParsingDiagnostics: ts.getConfigFileParsingDiagnostics(config)
+	};
+	var program = ts.createProgram(programOptions);
+	var exitStatus = ts.emitFilesAndReportErrorsAndGetExitStatus(program, reportDiagnostic, function (s) { return sys.write(s + sys.newLine); }, createReportErrorSummary(sys, options));
+	reportStatistics(sys, program);
+	cb(program);
+	return sys.exit(exitStatus);
+}
+
+//	Mimic basic flow of tsc.js: executeCommandLine, executeCommandLineWorker
+
 var project = process.env.PROJECT;
-console.log("project: " + project);
 var normalized = path.resolve(ts.normalizePath(project))
-console.log("path: " + normalized);
 
 const configFileName = ts.findConfigFile(
 	normalized,
@@ -16,4 +54,30 @@ const configFileName = ts.findConfigFile(
 	"jsconfig.json"
 );
 
-console.log("config = " + configFileName);
+const commandLine = ts.parseCommandLine([], function (path) { return system.readFile(path); });
+const commandLineOptions = ts.convertToOptionsWithAbsolutePaths(commandLine.options, function (fileName) { return ts.getNormalizedAbsolutePath(fileName, currentDirectory); });
+const reportDiagnostic = ts.createDiagnosticReporter(ts.sys);
+
+const configParseResult = ts.parseConfigFileWithSystem(
+	configFileName,
+	commandLineOptions,
+	commandLine.watchOptions,
+	ts.sys,
+	reportDiagnostic
+);
+
+const status = performCompilation(
+	Object.assign({}, ts.sys, {
+		exit: function(status) {
+			return status;
+		}
+	}),
+	function(program) {
+		console.log(program);
+		console.log(program.getRootFileNames());
+	},
+	reportDiagnostic,
+	configParseResult
+)
+
+console.log("Finished with exit status: " + status);
