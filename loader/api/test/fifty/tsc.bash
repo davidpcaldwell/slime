a24bae6e5ab25c49b63b51ae9cbb94458a386111
David P. Caldwell <david@davidpcaldwell.com>
2020-06-09 16:40:52 -0400

    Get Fifty server working to return JSON response representing TypeScript data ... remove all global definitions of 'base' in scripts

--- loader/api/test/fifty/tsc.bash
+++ loader/api/test/fifty/tsc.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR="$(dirname $0)"
 SLIME="${DIR}/../../../.."
-export PROJECT="${SLIME}"
+: "${PROJECT:=${SLIME}}"
+export PROJECT
 env PATH="${PATH}:${SLIME}/local/jsh/lib/node/bin" NODE_PATH="${SLIME}/local/jsh/lib/node/lib/node_modules" node ${NODE_DEBUG} ${DIR}/tsc.node.js "$@"
