af9b44e74c846c1e01f047aabe484c01cc7a55a5
David P. Caldwell <david@davidpcaldwell.com>
2020-06-14 13:01:52 -0400

    Add JDK8 regression test to test case for #160 (JDK 11)

--- tools/test/issue-160.jsh.js
+++ tools/test/issue-160.jsh.js
@@ -13,6 +13,14 @@
 		command: jsh.shell.jsh.src.getFile("jsh.bash"),
 		arguments: [ jsh.shell.jsh.src.getFile("jsh/test/jsh-data.jsh.js") ],
 		environment: $api.Object.compose(jsh.shell.environment, {
+			JSH_LAUNCHER_JDK_HOME: jsh.shell.jsh.src.getSubdirectory("local/jdk/default").toString()
+		})
+	})
+
+	jsh.shell.run({
+		command: jsh.shell.jsh.src.getFile("jsh.bash"),
+		arguments: [ jsh.shell.jsh.src.getFile("jsh/test/jsh-data.jsh.js") ],
+		environment: $api.Object.compose(jsh.shell.environment, {
 			JSH_LAUNCHER_JDK_HOME: jsh.shell.jsh.src.getSubdirectory("local/jdk/11").toString()
 		})
 	})
