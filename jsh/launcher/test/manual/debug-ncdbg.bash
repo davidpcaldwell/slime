b069aae2c7b66fcdaa916e9cb642c4ba9c1fb7b9
David P. Caldwell <david@davidpcaldwell.com>
2020-06-18 09:33:46 -0400

    Fixes wrong URL for Chrome DevTools in ncdbg startup script ... fixes assumption about JDK structure in ncdbg script which was incorrect for JDK 11 ... launches browser to execute DevTools in ncdbg manual test

--- jsh/launcher/test/manual/debug-ncdbg.bash
+++ jsh/launcher/test/manual/debug-ncdbg.bash
@@ -2,4 +2,4 @@
 SLIME="$(dirname $0)/../../../.."
 #export JSH_DEBUG_SCRIPT=ncdbg
 #${SLIME}/jsh.bash ${SLIME}/jsh/test/jsh-data.jsh.js
-${SLIME}/jsh.bash ${SLIME}/jsh/tools/ncdbg.jsh.js ${SLIME}/jsh/test/jsh-data.jsh.js
+${SLIME}/jsh.bash ${SLIME}/jsh/tools/ncdbg.jsh.js -ncdbg:chrome:instance ${SLIME}/local/chrome/test/ncdbg ${SLIME}/jsh/test/jsh-data.jsh.js
