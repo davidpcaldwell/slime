81b21ebbd8e33088e3760d19d0a65e694dc099fe
David P. Caldwell <david@davidpcaldwell.com>
2020-06-23 04:41:10 -0400

    Factor jsh.shell.tools documentation into correct directory

--- rhino/shell/jsh.d.ts
+++ rhino/shell/jsh.d.ts
@@ -38,29 +38,6 @@ namespace jsh.shell {
 				}
 			}
 		},
-		tools: {
-			rhino: {
-				install: (
-					p: {
-						mock?: { lib: slime.jrunscript.file.Directory, rhino: slime.jrunscript.file.File }
-						local?: slime.jrunscript.file.File
-						replace?: boolean
-						version?: string
-					},
-					events?: any
-				) => void
-				require: any
-			}
-			graal: any
-			tomcat: any
-			ncdbg: any
-			kotlin: any
-			jsyaml: any
-			node: any
-			javamail: any
-			jsoup: any
-			postgresql: any
-			scala: any
-		}
+		tools: jsh.shell.Tools
 	}
 }
