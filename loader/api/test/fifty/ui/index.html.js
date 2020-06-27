b555e4d52a75aafd9e9a2576bac4134d05545721
David P. Caldwell <david@davidpcaldwell.com>
2020-06-26 22:07:21 -0400

    Improve custom element handling in Fifty

--- loader/api/test/fifty/ui/index.html.js
+++ loader/api/test/fifty/ui/index.html.js
@@ -26,10 +26,8 @@ var defineCustomElement = function(p) {
 	return {
 		create: function(m) {
 			var rv = document.createElement(p.extends, { is: p.tagName });
-			var name = document.createElement("span");
-			name.appendChild(document.createTextNode(p.name));
-			for (var x in m.slots) {
-				fillSlot(rv, x, m.slots[x]());
+			for (var x in p.slots) {
+				fillSlot(rv, x, p.slots[x](m));
 			}
 			return rv;
 		}
@@ -38,29 +36,24 @@ var defineCustomElement = function(p) {
 
 var fiftyInterface = defineCustomElement({
 	tagName: "fifty-interface",
-	extends: "div"
-});
-
-var createFiftyInterface = function(p) {
-	return fiftyInterface.create({
-		slots: {
-			name: function() {
-				var name = document.createElement("span");
-				name.appendChild(document.createTextNode(p.name));
-				return name;
-			}
+	extends: "div",
+	slots: {
+		name: function(p) {
+			var name = document.createElement("span");
+			name.appendChild(document.createTextNode(p.name));
+			return name;
 		}
-	});
-}
+	}
+});
 
 window.addEventListener("load", function() {
 	if (window.location.search == "?design") {
-		var element = createFiftyInterface({ name: "a.a.name" });
+		var element = fiftyInterface.create({ name: "a.a.name" });
 		document.getElementById("interfaces").appendChild(element);
 	} else {
 		var tsc = inonit.loader.loader.get("tsc.json").read(JSON);
 		for (var x in tsc.interfaces) {
-			element = createFiftyInterface({ name: x });
+			element = fiftyInterface.create({ name: x });
 			document.getElementById("interfaces").appendChild(element);
 		}
 	}
