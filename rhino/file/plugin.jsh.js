2ed7282e748c54c80c75152fef3739d8a7a8538b
David P. Caldwell <david@davidpcaldwell.com>
2020-07-04 17:51:17 -0400

    Add type checking for jsh.file plugin

--- rhino/file/plugin.jsh.js
+++ rhino/file/plugin.jsh.js
@@ -11,33 +11,45 @@
 //	Contributor(s):
 //	END LICENSE
 
-plugin({
-	isReady: function() {
-		return jsh.loader && jsh.loader.addFinalizer && jsh.js && jsh.java && jsh.io;
-	},
-	load: function() {
-		var context = {
-			$slime: {
-				Resource: $slime.Resource
+//@ts-check
+(
+	/**
+	 * @param { jsh.plugin.$slime } $slime
+	 * @param { jsh } jsh
+	 * @param { jsh.plugin.plugin } plugin
+	 * @param { slime.Loader } $loader
+	 */
+	function($slime, jsh, plugin, $loader) {
+		plugin({
+			isReady: function() {
+				return jsh.loader && jsh.loader.addFinalizer && jsh.js && jsh.java && jsh.io;
 			},
-			$pwd: $slime.getSystemProperty("user.dir"),
-			addFinalizer: jsh.loader.addFinalizer,
-			api: {
-				js: jsh.js,
-				java: jsh.java,
-				io: jsh.io
+			load: function() {
+				var context = {
+					$slime: {
+						Resource: $slime.Resource
+					},
+					$pwd: $slime.getSystemProperty("user.dir"),
+					addFinalizer: jsh.loader.addFinalizer,
+					api: {
+						js: jsh.js,
+						java: jsh.java,
+						io: jsh.io
+					}
+				};
+
+				//	Windows
+				var environment = jsh.java.Environment($slime.getEnvironment());
+				if (environment.PATHEXT) {
+					context.pathext = environment.PATHEXT.split(";");
+				}
+
+				//	Cygwin
+				$loader.run("plugin.jsh.cygwin.js", { $slime: $slime, context: context });
+
+				jsh.file = $loader.module("module.js", context);
 			}
-		};
-
-		//	Windows
-		var environment = jsh.java.Environment($slime.getEnvironment());
-		if (environment.PATHEXT) {
-			context.pathext = environment.PATHEXT.split(";");
-		}
-
-		//	Cygwin
-		$loader.run("plugin.jsh.cygwin.js", { $slime: $slime, context: context });
-
-		jsh.file = $loader.module("module.js", context);
+		})
 	}
-})
+//@ts-ignore
+)($slime, jsh, plugin, $loader)
