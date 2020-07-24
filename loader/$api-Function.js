f0cb00389835a065f1b7e730f767c66abb09e24d
David P. Caldwell <david@davidpcaldwell.com>
2020-07-24 13:08:19 -0400

    Add type checking to $api-Function.js and add Function.returning and Function.Array.find to TypeScript declaration

--- loader/$api-Function.js
+++ loader/$api-Function.js
@@ -1,173 +1,183 @@
-$exports.Function = $context.old.Function;
+//@ts-check
+(
+	/**
+	 *
+	 * @param { { old: Partial<$api.Function> } } $context
+	 * @param { { Function: Partial<$api.Function> }} $exports
+	 */
+	function($context,$exports) {
+		$exports.Function = $context.old.Function;
 
-$exports.Function.identity = function(v) { return v; };
+		$exports.Function.identity = function(v) { return v; };
 
-$exports.Function.type = function(o) {
-	if (o === null) return "null";
-	return typeof(o);
-}
-
-$exports.Function.memoized = function(f) {
-	var returns;
-
-	return function() {
-		if (arguments.length > 0) throw new TypeError("Memoized functions may not have arguments.");
-		//	Ignore 'this'
-		if (!returns) {
-			returns = { value: f.call(this) };
+		$exports.Function.type = function(o) {
+			if (o === null) return "null";
+			return typeof(o);
 		}
-		return returns.value;
-	};
-};
 
-$exports.Function.pipe = function() {
-	var items = Array.prototype.slice.call(arguments);
-	return function(v) {
-		var rv = v;
-		for (var i=0; i<items.length; i++) {
-			rv = items[i].call(this,rv);
-		}
-		return rv;
-	}
-};
+		$exports.Function.memoized = function(f) {
+			var returns;
 
-$exports.Function.result = function() {
-	var items = Array.prototype.slice.call(arguments);
-	return $exports.Function.pipe.apply(this, items.slice(1))(items[0]);
-}
-
-$exports.Function.property = function(name) {
-	return function(v) {
-		//  TODO    handling of undefined, null?
-		return v[name];
-	}
-};
-
-$exports.Function.safeNavigate = function(name) {
-	return function(p) {
-		return (p == null) ? void(0) : p[name];
-	}
-};
-
-$exports.Function.is = function(value) {
-	return function(v) {
-		return v === value;
-	}
-};
-
-$exports.Function.returning = function(v) {
-	return function() {
-		return v;
-	};
-};
-
-$exports.Function.conditional = function(test,yes,no) {
-	return function() {
-		var condition = test.apply(this,arguments);
-		return (condition) ? yes.apply(this,arguments) : no.apply(this,arguments);
-	};
-};
-
-$exports.Function.String = {
-	split: function(delimiter) {
-		return function(string) {
-			return string.split(delimiter);
-		}
-	}
-};
-
-$exports.Function.Array = {
-	/** @type { $api["Function"]["Array"]["filter"] } */
-	filter: function(f) {
-		return function(array) {
-			return array.filter(f, this);
-		}
-	},
-	map: function(f) {
-		return function(array) {
-			return array.map(f, this);
-		}
-	},
-	find: function(f) {
-		return function(array) {
-			return array.find(f, this);
-		}
-	}
-};
-
-$exports.Function.Object = {
-	entries: function(o) {
-		return Object.entries(o);
-	},
-	fromEntries: function(iterable) {
-		return Object.fromEntries(iterable);
-	}
-}
-
-$exports.Function.JSON = {};
-$exports.Function.JSON.stringify = function(p) {
-	var replacer = (p && p.replacer) ? p.replacer : void(0);
-	//	TODO	is the below correct for 0 and '' ?
-	var space = (p && p.space) ? p.space : void(0);
-	return function(v) {
-		return JSON.stringify(v, replacer, space);
-	}
-};
-
-$exports.Function.argument = {};
-$exports.Function.argument.check = function(p) {
-	if (p.type) {
-		var reference = (p.name) ? "arguments[" + p.index + "] (" + p.name + ")" : "arguments[" + p.index + "]";
-		return function() {
-			if (typeof(arguments[p.index]) == "undefined" && !p.undefined) throw new TypeError(reference + " must be a string, not undefined.");
-			if (arguments[p.index] === null && !p["null"]) throw new TypeError(reference + " must be a string, not null.");
-			if (typeof(arguments[p.index]) != p.type) throw new TypeError(reference + " must be type " + "\"" + p.type + "\"" + ", not " + typeof(arguments[p.index]));
-		}
-	}
-};
-$exports.Function.argument.isString = function(p) {
-	var reference = (p.name) ? "arguments[" + p.index + "] (" + p.name + ")" : "arguments[" + p.index + "]";
-	return function() {
-		if (typeof(arguments[p.index]) == "undefined" && !p.undefined) throw new TypeError(reference + " must be a string, not undefined.");
-		if (arguments[p.index] === null && !p["null"]) throw new TypeError(reference + " must be a string, not null.");
-		if (typeof(arguments[p.index]) != "string") throw new TypeError(reference + " must be a string, not " + typeof(arguments[p.index]));
-	};
-};
-
-$exports.Function.evaluator = function() {
-	//	Provides an ordered cache implementation
-	var components = arguments;
-	return function() {
-		var rv;
-		var invocation = {
-			target: this,
-			arguments: Array.prototype.slice.call(arguments)
+			return function() {
+				if (arguments.length > 0) throw new TypeError("Memoized functions may not have arguments.");
+				//	Ignore 'this'
+				if (!returns) {
+					returns = { value: f.call(this) };
+				}
+				return returns.value;
+			};
 		};
-		for (var i=0; i<components.length; i++) {
-			if (typeof(components[i]) == "function") {
-				rv = components[i].apply(this,arguments);
-			} else if (typeof(components[i]) == "object") {
-				rv = components[i].get(invocation);
-			}
-			if (typeof(rv) != "undefined") {
-				for (var j=i; j>=0; j--) {
-					if (typeof(components[j]) == "object") {
-						components[j].set(invocation,rv);
-					}
+
+		$exports.Function.pipe = function() {
+			var items = Array.prototype.slice.call(arguments);
+			return function(v) {
+				var rv = v;
+				for (var i=0; i<items.length; i++) {
+					rv = items[i].call(this,rv);
 				}
 				return rv;
 			}
-		}
-		return rv;
-	};
-};
+		};
 
-$exports.Function.series = function() {
-	var functions = arguments;
-	return function() {
-		for (var i=0; i<functions.length; i++) {
-			var rv = functions[i].apply(this,arguments);
-			if (typeof(rv) != "undefined") return rv;
+		$exports.Function.result = function() {
+			var items = Array.prototype.slice.call(arguments);
+			return $exports.Function.pipe.apply(this, items.slice(1))(items[0]);
 		}
-	};
-};
+
+		$exports.Function.property = function(name) {
+			return function(v) {
+				//  TODO    handling of undefined, null?
+				return v[name];
+			}
+		};
+
+		$exports.Function.safeNavigate = function(name) {
+			return function(p) {
+				return (p == null) ? void(0) : p[name];
+			}
+		};
+
+		$exports.Function.is = function(value) {
+			return function(v) {
+				return v === value;
+			}
+		};
+
+		$exports.Function.returning = function(v) {
+			return function() {
+				return v;
+			};
+		};
+
+		$exports.Function.conditional = function(test,yes,no) {
+			return function() {
+				var condition = test.apply(this,arguments);
+				return (condition) ? yes.apply(this,arguments) : no.apply(this,arguments);
+			};
+		};
+
+		$exports.Function.String = {
+			split: function(delimiter) {
+				return function(string) {
+					return string.split(delimiter);
+				}
+			}
+		};
+
+		$exports.Function.Array = {
+			filter: function(f) {
+				return function(array) {
+					return array.filter(f, this);
+				}
+			},
+			find: function(f) {
+				return function(array) {
+					return array.find(f, this);
+				}
+			},
+			map: function(f) {
+				return function(array) {
+					return array.map(f, this);
+				}
+			}
+		};
+
+		$exports.Function.Object = {
+			entries: function(o) {
+				return Object.entries(o);
+			},
+			fromEntries: function(iterable) {
+				return Object.fromEntries(iterable);
+			}
+		}
+
+		$exports.Function.JSON = {};
+		$exports.Function.JSON.stringify = function(p) {
+			var replacer = (p && p.replacer) ? p.replacer : void(0);
+			//	TODO	is the below correct for 0 and '' ?
+			var space = (p && p.space) ? p.space : void(0);
+			return function(v) {
+				return JSON.stringify(v, replacer, space);
+			}
+		};
+
+		$exports.Function.argument = {};
+		$exports.Function.argument.check = function(p) {
+			if (p.type) {
+				var reference = (p.name) ? "arguments[" + p.index + "] (" + p.name + ")" : "arguments[" + p.index + "]";
+				return function() {
+					if (typeof(arguments[p.index]) == "undefined" && !p.undefined) throw new TypeError(reference + " must be a string, not undefined.");
+					if (arguments[p.index] === null && !p["null"]) throw new TypeError(reference + " must be a string, not null.");
+					if (typeof(arguments[p.index]) != p.type) throw new TypeError(reference + " must be type " + "\"" + p.type + "\"" + ", not " + typeof(arguments[p.index]));
+				}
+			}
+		};
+		$exports.Function.argument.isString = function(p) {
+			var reference = (p.name) ? "arguments[" + p.index + "] (" + p.name + ")" : "arguments[" + p.index + "]";
+			return function() {
+				if (typeof(arguments[p.index]) == "undefined" && !p.undefined) throw new TypeError(reference + " must be a string, not undefined.");
+				if (arguments[p.index] === null && !p["null"]) throw new TypeError(reference + " must be a string, not null.");
+				if (typeof(arguments[p.index]) != "string") throw new TypeError(reference + " must be a string, not " + typeof(arguments[p.index]));
+			};
+		};
+
+		$exports.Function.evaluator = function() {
+			//	Provides an ordered cache implementation
+			var components = arguments;
+			return function() {
+				var rv;
+				var invocation = {
+					target: this,
+					arguments: Array.prototype.slice.call(arguments)
+				};
+				for (var i=0; i<components.length; i++) {
+					if (typeof(components[i]) == "function") {
+						rv = components[i].apply(this,arguments);
+					} else if (typeof(components[i]) == "object") {
+						rv = components[i].get(invocation);
+					}
+					if (typeof(rv) != "undefined") {
+						for (var j=i; j>=0; j--) {
+							if (typeof(components[j]) == "object") {
+								components[j].set(invocation,rv);
+							}
+						}
+						return rv;
+					}
+				}
+				return rv;
+			};
+		};
+
+		$exports.Function.series = function() {
+			var functions = arguments;
+			return function() {
+				for (var i=0; i<functions.length; i++) {
+					var rv = functions[i].apply(this,arguments);
+					if (typeof(rv) != "undefined") return rv;
+				}
+			};
+		};
+	}
+//@ts-ignore
+)($context,$exports)
