881ac4d87cf9da888fd07e6e6579a724d9423516
David P. Caldwell <david@davidpcaldwell.com>
2020-07-30 19:29:00 -0400

    Get initial version of Fifty test scoping working

--- loader/api/test/fifty/test.jsh.js
+++ loader/api/test/fifty/test.jsh.js
@@ -9,25 +9,73 @@
 	/** @type { slime.definition.verify.Factory } */
 	var Verify = jsh.loader.file(jsh.shell.jsh.src.getFile("loader/api/verify.js")).Verify;
 
-	var execute = function(file) {
-		/** @type { slime.definition.verify.Scope & { success: boolean } } */
-		var scope = new function() {
-			this.success = true;
-
-			/** @param { slime.definition.verify.Scope.Test } f */
-			this.test = function(f) {
-				var result = f();
-				if (result.success === false) {
-					this.success = false;
-					jsh.shell.console(result.message);
-				} else if (result.success === true) {
-					//	do nothing
-					jsh.shell.console(result.message);
-				} else {
-					throw new TypeError();
-				}
-			}
+	var console = new function() {
+		var write = function(indent,string) {
+			var prefix = new Array(indent + 1).join("  ")
+			jsh.shell.console(prefix + string);
 		};
+
+		this.start = function(indent,name) {
+			write(indent, "RUN: " + name);
+		};
+
+		this.end = function(indent,name,result) {
+			var resultString = (result) ? "PASSED" : "FAILED"
+			write(indent, resultString + ": " + name);
+		};
+
+		this.test = function(indent,message) {
+			write(indent, message);
+		}
+	}
+
+	/** @type { new (p?: {}) => slime.definition.verify.Scope & { success: boolean, start: Function, end: Function } } */
+	var Scope = function(p) {
+		if (!p) p = {};
+
+		this.success = true;
+
+		this.parent = (p.parent) ? p.parent : null;
+
+		this.depth = function() {
+			return (this.parent) ? this.parent.depth() + 1 : 0;
+		};
+
+		this.toString = function() {
+			return "Scope: " + this.depth();
+		}
+
+		this.start = function(name) {
+			console.start(this.depth() + 1, name);
+		}
+
+		this.end = function(name,result) {
+			console.end(this.depth() + 1, name, result);
+		}
+
+		this.fail = function() {
+			this.success = false;
+			if (p.parent) p.parent.fail();
+		}
+
+		/** @param { slime.definition.verify.Scope.Test } f */
+		this.test = function(f) {
+			var result = f();
+			if (result.success === false) {
+				fail();
+				console.test(this.depth() + 1, result.message);
+			} else if (result.success === true) {
+				//	do nothing
+			} else {
+				throw new TypeError();
+			}
+			console.test(this.depth() + 1, result.message);
+		}
+	}
+
+	var execute = function(file) {
+		/** @type { slime.definition.verify.Scope & { success: boolean, start: Function, end: Function } } */
+		var scope = new Scope();
 
 		var verify = Verify(scope);
 
@@ -40,13 +88,32 @@
 			types: {}
 		};
 
-		loader.run(file.pathname.basename, {
+		loader.run(file.pathname.basename, $api.Object.compose({
 			$loader: loader,
-			verify: verify,
+			run: function(code,name) {
+				if (!name) name = "run";
+				scope.start(name);
+				var was = {
+					scope: scope,
+					verify: verify
+				};
+				verify = Verify(new Scope({ parent: scope }));
+				code();
+				var result = scope.success;
+				scope = was.scope;
+				verify = was.verify;
+				scope.end(name,result);
+			},
 			tests: tests
-		});
+		}, new function() {
+			this.verify = function() {
+				return verify.apply(this,arguments);
+			}
+		}));
 
+		console.start(0, file);
 		tests.suite();
+		console.end(0, file, scope.success);
 
 		return scope.success;
 	};
