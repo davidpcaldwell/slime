41beb0899ebb05f1614816b01cf273d5eed91080
David P. Caldwell <david@davidpcaldwell.com>
2020-10-03 08:27:52 -0400

    Remove rv from TypeScript global scope

--- jsh/test/manual/issue62.jsh.js
+++ jsh/test/manual/issue62.jsh.js
@@ -10,14 +10,18 @@
 //	Contributor(s):
 //	END LICENSE
 
-var tmp = jsh.shell.TMPDIR.createTemporary({ prefix: "issue62.", suffix: ".txt" });
-var _writer = new Packages.java.io.PrintWriter(new Packages.java.io.FileWriter(tmp.pathname.java.adapt()));
-_writer.println("First line");
-_writer.println("Second line");
-_writer.close();
+(
+	function() {
+		var tmp = jsh.shell.TMPDIR.createTemporary({ prefix: "issue62.", suffix: ".txt" });
+		var _writer = new Packages.java.io.PrintWriter(new Packages.java.io.FileWriter(tmp.pathname.java.adapt()));
+		_writer.println("First line");
+		_writer.println("Second line");
+		_writer.close();
 
-var rv = [];
-tmp.readLines(function(line) {
-	rv.push(line);
-});
-jsh.shell.echo(rv.join("|"));
+		var rv = [];
+		tmp.readLines(function(line) {
+			rv.push(line);
+		});
+		jsh.shell.echo(rv.join("|"));
+	}
+)();
