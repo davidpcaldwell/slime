ba21e5c33271e163ea70b231c144577fff8eb6e4
David P. Caldwell <david@davidpcaldwell.com>
2020-11-20 07:07:32 -0500

    Improve TypeScript definition for jsh.shell.tools.rhino

--- jsh/tools/install/plugin.jsh.d.ts
+++ jsh/tools/install/plugin.jsh.d.ts
@@ -6,18 +6,24 @@ namespace jsh.tools.install {
 }
 
 namespace jsh.shell.tools {
+	namespace rhino {
+		interface InstallCommand {
+			mock?: { lib: slime.jrunscript.file.Directory, rhino: slime.jrunscript.file.File }
+			local?: slime.jrunscript.file.File
+			replace?: boolean
+			version?: string
+		}
+	}
 	interface Exports {
 		rhino: {
 			install: (
-				p?: {
-					mock?: { lib: slime.jrunscript.file.Directory, rhino: slime.jrunscript.file.File }
-					local?: slime.jrunscript.file.File
-					replace?: boolean
-					version?: string
-				},
+				p?: rhino.InstallCommand,
 				events?: any
 			) => void
-			require: any
+			require: (
+				p?: rhino.InstallCommand,
+				events?: any
+			) => void
 		}
 		graal: any
 		tomcat: any
