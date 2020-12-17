aee5a4c229af82d1742f8a01f96177b46531df7e
David P. Caldwell <david@davidpcaldwell.com>
2020-12-17 12:54:10 -0500

    Add command-line tool to serve a directory and potentially open Chrome to browse it

--- tools/test/manual/requireGitIdentity-gui.jsh.js
+++ tools/test/manual/requireGitIdentity-gui.jsh.js
@@ -1,13 +1,17 @@
-var directory = jsh.shell.TMPDIR.createTemporary({ directory: true });
-var repository = jsh.tools.git.init({ pathname: directory.pathname });
-jsh.wf.requireGitIdentity({
-	repository: repository,
-	get: jsh.wf.requireGitIdentity.get.gui
-});
-var config = repository.config({
-	arguments: ["--list"]
-});
-jsh.shell.console(JSON.stringify({
-	name: config["user.name"],
-	email: config["user.email"]
-}));
+(
+	function() {
+		var directory = jsh.shell.TMPDIR.createTemporary({ directory: true });
+		var repository = jsh.tools.git.init({ pathname: directory.pathname });
+		jsh.wf.requireGitIdentity({
+			repository: repository,
+			get: jsh.wf.requireGitIdentity.get.gui
+		});
+		var config = repository.config({
+			arguments: ["--list"]
+		});
+		jsh.shell.console(JSON.stringify({
+			name: config["user.name"],
+			email: config["user.email"]
+		}));
+	}
+)()
