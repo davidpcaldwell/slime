b4f1c2338d5af383001699fccc293f496c9322d5
David P. Caldwell <david@davidpcaldwell.com>
2020-12-22 18:02:39 -0500

    Rename loader/api/unit.d.ts to loader/api/unit.fifty.ts ... migrate one existing test to Fifty

---
+++ loader/api/unit.fifty.ts
@@ -0,0 +1,180 @@
+(
+	function(
+		fifty: slime.fifty.test.kit
+	) {
+
+	}
+//@ts-ignore
+)(fifty);
+
+namespace slime.definition.unit {
+	export interface Context {
+		log: (message: string, p: any) => void,
+		api: {
+			Promise: () => PromiseConstructorLike & { all: Function, resolve: Function }
+		}
+	}
+
+	export interface Exports {
+		Verify: (scope: any, vars?: any) => slime.definition.verify.Verify
+		Suite: new (o: any) => { listeners: { add: (type: string, handler: Function) => void }, run: () => boolean, promise: () => void }
+		View: {
+			(o: View.Handler): View
+			(f: View.Listener): View
+		}
+		getStructure: Function
+		Scenario: new () => {}
+		TestExecutionProcessor: Function
+		JSON: {
+			Encoder: (o: {
+				send: (json: string) => void
+			}) => View
+
+			Decoder: () => {
+				/**
+				 * A property that allows listeners to be added to / removed from the underlying event emitter.
+				 */
+				listeners: $api.Events["listeners"]
+
+				/**
+				 * Fires the decoded event to this decoder's listeners.
+				 */
+				decode: (json: string) => void
+			}
+		}
+	}
+
+	export type Factory = slime.Loader.Product<Context,Exports>
+}
+
+namespace slime.definition.unit.internal {
+	export const { subject, types } = (function(fifty: slime.fifty.test.kit) {
+		const code = fifty.$loader.factory("unit.js") as slime.definition.unit.Factory;
+
+		const verify = fifty.verify;
+
+		const subject = code();
+
+		return {
+			subject: subject,
+			types: {
+				definition: function types_definition() {
+					//	TODO	add test for error being thrown
+
+					var a = {};
+					var suite = new subject.Suite({
+						parts: {
+							a: {
+								initialize: function(scope) {
+									scope.a = a;
+								},
+								execute: function(scope,verify) {
+									verify(scope.a).is(a);
+								}
+							}
+						}
+					});
+					var rv = suite.run() as boolean;
+					verify(rv,"success").is(true);
+
+					//	TODO	add tests for suite
+				}
+			}
+		}
+	//@ts-ignore
+	})(fifty);
+}
+
+namespace slime.definition.unit {
+	(
+		function(
+			fifty: slime.fifty.test.kit
+		) {
+			fifty.tests.suite = function() {
+				run(internal.types.definition);
+			}
+		}
+	//@ts-ignore
+	)(fifty);
+}
+
+namespace slime.definition.unit {
+	export namespace Verify {
+		export type Factory = {
+			( scope: slime.definition.verify.Scope ): Verify
+		}
+	}
+
+	type Verify = slime.definition.verify.Verify & {
+		test: Function
+		suite: Function
+		scope: Function
+		scenario: Function
+		fire: Function
+	}
+
+	export namespace Event {
+		export namespace Scenario {
+			export type Start = {
+				start: any
+			}
+
+			type End = {
+				end: any
+				success: boolean
+			}
+
+			export type Detail = Start | End
+		}
+
+		export namespace Test {
+			export type Detail = {
+				success: boolean
+				message: string
+				error: any
+			}
+		}
+
+		export type Handler = $api.Events.Handler<{
+			scenario: Event.Scenario.Detail
+			test: Event.Test.Detail
+		}>
+	}
+
+	export interface View {
+		listen: (scenario: $api.Events) => void
+		on: Event.Handler
+	}
+
+	export namespace View {
+		export type Error = {
+			type: string
+			message: string
+			stack: string
+		}
+
+		export type Handler = {
+			start: (scenario: {
+				name: string
+			}) => void
+
+			test: (result: {
+				success: boolean
+				message: string
+				error?: Error
+			}) => void
+
+			end: (
+				scenario: {
+					name: string
+				},
+				success: boolean
+			) => void
+		}
+
+		export type Listener = $api.Event.Handler<any>
+
+		export type Argument = Handler | Listener
+	}
+
+}
