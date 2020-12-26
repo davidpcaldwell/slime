420c1c4046d471b17d7522092b79c332d27f77c4
David P. Caldwell <david@davidpcaldwell.com>
2020-12-26 11:42:29 -0500

    Merge loader/$api-Function.d.ts into loader/$api-Function.fifty.ts

--- loader/$api-Function.fifty.ts
+++ loader/$api-Function.fifty.ts
@@ -1,3 +1,214 @@
+interface $api {
+	Function: $api.Function
+}
+
+namespace $api {
+	export namespace Function {
+		export type Predicate<T> = (t: T) => boolean
+		/** @deprecated */
+		export type Filter<T> = (t: T) => boolean
+	}
+
+	export interface Function {
+		identity: <T>(t: T) => T
+		returning: <T>(t: T) => () => T
+		is: <T>(value: T) => (t: T) => boolean
+		property: <T,K extends keyof T>(name: K) => (t: T) => T[K]
+		Array: {
+			filter: <T>(f: Function.Predicate<T>) => (ts: T[]) => T[]
+			find: <T>(f: Function.Predicate<T>) => (ts: T[]) => T | undefined
+			map: any
+			join: (s: string) => (elements: any[]) => string
+
+			groupBy: <V,G>(c: {
+				group: (element: V) => G,
+				groups?: G[],
+				codec?: { encode: (group: G) => string, decode: (string: string) => G },
+			}) => (p: V[]) => { group: G, array: V[] }[]
+
+			sum: <T>(attribute: (T) => number) => (array: T[]) => number
+		}
+		Boolean: {
+			map: <T>(p: { true: T, false: T }) => (b: boolean) => T
+		}
+		optionalChain<T,K extends keyof T>(k: K): (t: T) => T[K]
+		memoized: <T>(f: () => T) => () => T
+		pipe: {
+			<T,U,V,W,X,Y,Z,R>(
+				f: (t: T) => U,
+				g: (u: U) => V,
+				h: (v: V) => W,
+				i: (w: W) => X,
+				j: (x: X) => Y,
+				k: (y: Y) => Z,
+				l: (z: Z) => R
+			): (t: T) => R
+			<T,U,V,W,X,Y,R>(
+				f: (t: T) => U,
+				g: (u: U) => V,
+				h: (v: V) => W,
+				i: (w: W) => X,
+				j: (x: X) => Y,
+				k: (y: Y) => R
+			): (t: T) => R
+			<T,U,V,W,X,R>(
+				f: (t: T) => U,
+				g: (u: U) => V,
+				h: (v: V) => W,
+				i: (w: W) => X,
+				j: (x: X) => R
+			): (t: T) => R
+			<T,U,V,W,R>(
+				f: (t: T) => U,
+				g: (u: U) => V,
+				h: (v: V) => W,
+				i: (w: W) => R
+			): (t: T) => R
+			<T,U,V,R>(
+				f: (t: T) => U,
+				g: (u: U) => V,
+				h: (v: V) => R
+			): (t: T) => R
+			<T,U,R>(
+				f: (t: T) => U,
+				g: (u: U) => R
+			): (t: T) => R
+			<T,R>(f: (t: T) => R): (t: T) => R
+		}
+		result: {
+			<P,T,U,V,W,X,Y,Z,R>(
+				p: P,
+				f: (p: P) => T,
+				g: (t: T) => U,
+				h: (u: U) => V,
+				i: (v: V) => W,
+				j: (w: W) => X,
+				k: (x: X) => Y,
+				l: (y: Y) => Z,
+				m: (z: Z) => R
+			): R
+			<P,T,U,V,W,X,Y,R>(
+				p: P,
+				f: (p: P) => T,
+				g: (t: T) => U,
+				h: (u: U) => V,
+				i: (v: V) => W,
+				j: (w: W) => X,
+				k: (x: X) => Y,
+				l: (y: Y) => R
+			): R
+			<P,T,U,V,W,X,R>(
+				p: P,
+				f: (p: P) => T,
+				g: (t: T) => U,
+				h: (u: U) => V,
+				i: (v: V) => W,
+				j: (w: W) => X,
+				k: (x: X) => R
+			): R
+			<P,T,U,V,W,R>(
+				p: P,
+				f: (p: P) => T,
+				g: (t: T) => U,
+				h: (u: U) => V,
+				i: (v: V) => W,
+				j: (w: W) => R
+			): R
+			<P,T,U,V,R>(
+				p: P,
+				f: (p: P) => T,
+				g: (t: T) => U,
+				h: (u: U) => V,
+				i: (v: V) => R
+			): R
+			<P,T,U,R>(
+				p: P,
+				f: (p: P) => T,
+				g: (t: T) => U,
+				h: (u: U) => R
+			): R
+			<P,T,R>(
+				p: P,
+				f: (p: P) => T,
+				g: (t: T) => R
+			): R
+			<P,R>(
+				p: P,
+				f: (i: P) => R
+			): R
+		}
+		Object: {
+			entries: {
+				(p: {}): [string, any][]
+			}
+			fromEntries: {
+				(p: Iterable<readonly [string | number | symbol, any]>): { [x: string]: any }
+			}
+		}
+		conditional: {
+			<T,R>(p: { condition: (t: T) => boolean, true: (t: T) => R, false: (t: T) => R }): (t: T) => R
+			(test: any, yes: any, no: any): any
+		}
+		[name: string]: any
+	}
+
+	export interface Function {
+		filter: {
+			or: {
+				<T>(f1: Function.Predicate<T>, f2: Function.Predicate<T>, f3: Function.Predicate<T>, f4: Function.Predicate<T>, f5: Function.Predicate<T>): Function.Predicate<T>
+				<T>(f1: Function.Predicate<T>, f2: Function.Predicate<T>, f3: Function.Predicate<T>, f4: Function.Predicate<T>): Function.Predicate<T>
+				<T>(f1: Function.Predicate<T>, f2: Function.Predicate<T>, f3: Function.Predicate<T>): Function.Predicate<T>
+				<T>(f1: Function.Predicate<T>, f2: Function.Predicate<T>): Function.Predicate<T>
+			}
+			and: {
+				<T>(f1: Function.Predicate<T>, f2: Function.Predicate<T>, f3: Function.Predicate<T>, f4: Function.Predicate<T>, f5: Function.Predicate<T>): Function.Predicate<T>
+				<T>(f1: Function.Predicate<T>, f2: Function.Predicate<T>, f3: Function.Predicate<T>, f4: Function.Predicate<T>): Function.Predicate<T>
+				<T>(f1: Function.Predicate<T>, f2: Function.Predicate<T>, f3: Function.Predicate<T>): Function.Predicate<T>
+				<T>(f1: Function.Predicate<T>, f2: Function.Predicate<T>): Function.Predicate<T>
+			}
+			not: <T>(f: Function.Predicate<T>) => Function.Predicate<T>
+		},
+		Predicate: {
+			is: <T>(value: T) => Function.Predicate<T>
+		}
+	}
+
+	export interface Function {
+		comparator: {
+			/**
+			 * Creates a comparator given a mapping (which represents some aspect of an underlying type) and a comparator that
+			 * compares the mapped values.
+			 */
+			create: <T,M>(mapping: (t: T) => M, comparator: Function.Comparator<M>) => Function.Comparator<T>
+
+			/**
+			 * A comparator that uses the < and > operators to compare its arguments.
+			 */
+			operators: Function.Comparator<any>,
+
+			/**
+			 * Creates a comparator that represents the opposite of the given comparator.
+			 */
+			reverse: <T>(comparator: Function.Comparator<T>) => Function.Comparator<T>
+
+			/**
+			 * Creates a comparator that applies the given comparators in order, using succeeding comparators if a comparator
+			 * indicates two values are equal.
+			 */
+			compose: {
+				<TXY>(c1: Function.Comparator<TXY>, c2: Function.Comparator<TXY>, c3: Function.Comparator<TXY>, c4: Function.Comparator<TXY>, c5: Function.Comparator<TXY>): Function.Comparator<TXY>
+				<TXY>(c1: Function.Comparator<TXY>, c2: Function.Comparator<TXY>, c3: Function.Comparator<TXY>, c4: Function.Comparator<TXY>): Function.Comparator<TXY>
+				<TXY>(c1: Function.Comparator<TXY>, c2: Function.Comparator<TXY>, c3: Function.Comparator<TXY>): Function.Comparator<TXY>
+				<TXY>(c1: Function.Comparator<TXY>, c2: Function.Comparator<TXY>): Function.Comparator<TXY>
+			}
+		}
+	}
+
+	export namespace Function {
+		export type Comparator<T> = (t1: T, t2: T) => number
+	}
+}
+
 namespace $api {
 	interface Function {
 		RegExp: {
