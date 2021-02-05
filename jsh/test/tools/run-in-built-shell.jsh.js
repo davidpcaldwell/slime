9368c03876f2845918b61e2184e0da192cb7fec3
David P. Caldwell <david@davidpcaldwell.com>
2021-02-05 08:59:04 -0500

    Remove 'path', 'script' from global TypeScript scope

--- jsh/test/tools/run-in-built-shell.jsh.js
+++ jsh/test/tools/run-in-built-shell.jsh.js
@@ -15,45 +15,49 @@ var args = [];
 //				} else if (Packages.java.lang.System.getProperty("jsh.engine.rhino.classpath")) {
 //					args.push("-Djsh.engine.rhino.classpath=" + Packages.java.lang.System.getProperty("jsh.engine.rhino.classpath"));
 //				}
-var SLIME = jsh.shell.jsh.src;
-args.push(SLIME.getRelativePath("rhino/jrunscript/api.js"));
-args.push("jsh");
-args.push(SLIME.getRelativePath("jsh/etc/build.jsh.js"));
-args.push(JSH_HOME);
-args.push("-notest");
-args.push("-nodoc");
-// TODO: Rhino; other parameters jsh/test/plugin.jsh.js
-jsh.shell.run({
-	command: jsh.shell.java.jrunscript,
-	arguments: args
-});
+(
+	function() {
+		var SLIME = jsh.shell.jsh.src;
+		args.push(SLIME.getRelativePath("rhino/jrunscript/api.js"));
+		args.push("jsh");
+		args.push(SLIME.getRelativePath("jsh/etc/build.jsh.js"));
+		args.push(JSH_HOME);
+		args.push("-notest");
+		args.push("-nodoc");
+		// TODO: Rhino; other parameters jsh/test/plugin.jsh.js
+		jsh.shell.run({
+			command: jsh.shell.java.jrunscript,
+			arguments: args
+		});
 
-var environment = Object.assign({}, jsh.shell.environment, (parameters.options.launcherDebug) ? {"JSH_LAUNCHER_DEBUG": "true"}: {});
-if (!parameters.options.packaged) {
-	jsh.shell.run({
-		command: jsh.shell.java.jrunscript,
-		arguments: (function() {
-			var rv = [];
-			rv.push(JSH_HOME.getFile("jsh.js"));
-			rv.push.apply(rv,parameters.arguments);
-			return rv;
-		})(),
-		environment: environment
-	});
-} else {
-	var to = jsh.shell.TMPDIR.createTemporary({ directory: true }).getRelativePath("packaged.jar");
-	var script = parameters.arguments.shift();
-	jsh.shell.jsh({
-		shell: JSH_HOME,
-		script: jsh.shell.jsh.src.getRelativePath("jsh/tools/package.jsh.js"),
-		arguments: ([
-			"-script", script,
-			"-to", to
-		]).concat( (!rhino) ? ["-norhino"] : [] )
-	});
-	jsh.shell.java({
-		jar: to.file,
-		arguments: parameters.arguments,
-		environment: environment
-	});
-}
+		var environment = Object.assign({}, jsh.shell.environment, (parameters.options.launcherDebug) ? {"JSH_LAUNCHER_DEBUG": "true"}: {});
+		if (!parameters.options.packaged) {
+			jsh.shell.run({
+				command: jsh.shell.java.jrunscript,
+				arguments: (function() {
+					var rv = [];
+					rv.push(JSH_HOME.getFile("jsh.js"));
+					rv.push.apply(rv,parameters.arguments);
+					return rv;
+				})(),
+				environment: environment
+			});
+		} else {
+			var to = jsh.shell.TMPDIR.createTemporary({ directory: true }).getRelativePath("packaged.jar");
+			var script = parameters.arguments.shift();
+			jsh.shell.jsh({
+				shell: JSH_HOME,
+				script: jsh.shell.jsh.src.getRelativePath("jsh/tools/package.jsh.js"),
+				arguments: ([
+					"-script", script,
+					"-to", to
+				]).concat( (!rhino) ? ["-norhino"] : [] )
+			});
+			jsh.shell.java({
+				jar: to.file,
+				arguments: parameters.arguments,
+				environment: environment
+			});
+		}
+	}
+)();
--- rhino/tools/hg/test/suite.jsh.js
+++ rhino/tools/hg/test/suite.jsh.js
@@ -11,36 +11,40 @@
 //	Contributor(s):
 //	END LICENSE
 
-var parameters = jsh.script.getopts({
-	options: {
-		view: "console",
-		part: String
+(
+	function() {
+		var parameters = jsh.script.getopts({
+			options: {
+				view: "console",
+				part: String
+			}
+		});
+
+		var page = new jsh.unit.html.Part({
+			name: "module",
+			pathname: jsh.script.file.parent.parent.getRelativePath("api.html"),
+			environment: {}
+			//	TODO	reload converts suite to scenario, which has no parts, so this breaks the search-into-part behavior
+			//,reload: true
+		});
+
+		var path;
+
+		if (parameters.options.part) {
+			var ids = parameters.options.part.split("/");
+			path = page.getPath(ids);
+			path.unshift("module");
+		}
+
+		var suite = new jsh.unit.Suite({
+			parts: {
+				module: page
+			}
+		});
+
+		jsh.unit.interface.create(suite, {
+			view: parameters.options.view,
+			path: path
+		});
 	}
-});
-
-var page = new jsh.unit.html.Part({
-	name: "module",
-	pathname: jsh.script.file.parent.parent.getRelativePath("api.html"),
-	environment: {}
-	//	TODO	reload converts suite to scenario, which has no parts, so this breaks the search-into-part behavior
-	//,reload: true
-});
-
-var path;
-
-if (parameters.options.part) {
-	var ids = parameters.options.part.split("/");
-	path = page.getPath(ids);
-	path.unshift("module");
-}
-
-var suite = new jsh.unit.Suite({
-	parts: {
-		module: page
-	}
-});
-
-jsh.unit.interface.create(suite, {
-	view: parameters.options.view,
-	path: path
-});
+)();
