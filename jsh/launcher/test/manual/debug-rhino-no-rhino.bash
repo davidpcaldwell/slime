81d81f2d97f92fe1445a1e705de19373b92592e5
David P. Caldwell <david@davidpcaldwell.com>
2021-04-15 22:04:31 -0400

    #120: simplify test case

--- jsh/launcher/test/manual/debug-rhino-no-rhino.bash
+++ jsh/launcher/test/manual/debug-rhino-no-rhino.bash
@@ -1,5 +1,3 @@
 #!/bin/bash
 SLIME="$(dirname $0)/../../../.."
-rm ${SLIME}/local/jsh/lib/js.jar 2>/dev/null
-export JSH_DEBUG_SCRIPT=rhino
-${SLIME}/jsh.bash ${SLIME}/jsh/test/jsh-data.jsh.js
+env JSH_SHELL_LIB=/dev/null JSH_DEBUG_SCRIPT=rhino ${SLIME}/jsh.bash ${SLIME}/jsh/test/jsh-data.jsh.js
