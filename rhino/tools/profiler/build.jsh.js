3dc90a0c92e7e76c2d88c46aff6a218691bab2e9
David P. Caldwell <david@davidpcaldwell.com>
2021-08-19 15:21:11 -0400

    remove 'manifest' from global TypeScript scope

--- rhino/tools/profiler/build.jsh.js
+++ rhino/tools/profiler/build.jsh.js
@@ -4,80 +4,84 @@
 //
 //	END LICENSE
 
-var parameters = jsh.script.getopts({
-	options: {
-		javassist: jsh.file.Pathname,
-		to: jsh.file.Pathname
+(
+	function() {
+		var parameters = jsh.script.getopts({
+			options: {
+				javassist: jsh.file.Pathname,
+				to: jsh.file.Pathname
+			}
+		});
+
+		if (!jsh.tools || !jsh.tools.install) {
+			jsh.loader.plugins(jsh.shell.jsh.home.getSubdirectory("src").getRelativePath("jsh/tools/install"));
+		}
+		if (!parameters.options.javassist) {
+			parameters.options.javassist = jsh.tools.install.get({ url: "https://github.com/jboss-javassist/javassist/releases/download/rel_3_20_0_ga/javassist.jar" }).pathname;
+		}
+
+		if (!parameters.options.to) {
+			jsh.shell.echo("Required: -to");
+			jsh.shell.exit(1);
+		}
+
+		var tmp = jsh.shell.TMPDIR.createTemporary({ directory: true });
+
+		jsh.shell.echo("Compiling profiler to " + tmp + " ...");
+		jsh.java.tools.javac({
+			destination: tmp.getRelativePath("classes"),
+			classpath: jsh.file.Searchpath([parameters.options.javassist]),
+			sourcepath: jsh.file.Searchpath([
+				jsh.script.file.getRelativePath("java")
+			]),
+			arguments: [
+				jsh.script.file.getRelativePath("java/inonit/tools/Profiler.java")
+			]
+		});
+
+		//	TODO	replace need for jar tool
+
+		//	TODO	replace jsh.shell.shell
+
+		var bins = [];
+		if (jsh.shell.java.home.parent.getSubdirectory("bin")) bins.push(jsh.shell.java.home.parent.getRelativePath("bin"));
+		if (jsh.shell.java.home.getSubdirectory("bin")) bins.push(jsh.shell.java.home.getRelativePath("bin"));
+		var searchpath = jsh.file.Searchpath(bins);
+
+		var jar = searchpath.getCommand("jar");
+
+		jsh.shell.shell(
+			jar,
+			[
+				"xf", parameters.options.javassist
+			],
+			{
+				workingDirectory: tmp.getSubdirectory("classes")
+			}
+		);
+
+		var manifest = jsh.shell.TMPDIR.createTemporary({ suffix: "mf" });
+		manifest.pathname.write("Premain-Class: inonit.tools.Profiler\n", { append: false });
+		parameters.options.to.parent.createDirectory({
+			exists: function(dir) {
+				return false;
+			},
+			recursive: true
+		});
+		jsh.shell.shell(
+			jar,
+			[
+				"cfm", parameters.options.to,
+				manifest,
+				//	TODO	should list directory
+			].concat(
+				tmp.getSubdirectory("classes").list().map(function(node) { return node.pathname.basename })
+			),
+			{
+				workingDirectory: tmp.getSubdirectory("classes")
+			}
+		);
+
+		jsh.shell.console("Created JAR at " + parameters.options.to);
 	}
-});
-
-if (!jsh.tools || !jsh.tools.install) {
-	jsh.loader.plugins(jsh.shell.jsh.home.getSubdirectory("src").getRelativePath("jsh/tools/install"));
-}
-if (!parameters.options.javassist) {
-	parameters.options.javassist = jsh.tools.install.get({ url: "https://github.com/jboss-javassist/javassist/releases/download/rel_3_20_0_ga/javassist.jar" }).pathname;
-}
-
-if (!parameters.options.to) {
-	jsh.shell.echo("Required: -to");
-	jsh.shell.exit(1);
-}
-
-var tmp = jsh.shell.TMPDIR.createTemporary({ directory: true });
-
-jsh.shell.echo("Compiling profiler to " + tmp + " ...");
-jsh.java.tools.javac({
-	destination: tmp.getRelativePath("classes"),
-	classpath: jsh.file.Searchpath([parameters.options.javassist]),
-	sourcepath: jsh.file.Searchpath([
-		jsh.script.file.getRelativePath("java")
-	]),
-	arguments: [
-		jsh.script.file.getRelativePath("java/inonit/tools/Profiler.java")
-	]
-});
-
-//	TODO	replace need for jar tool
-
-//	TODO	replace jsh.shell.shell
-
-var bins = [];
-if (jsh.shell.java.home.parent.getSubdirectory("bin")) bins.push(jsh.shell.java.home.parent.getRelativePath("bin"));
-if (jsh.shell.java.home.getSubdirectory("bin")) bins.push(jsh.shell.java.home.getRelativePath("bin"));
-var searchpath = jsh.file.Searchpath(bins);
-
-var jar = searchpath.getCommand("jar");
-
-jsh.shell.shell(
-	jar,
-	[
-		"xf", parameters.options.javassist
-	],
-	{
-		workingDirectory: tmp.getSubdirectory("classes")
-	}
-);
-
-var manifest = jsh.shell.TMPDIR.createTemporary({ suffix: "mf" });
-manifest.pathname.write("Premain-Class: inonit.tools.Profiler\n", { append: false });
-parameters.options.to.parent.createDirectory({
-	exists: function(dir) {
-		return false;
-	},
-	recursive: true
-});
-jsh.shell.shell(
-	jar,
-	[
-		"cfm", parameters.options.to,
-		manifest,
-		//	TODO	should list directory
-	].concat(
-		tmp.getSubdirectory("classes").list().map(function(node) { return node.pathname.basename })
-	),
-	{
-		workingDirectory: tmp.getSubdirectory("classes")
-	}
-);
-
-jsh.shell.console("Created JAR at " + parameters.options.to);
+)();
