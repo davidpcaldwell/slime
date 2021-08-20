3ecf5a4fd391def8c37ce021ae49e3dc1bee2d8e
David P. Caldwell <david@davidpcaldwell.com>
2021-08-20 18:21:53 -0400

    Do not launch credential helper script from bash when github-tokens-directory is asked to store GitHub credentials

--- rhino/tools/github/git-credential-github-tokens-directory.bash
+++ rhino/tools/github/git-credential-github-tokens-directory.bash
@@ -7,4 +7,13 @@
 
 DIR="$(dirname $0)"
 SLIME="${DIR}/../../.."
+
+#	This credential helper does not store credentials; we assume the workflow is for these tokens to be obtained at github.com
+#	and then pasted into a local file. Credential helpers earlier in the process should have no way of getting at these
+#	credentials and providing them for this helper to store.
+
+if [ "$1" == "store" ]; then
+	exit 0
+fi
+
 ${SLIME}/jsh.bash ${DIR}/git-credential-github-tokens-directory.jsh.js "$@"
