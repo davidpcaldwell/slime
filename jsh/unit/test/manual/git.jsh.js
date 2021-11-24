deae2123e8beb7d8b42c74625c005e0e3094b8fe
David P. Caldwell <david@davidpcaldwell.com>
2021-11-24 18:44:25 -0500

    Remove 'to' from global TypeScript scope

--- jsh/unit/test/manual/git.jsh.js
+++ jsh/unit/test/manual/git.jsh.js
@@ -4,50 +4,54 @@
 //
 //	END LICENSE
 
-var parameters = jsh.script.getopts({
-	options: {
-		directory: jsh.file.Pathname
-	}
-});
+(
+	function() {
+		var parameters = jsh.script.getopts({
+			options: {
+				directory: jsh.file.Pathname
+			}
+		});
 
-jsh.java.tools.plugin.git();
+		jsh.java.tools.plugin.git();
 
-var name = parameters.options.directory.basename;
+		var name = parameters.options.directory.basename;
 
-var server = new jsh.httpd.Tomcat();
-server.map({
-	path: "/",
-	servlets: {
-		"/*": {
-			load: function(scope) {
-				var backend = new jsh.unit.mock.git.Server({
-					getLocation: function(path) {
-						//jsh.shell.console("path = " + path + " basename = " + name);
-						if (path.substring(0,name.length+1) == (name + "/")) {
-							return parameters.options.directory.parent.directory.getRelativePath(path);
+		var server = new jsh.httpd.Tomcat();
+		server.map({
+			path: "/",
+			servlets: {
+				"/*": {
+					load: function(scope) {
+						var backend = new jsh.unit.mock.git.Server({
+							getLocation: function(path) {
+								//jsh.shell.console("path = " + path + " basename = " + name);
+								if (path.substring(0,name.length+1) == (name + "/")) {
+									return parameters.options.directory.parent.directory.getRelativePath(path);
+								}
+							}
+						});
+
+						scope.$exports.handle = function(request) {
+							//jsh.shell.console("Got request for " + request.path);
+							return backend(request);
 						}
 					}
-				});
-
-				scope.$exports.handle = function(request) {
-					//jsh.shell.console("Got request for " + request.path);
-					return backend(request);
 				}
 			}
+		});
+		server.start();
+		if (false) {
+			jsh.shell.console("Starting server on port " + server.port + " ...");
+			server.run();
+		}
+
+		var to = jsh.shell.TMPDIR.createTemporary({ directory: true });
+		var remote = new git.Repository({ remote: "http://127.0.0.1:" + server.port + "/" + parameters.options.directory.basename });
+		try {
+			var local = remote.clone({ to: to.getRelativePath(name) });
+			jsh.shell.console("Cloned to " + to.getRelativePath(name));
+		} finally {
+			server.stop();
 		}
 	}
-});
-server.start();
-if (false) {
-	jsh.shell.console("Starting server on port " + server.port + " ...");
-	server.run();
-}
-
-var to = jsh.shell.TMPDIR.createTemporary({ directory: true });
-var remote = new git.Repository({ remote: "http://127.0.0.1:" + server.port + "/" + parameters.options.directory.basename });
-try {
-	var local = remote.clone({ to: to.getRelativePath(name) });
-	jsh.shell.console("Cloned to " + to.getRelativePath(name));
-} finally {
-	server.stop();
-}
+)();
