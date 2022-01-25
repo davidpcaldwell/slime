71be511d11a1958a25453e5fb1feaa21f1039323
David P. Caldwell <david@davidpcaldwell.com>
2022-01-24 22:13:35 -0500

    Improve slime.jrunscript.file namespace comment to include world-oriented information

--- rhino/file/_.fifty.ts
+++ rhino/file/_.fifty.ts
@@ -6,6 +6,13 @@
 
 /**
  * Provides access to filesystems.
+ *
+ * ## World-oriented implementation
+ *
+ * The {@link slime.jrunscript.file.World} interface, available as the `world` property of
+ * {@link slime.jrunscript.file.Exports | Exports}, provides a world-oriented interface to filesystems, beginning with the
+ * `filesystems.os` property, providing a {@link slime.jrunscript.file.world.Filesystem} implementation which provides a
+ * world-oriented API for accessing the host filesystem.
  */
 namespace slime.jrunscript.file {
 
