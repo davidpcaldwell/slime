12428d10805b5b7ccf5522889b071c962bd2a6fb
David P. Caldwell <david@davidpcaldwell.com>
2022-02-05 23:07:17 -0500

    Add ESLint report to help contributors work on remediating linting problems

---
+++ contributor/eslint-report.jsh.js
@@ -0,0 +1,87 @@
+//	LICENSE
+//	This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+//	distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//	END LICENSE
+
+//@ts-check
+(
+	/**
+	 * @param { slime.$api.Global } $api
+	 * @param { slime.jsh.Global } jsh
+	 */
+	function($api,jsh) {
+		var base = jsh.script.file.parent.parent;
+
+		var json = base.getFile(".eslintrc.json").read(String);
+		var configuration = eval("(" + json + ")");
+		var rules = Object.entries(configuration.rules).filter(function(entry) {
+			return entry[1] == "warn";
+		}).map(function(entry) {
+			return entry[0];
+		});
+		//jsh.shell.console("rules = " + Object.keys(configuration.rules));
+
+		jsh.shell.tools.node.require();
+
+		/**
+		 * @type { (node: any) => node is slime.jsh.shell.tools.node.Installed }
+		 */
+		function isInstalled(node) {
+			return true;
+		}
+
+		var node = jsh.shell.tools.node;
+
+		if (isInstalled(node)) {
+			node.modules.require({ name: "eslint" });
+
+			var results = node.run({
+				command: "eslint",
+				arguments: [/*"--debug",*/ "--format", "json", "."],
+				directory: base,
+				stdio: {
+					output: String
+				},
+				evaluate: function(result) {
+					return JSON.parse(result.stdio.output);
+				}
+			});
+
+			// var messages = results.reduce(function(rv,item) {
+			// 	return rv.concat(item.messages);
+			// },[]);
+
+			// jsh.shell.console(JSON.stringify(messages));
+			// jsh.shell.console(String(messages.length));
+
+			var warnings = results.reduce(function(rv,item) {
+				return rv.concat(
+					item.messages.map(function(message) {
+						return $api.Object.compose(message, {
+							file: item.filePath
+						})
+					})
+				)
+			},[]);
+
+			// jsh.shell.console(JSON.stringify(results, void(0), 4));
+			rules.map(function(name) {
+				return {
+					name: name,
+					violations: warnings.filter(function(warning) {
+						return warning.ruleId == name;
+					})
+				}
+			}).sort(function(a,b) {
+				return b.violations.length - a.violations.length;
+			}).forEach(function(rule) {
+				jsh.shell.console(rule.name + ": " + rule.violations.length);
+				rule.violations.forEach(function(violation) {
+					jsh.shell.console(violation.file + ":" + violation.line + ":" + violation.column + " " + violation.message);
+				})
+			});
+		}
+	}
+//@ts-ignore
+)($api,jsh);
