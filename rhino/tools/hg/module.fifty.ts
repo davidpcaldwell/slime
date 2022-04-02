a8365f5db256356cb0790ead70a0a2c1fb532032
David P. Caldwell <david@davidpcaldwell.com>
2022-04-02 15:47:59 -0400

    Rename slime.jrunscript.hg to slime.jrunscript.tools.hg

--- rhino/tools/hg/module.fifty.ts
+++ rhino/tools/hg/module.fifty.ts
@@ -4,7 +4,7 @@
 //
 //	END LICENSE
 
-namespace slime.jrunscript.hg {
+namespace slime.jrunscript.tools.hg {
 	export interface Commit {
 		changeset: {
 			local: number
@@ -19,18 +19,18 @@ namespace slime.jrunscript.hg {
 	}
 
 	export namespace Repository {
-		export interface Remote extends slime.jrunscript.hg.Repository {
+		export interface Remote extends slime.jrunscript.tools.hg.Repository {
 			url: any
 		}
 
-		export interface Local extends slime.jrunscript.hg.Repository {
+		export interface Local extends slime.jrunscript.tools.hg.Repository {
 			directory: slime.jrunscript.file.Directory
 			identify: Function
 			heads: () => any
 			log: (p?: any) => Commit[]
 			subrepositories: {
-				(): { [path: string]: { repository: slime.jrunscript.hg.Repository.Local, revision: string } }
-				({ array: boolean }): slime.jrunscript.hg.Repository.Local[]
+				(): { [path: string]: { repository: slime.jrunscript.tools.hg.Repository.Local, revision: string } }
+				({ array: boolean }): slime.jrunscript.tools.hg.Repository.Local[]
 			}
 			paths: {
 				default: {
