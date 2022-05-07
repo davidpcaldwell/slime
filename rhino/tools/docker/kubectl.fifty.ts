66be6133ba9be6e3529bc435fc8f4c2e640481f1
David P. Caldwell <david@davidpcaldwell.com>
2022-05-07 09:28:17 -0400

    Extract kubectl implementation from docker

---
+++ rhino/tools/docker/kubectl.fifty.ts
@@ -0,0 +1,17 @@
+namespace slime.jrunscript.tools.docker.internal.kubectl {
+	export interface Context {
+	}
+
+	(
+		function(
+			fifty: slime.fifty.test.Kit
+		) {
+			fifty.tests.suite = function() {
+
+			}
+		}
+	//@ts-ignore
+	)(fifty);
+
+	export type Script = slime.loader.Script<Context,slime.jrunscript.tools.kubectl.Exports>
+}
