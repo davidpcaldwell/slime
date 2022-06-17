3dc921605ee6b5373cced3f1f536a1619b28ee19
David P. Caldwell <david@davidpcaldwell.com>
2022-06-17 18:02:56 -0400

    Launch UI to view profiler JSON if possible
    Improves utility of manual test for #466 because UI automatically
    launches.

--- rhino/tools/profiler/viewer/viewer.fifty.ts
+++ rhino/tools/profiler/viewer/viewer.fifty.ts
@@ -17,7 +17,7 @@ namespace slime.jrunscript.tools.profiler.viewer {
 
 			fifty.tests.manual = {};
 
-			fifty.tests.manual.json = function() {
+			fifty.tests.manual.viewer = function() {
 				jsh.shell.world.run(
 					jsh.shell.Invocation.create({
 						command: jsh.shell.jsh.src.getRelativePath("jsh.bash").toString(),
@@ -28,33 +28,6 @@ namespace slime.jrunscript.tools.profiler.viewer {
 						})
 					})
 				)();
-			}
-
-			fifty.tests.manual.viewer = function() {
-				fifty.tests.manual.json();
-				var server = new jsh.httpd.Tomcat();
-				server.servlet({
-					load: function(scope) {
-						scope.$exports.handle = scope.httpd.Handler.Loader({
-							loader: new jsh.file.Loader({ directory: jsh.shell.jsh.src })
-						})
-					}
-				});
-				server.start();
-
-				//	TODO	this somewhat awkward series of API calls is caused by using the unit testing browser implementation,
-				//			which has a simpler API, rather than using the Chrome API directly. Should revisit.
-				var browser = jsh.unit.browser.local.Chrome({
-					program: jsh.shell.browser.installed.chrome.program,
-					user: jsh.shell.jsh.src.getRelativePath("local/chrome/profiler").toString(),
-					devtools: false,
-					debugPort: 9222
-				});
-
-				browser.open({
-					uri: "http://127.0.0.1:" + server.port + "/rhino/tools/profiler/viewer/viewer.html?profiles=../../../../local/profiler/profiles.json"
-				});
-				server.run();
 			}
 		}
 	//@ts-ignore
