7cdc3658e7a77e01110174d02f66cafc35173b2b
David P. Caldwell <david@davidpcaldwell.com>
2022-07-10 15:56:09 -0400

    Fix ESLint warnings, add type checking

--- jsh/test/manual/engine/echo.jsh.js
+++ jsh/test/manual/engine/echo.jsh.js
@@ -8,43 +8,57 @@
 //
 //	Expected results:
 //		under JDK 8, arguments -builder nashorn ==> should complete successfully
-var parameters = jsh.script.getopts({
-	options: {
-		builder: String,
-		engine: String
-	}
-});
-
-var javaCommands = jsh.file.Searchpath([jsh.shell.java.home.getRelativePath("bin")]);
-
-var SRC = jsh.script.file.getRelativePath("../../../..").directory;
-
-var TMP = jsh.shell.TMPDIR.createTemporary({ directory: true });
-
-var unbuiltCommand = SRC.getFile("jsh/etc/unbuilt.rhino.js");
-
-var jjs = javaCommands.getCommand("jjs");
-
-var builders = {};
-if (jjs) {
-	builders.nashorn = function() {
-		jsh.shell.run({
-			command: jjs,
-			arguments: [unbuiltCommand.toString(), "--", "build", TMP.toString()],
-			environment: {
-				JSH_BUILD_NOTEST: "true",
-				JSH_BUILD_NODOC: "true"
+//@ts-check
+(
+	/**
+	 *
+	 * @param { slime.jsh.Global } jsh
+	 */
+	function(jsh) {
+		if (true) {
+			jsh.shell.console("Script is outdated and needs to be updated and retested.");
+			jsh.shell.exit(1);
+		}
+		var parameters = jsh.script.getopts({
+			options: {
+				builder: String,
+				engine: String
 			}
 		});
-	};
-}
 
-builders[parameters.options.builder]();
+		var javaCommands = jsh.file.Searchpath([jsh.shell.java.home.getRelativePath("bin")]);
 
-jsh.shell.run({
-	command: jsh.shell.java.launcher,
-	arguments: ["-jar", TMP.getRelativePath("jsh.jar"), SRC.getRelativePath("jsh/test/jsh-data.jsh.js")],
-	environment: {
-		JSH_ENGINE: parameters.options.engine
+		var SRC = jsh.script.file.parent.getRelativePath("../../../..").directory;
+
+		var TMP = jsh.shell.TMPDIR.createTemporary({ directory: true });
+
+		var unbuiltCommand = SRC.getFile("jsh/etc/unbuilt.rhino.js");
+
+		var jjs = javaCommands.getCommand("jjs");
+
+		var builders = {};
+		if (jjs) {
+			builders.nashorn = function() {
+				jsh.shell.run({
+					command: jjs,
+					arguments: [unbuiltCommand.toString(), "--", "build", TMP.toString()],
+					environment: {
+						JSH_BUILD_NOTEST: "true",
+						JSH_BUILD_NODOC: "true"
+					}
+				});
+			};
+		}
+
+		builders[parameters.options.builder]();
+
+		jsh.shell.run({
+			command: jsh.shell.java.launcher,
+			arguments: ["-jar", TMP.getRelativePath("jsh.jar"), SRC.getRelativePath("jsh/test/jsh-data.jsh.js")],
+			environment: {
+				JSH_ENGINE: parameters.options.engine
+			}
+		});
 	}
-});
+//@ts-ignore
+)(jsh);
