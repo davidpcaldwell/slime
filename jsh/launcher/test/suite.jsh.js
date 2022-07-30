9f1472b4b46ab90735d7992c639f98636acae6fd
David P. Caldwell <david@davidpcaldwell.com>
2022-07-30 11:01:52 -0400

    #128: Migrate launcher suite toward library

--- jsh/launcher/test/suite.jsh.js
+++ jsh/launcher/test/suite.jsh.js
@@ -23,7 +23,7 @@
 				library: {
 					shell: jsh.shell
 				},
-				script: jsh.script.file,
+				script: jsh.script.file.parent.getFile("scenario.jsh.js"),
 				console: jsh.shell.console
 			})
 		};
@@ -55,121 +55,95 @@
 		});
 
 		jsh.loader.plugins(getSrc().getRelativePath("jsh/test"));
-		jsh.test.integration({
-			getopts: {
-				options: {
-					"shell:built": jsh.file.Pathname,
-				}
-			},
-			scenario: function(parameters) {
-				var home = library.script.getBuiltShellHomeDirectory({
-					context: getContext(),
-					built: parameters.options["shell:built"],
-					console: jsh.shell.console
-				});
 
-				var tmp = jsh.shell.TMPDIR.createTemporary({ directory: true });
-
-				var unbuilt = getUnbuilt();
-
-				/** @type { slime.jsh.internal.launcher.test.ShellImplementation } */
-				var built = {
-					type: "built",
-					shell: [
-						home.getRelativePath("jsh.js")
-					],
-					coffeescript: home.getFile("lib/coffee-script.js")
-				};
-
-				var addScenario = (
-					function() {
-						var index = 0;
-						return function(o) {
-							this.scenario(String(++index), {
-								create: function() {
-									this.name = o.name;
-
-									this.execute = function(scope,verify) {
-										o.execute(verify);
-									};
-								}
-							});
-						}
-					}
-				)().bind(this);
-
-				[unbuilt,built].forEach(function(implementation) {
-					var shell = (unbuilt) ? home : getSrc();
-					var id = ["unbuilt","built"][arguments[1]];
-
-					//	TODO	the below test does not pass under JDK 11; disabling it for later examination
-					// this.scenario(id, jsh.test.Suite({
-					// 	shell: shell,
-					// 	script: jsh.script.file.parent.getFile("options.jsh.js")
-					// }));
-
-					//	The was already commented-out when the above comment was written
-
-					// this.add({
-					// 	scenario: new jsh.unit.Scenario.Integration({
-					// 		shell: shell,
-					// 		script: jsh.script.file.parent.getFile("options.jsh.js")
-					// 	})
-					// });
-				},this);
-
-				getEngines().forEach(function(engine) {
-					[unbuilt,built].forEach(function(shell) {
-						var UNSUPPORTED = (engine == "rhino" && shell.coffeescript);
-						if (!UNSUPPORTED) {
-							addScenario(
-								library.script.toScenario(
-									parameters.options.rhino,
-									home,
-									tmp
-								)(
-									engine,
-									shell
-								)
-							);
-						}
-					});
-				},this);
-			},
-			run: function(parameters) {
-				var getProperty = function(name) {
-					var rv = Packages.java.lang.System.getProperty(name);
-					if (rv) return String(rv);
-					return null;
-				};
-
-				var home = (jsh.shell.jsh.home) ? jsh.shell.jsh.home.toString() : null;
-				var src = (jsh.shell.jsh.src) ? jsh.shell.jsh.src.toString() : null;
-				var logging = getProperty("java.util.logging.config.file");
-				var rhino = (function() {
-					var rv = {
-						running: (function() {
-							if (typeof(Packages.org.mozilla.javascript.Context) != "function") return false;
-							return Boolean(Packages.org.mozilla.javascript.Context.getCurrentContext());
-						})()
-					};
-					rv.optimization = (rv.running) ? Number(Packages.org.mozilla.javascript.Context.getCurrentContext().getOptimizationLevel()) : null;
-					rv.classpath = (jsh.shell.rhino && jsh.shell.rhino.classpath) ? String(jsh.shell.rhino.classpath) : null;
-					return rv;
-				})();
-				jsh.shell.echo(
-					JSON.stringify({
-						src: src,
-						home: home,
-						logging: logging,
-						foo1: getProperty("foo.1"),
-						foo2: getProperty("foo.2"),
-						tmp: String(jsh.shell.TMPDIR),
-						rhino: rhino
-					})
-				);
+		var parameters = jsh.script.getopts({
+			options: {
+				part: String,
+				view: "console",
+				"shell:built": jsh.file.Pathname
 			}
 		});
+
+		var suite = new jsh.unit.Suite({
+			name: jsh.script.file.pathname.basename
+		});
+
+		var home = library.script.getBuiltShellHomeDirectory({
+			context: getContext(),
+			built: parameters.options["shell:built"],
+			console: jsh.shell.console
+		});
+
+		var tmp = jsh.shell.TMPDIR.createTemporary({ directory: true });
+
+		var unbuilt = getUnbuilt();
+
+		/** @type { slime.jsh.internal.launcher.test.ShellImplementation } */
+		var built = {
+			type: "built",
+			shell: [
+				home.getRelativePath("jsh.js")
+			],
+			coffeescript: home.getFile("lib/coffee-script.js")
+		};
+
+		var addScenario = (
+			function() {
+				var index = 0;
+				return function(o) {
+					suite.scenario(String(++index), {
+						create: function() {
+							this.name = o.name;
+
+							this.execute = function(scope,verify) {
+								o.execute(verify);
+							};
+						}
+					});
+				}
+			}
+		)();
+
+		[unbuilt,built].forEach(function(implementation) {
+			var shell = (unbuilt) ? home : getSrc();
+			var id = ["unbuilt","built"][arguments[1]];
+
+			//	TODO	the below test does not pass under JDK 11; disabling it for later examination
+			// this.scenario(id, jsh.test.Suite({
+			// 	shell: shell,
+			// 	script: jsh.script.file.parent.getFile("options.jsh.js")
+			// }));
+
+			//	The was already commented-out when the above comment was written
+
+			// this.add({
+			// 	scenario: new jsh.unit.Scenario.Integration({
+			// 		shell: shell,
+			// 		script: jsh.script.file.parent.getFile("options.jsh.js")
+			// 	})
+			// });
+		},this);
+
+		getEngines().forEach(function(engine) {
+			[unbuilt,built].forEach(function(shell) {
+				var UNSUPPORTED = (engine == "rhino" && shell.coffeescript);
+				if (!UNSUPPORTED) {
+					addScenario(
+						library.script.toScenario(
+							parameters.options.rhino,
+							home,
+							tmp
+						)(
+							engine,
+							shell
+						)
+					);
+				}
+			});
+		},this);
+
+		var path = (parameters.options.part) ? parameters.options.part.split("/") : void(0);
+		jsh.unit.interface.create(suite, { view: parameters.options.view, path: path });
 	}
 //@ts-ignore
 )(Packages,$api,jsh);
