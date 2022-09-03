594a5da3361bb024dc989765990d92638ec4e324
David P. Caldwell <david@davidpcaldwell.com>
2022-09-03 07:23:04 -0400

    Fix crash when tar not present

--- jrunscript/tools/install/module.js
+++ jrunscript/tools/install/module.js
@@ -92,7 +92,7 @@
 			},
 			targz: {
 				extension: ".tgz",
-				extract: formats.gzip.extract
+				extract: algorithms.gzip.extract
 			}
 		};
 
@@ -405,6 +405,7 @@
 						)();
 
 						if (!format) throw new Error("Could not determine format of archive: " + archive.file);
+						if (!format.extract) throw new Error("No algorithm to extract " + format.extension);
 
 						//	TODO	what if directory exists? Right now will bomb, which may be OK
 						var to = p.to.createDirectory();
