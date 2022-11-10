6e4edd71363d846ee567b5bce4e51caf9cb4d251
David P. Caldwell <david@davidpcaldwell.com>
2022-11-10 00:11:51 -0500

    #322: Refactoring definitions

--- README.fifty.ts
+++ README.fifty.ts
@@ -5,29 +5,67 @@
 //	END LICENSE
 
 /**
- * ## Using SLIME
+ * ## Provided Platforms
  *
- * ### SLIME definitions (documentation and testing)
+ * ### Java
  *
- * SLIME has the concept of a _definition_, which is a construct that provides both documentation and
- * a test suite for a particular software module.
+ * SLIME's Java support allows applications to be written in JavaScript that call out to Java platform libraries. As such, it is an
+ * ideal platform for migrating Java software toward JavaScript.
  *
- * Documentation for SLIME itself is mostly contained in SLIME definitions that define its APIs.
+ * SLIME provides two Java platforms: the `jsh` scripting platform that runs on the JVM, and the Java servlet platform that runs on
+ * a standard Java servlet implementation. `jsh` also provides the ability to run a servlet container from within its shell.
  *
- * The best way to create SLIME definitions is via the {@link slime.fifty | Fifty}
- * definition framework, which uses TypeScript to provide type definitions and `tsdoc`-compatible
- * documentation (and uses [TypeDoc](https://typedoc.org/) to
- * publish that documentation, and the `fifty view` tool to serve it), and allows
- * inline tests to be authored within those TypeScript definitions. A simple example that contains tests
- * for the project's `wf` commands can be found at `./wf.fifty.ts`.
+ * #### `jsh`
  *
- * Many existing SLIME APIs are currently defined via the deprecated JSAPI definition format, which used literate definitions that
- * allowed documentation and tests to be defined via annotated HTML (typically using the file extension `.api.html`), using HTML
- * constructs for documentation and embedded scripts for testing.
+ * {@link slime.jsh | `jsh`} is a JavaScript application environment that runs on, and can access, the Java platform.
  *
- * ### Embedding
- * SLIME provides several embeddings (`jsh`, a Java servlet-based embedding, a browser embedding, and a JXA embedding for macOS
- * automation), and a simple Node.js embedding that primarily allows the use of the {@link slime.$api} tools in Node applications.
+ * #### SLIME Servlets
+ *
+ * {@link slime.servlet | SLIME Servlets} is a JavaScript environment for authoring Java servlets.
+ *
+ * #### Compatibility
+ *
+ * The SLIME Java runtime is compatible with the following Java environments.
+ *
+ * ##### JDK
+ *
+ * Current goal is to be compatible with version used by most Java developers per most recent [JetBrains
+ * survey](https://www.jetbrains.com/lp/devecosystem-2021/java/), which is currently **Java 8**.
+ *
+ * ##### JVM JavaScript engine
+ *
+ * The test suite executes tests primarily with **Rhino 1.7.14**, with some tests for the **Nashorn** engine on JDK 8. **GraalVM**
+ * is not yet supported, although [development is underway](https://github.com/davidpcaldwell/slime/projects/10).
+ *
+ * ##### Servlet containers
+ *
+ * SLIME Servlets is compatible with the Servlet 3.0.1 implementation.
+ *
+ * `jsh` currently supports **Tomcat 7.0.x** and is tested via the test suite. Updating to Tomcat 9 is on the roadmap via [issue
+ * 209](https://github.com/davidpcaldwell/slime/issues/209).
+ *
+ * ### Browser
+ *
+ * {@link slime.browser | SLIME Browser} is a SLIME environment for the browser. Browser support is less mature than Java support,
+ * but is currently tested in Google Chrome and Firefox.
+ *
+ * ### Node.js
+ *
+ * {@link slime.node | SLIME Node.js} is a SLIME environment for Node.js. Node.js support is new; currently, the Node environment
+ * simply loads the low-level SLIME runtime (providing SLIME's object-oriented and functional programming APIs) and provides access
+ * to it, as well as a simple filesystem-based {@link slime.Loader} implementation.
+ *
+ * ### JXA
+ *
+ * {@link slime.jxa | SLIME JXA} is a SLIME environment for use in macOS automation.
+ *
+ * JXA support is alpha quality. That said, JXA is a very difficult environment to use, so SLIME is already extremely helpful, as it
+ * provides basic abilities not provided by the platform (like the ability to load code from other source files).
+ *
+ * ### Creating a custom embedding
+ *
+ * SLIME provides several embeddings (`jsh`, a Java servlet-based embedding, a browser embedding, a JXA embedding for macOS
+ * automation), and a simple Node.js embedding.
  *
  * Custom SLIME embeddings may be developed by creating a suitable implementation of {@link slime.runtime.Scope} and putting that
  * object in scope when evaluating `loader/expression.js`, which yields an object of type {@link slime.runtime.Exports}.
@@ -35,6 +73,38 @@
  * The SLIME {@link slime.runtime.Exports | runtime} provides APIs that are ordinarily not available to application code directly, but are
  * provided to support embedders (who can provide them, in turn, to application code).
  *
+ * ## Using SLIME
+ *
+ * ### SLIME definitions (documentation and testing)
+ *
+ * SLIME has the concept of a _definition_, which is a construct that provides both documentation and a test suite for a particular
+ * software module.
+ *
+ * Documentation for SLIME itself is mostly contained in SLIME definitions that define its APIs.
+ *
+ * The best way to create SLIME definitions is via the {@link slime.fifty | Fifty} definition framework, which uses TypeScript to
+ * provide type definitions and `tsdoc`-compatible documentation (and uses [TypeDoc](https://typedoc.org/) to publish that
+ * documentation, and the `fifty view` tool to serve it), and allows inline tests to be authored within those TypeScript
+ * definitions. A simple example that contains tests for the project's `wf` commands can be found at `./wf.fifty.ts`.
+ *
+ * Some existing SLIME APIs are currently defined via the deprecated JSAPI definition format, which used literate definitions that
+ * allowed documentation and tests to be defined via annotated HTML (typically using the file extension `.api.html`), using HTML
+ * constructs for documentation and embedded scripts for testing.
+ *
+ * #### Running SLIME definition tests
+ *
+ * ##### Running Fifty definitions
+ *
+ * See the {@link slime.fifty.test | Fifty documentation}.
+ *
+ * ##### Running JSAPI definitions
+ *
+ * (**deprecated**) Running individual definitions in JSAPI:
+ *
+ * * `jsh`: `./jsh.bash jsh/test/suite.jsh.js -definition *definition* [-part *part*]`
+ * * Browser definition page: `./jsh.bash [loader/browser/test/suite.jsh.js](src/loader/browser/test/suite.jsh.api.html) -definition *pathname* [...]`
+ *
+ * (**deprecated**) Running a JSAPI browser suite: `./jsh.bash [loader/browser/test/suite.jsh.js](src/loader/browser/test/suite.jsh.api.html) -suite *pathname* [-base *directory*] [-interactive]`
  */
 namespace slime {
 	export interface Codec<T,E> {
--- README.md
+++ README.md
@@ -36,6 +36,29 @@ wget https://raw.githubusercontent.com/davidpcaldwell/slime/master/jsh.bash -O -
 
 ## Getting Started: Using SLIME locally
 
+### TL;DR
+
 * `git clone https://github.com/davidpcaldwell/slime.git; cd slime`
 * `./jsh.bash jsh/test/jsh-data.jsh.js` - runs a sample program emitting information about the installed `jsh` shell.
 * `./fifty view` - runs a server that serves the SLIME documentation and opens an instance of Google Chrome to browse it.
+
+### Getting the code
+
+To be able to work with SLIME locally, you'll want to get the code. SLIME is a scripting platform, and so it is written with the
+philosophy that essentially nothing should be prebuilt; it runs from its own source code. You can clone the source code from
+[GitHub](https://github.com/davidpcaldwell/slime) or
+[download](https://github.com/davidpcaldwell/slime/archive/refs/heads/master.zip) it and unzip it.
+
+### Executing `jsh` scripts
+
+At that point, it is ready to use; SLIME is capable of installing its own dependencies (including Java, Node.js, and TypeScript)
+over the internet.
+
+The top-level [`jsh.bash`](src/jsh.bash) script can be used to run scripts. `jsh` scripts that are top-level scripts and intended to
+be used as main programs are denoted by the suffix `.jsh.js`. The example `jsh-data.jsh.js` invocation above will run a script that
+will output a JSON data structure describing the shell.
+
+### Running the documentation server
+
+The documentation server can be run with the `./fifty view` command, which will start a server and Google Chrome browser for
+displaying SLIME documentation locally.
