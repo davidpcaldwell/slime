fb2c975af5406d51066143d3a544398e79f216b2
David P. Caldwell <david@davidpcaldwell.com>
2023-01-04 14:10:38 -0500

    jrunscript.shell: remove invocation.js
    Incorporate functionality into module.js to make it easier to organize
    further

--- rhino/shell/module.fifty.ts
+++ rhino/shell/module.fifty.ts
@@ -692,6 +692,72 @@ namespace slime.jrunscript.shell {
 		browser: slime.jrunscript.shell.browser.Exports
 	}
 
+	export interface Exports {
+		test: {
+			invocation: slime.jrunscript.shell.internal.invocation.Export
+		}
+	}
+
+	(
+		function(
+			fifty: slime.fifty.test.Kit
+		) {
+			const { verify } = fifty;
+			const { jsh } = fifty.global;
+			const subject: slime.jrunscript.shell.internal.invocation.Export = fifty.global.jsh.shell.test.invocation;
+
+			fifty.tests.invocation = fifty.test.Parent();
+
+			fifty.tests.invocation.sudo = function() {
+				var sudoed = subject.exports(void(0)).sudo({})(fifty.global.jsh.shell.Invocation.from.argument({
+					command: "ls"
+				}));
+
+				fifty.verify(sudoed).configuration.command.evaluate(String).is("sudo");
+				fifty.verify(sudoed).configuration.arguments[0].is("ls");
+				fifty.verify(sudoed).context.environment.evaluate.property("SUDO_ASKPASS").is(void(0));
+			};
+
+			fifty.tests.invocation.toBashScript = function() {
+				var parent = fifty.global.jsh.shell.environment;
+				fifty.verify(parent).evaluate.property("PATH").is.type("string");
+				fifty.verify(parent).evaluate.property("TO_BASH_SCRIPT_EXAMPLE").is(void(0));
+				var script = subject.invocation.toBashScript()({
+					command: "foo",
+					arguments: ["bar", "baz"],
+					directory: "/path/to/use",
+					environment: {
+						values: {
+							PATH: null,
+							TO_BASH_SCRIPT_EXAMPLE: "example"
+						}
+					}
+				});
+				var lines = script.split("\n");
+				fifty.verify(lines[0]).is("#!/bin/bash");
+				fifty.verify(lines[1]).is("cd /path/to/use");
+				fifty.verify(lines[2]).is("env -u PATH TO_BASH_SCRIPT_EXAMPLE=\"example\" foo bar baz");
+			}
+
+			fifty.tests.invocation.suite = function() {
+				fifty.run(function askpass() {
+					var sudoed = subject.exports(void(0)).sudo({
+						askpass: "/path/to/askpass"
+					})(jsh.shell.Invocation.from.argument({
+						command: "ls"
+					}));
+
+					verify(sudoed).configuration.command.evaluate(String).is("sudo");
+					verify(sudoed).configuration.arguments[0].is("--askpass");
+					verify(sudoed).configuration.arguments[1].is("ls");
+					verify(sudoed).context.environment.SUDO_ASKPASS.is("/path/to/askpass");
+				});
+			}
+		}
+	//@ts-ignore
+	)(fifty);
+
+
 	(
 		function(fifty: slime.fifty.test.Kit) {
 			fifty.tests.suite = function() {
@@ -702,11 +768,256 @@ namespace slime.jrunscript.shell {
 
 				fifty.run(fifty.tests.jsapi);
 
-				fifty.load("invocation.fifty.ts");
+				fifty.run(fifty.tests.invocation);
+
 				fifty.load("run.fifty.ts");
 				fifty.load("run-old.fifty.ts");
 			}
 		}
 	//@ts-ignore
 	)(fifty)
+}
+
+namespace slime.jrunscript.shell.internal.invocation {
+	export interface Context {
+		library: {
+			io: slime.jrunscript.io.Exports
+		}
+	}
+}
+
+namespace slime.jrunscript.shell {
+	export namespace invocation {
+		export interface Stdio {
+			input?: slime.jrunscript.runtime.io.InputStream
+			output?: Omit<slime.jrunscript.runtime.io.OutputStream, "close">
+			error?: Omit<slime.jrunscript.runtime.io.OutputStream, "close">
+		}
+
+		export type Input = string | slime.jrunscript.runtime.io.InputStream
+
+		export interface Argument {
+			/**
+			 * The command to run.
+			 */
+			command: string
+
+			/**
+			 * The arguments to supply to the command. If not present, an empty array will be supplied.
+			 */
+			arguments?: string[]
+
+			/**
+			 * The environment to supply to the command. If `undefined`, this process's environment will be provided.
+			 */
+			environment?: {
+				[name: string]: string
+			}
+
+			//	TODO	possibly allow Location
+			/**
+			 * The working directory in which the command will be executed. If not specified, this process's working directory
+			 * will be provided.
+			 */
+			directory?: string
+
+			stdio?: {
+				input?: Input
+				output?: run.OutputCapture
+				error?: run.OutputCapture
+			}
+		}
+
+		export namespace old {
+			export type Token = string | slime.jrunscript.file.Pathname | slime.jrunscript.file.Node
+
+			export type OutputStreamToStream = slime.jrunscript.runtime.io.OutputStream
+			export type OutputStreamToString = StringConstructor
+			export type OutputStreamToLines = { line: (line: string) => void }
+			export type OutputStreamConfiguration = OutputStreamToStream | OutputStreamToString | OutputStreamToLines
+
+			/**
+			 * Specifies the standard input, output, and error streams to use for an invocation.
+			 *
+			 * For the output streams:
+			 * * if the global `String` object is used as the value, the stream's output will be captured as a string and returned along with the result of the subprocess.
+			 * * if an object with a `line()` function is used as the value, the output will be buffered and the given object will receive a callback for each line of output.
+			 *
+			 * For the input stream:
+			 * * if the value is a string, that string will be provided on the standard input stream for the subprocess.
+			 */
+			export interface Stdio {
+				output?: OutputStreamConfiguration
+				error?: OutputStreamConfiguration
+				input?: Input
+			}
+
+			/**
+			 * Type used by callers to specify {@link slime.jrunscript.shell.old.Invocation}s, without requiring boilerplate defaults; only the `command`
+			 * property is required.
+			 */
+			export interface Argument {
+				command: slime.jrunscript.shell.invocation.Argument["command"] | slime.jrunscript.file.Pathname | slime.jrunscript.file.File
+				arguments?: Token[]
+				environment?: slime.jrunscript.shell.invocation.Argument["environment"]
+				directory?: slime.jrunscript.file.Directory
+
+				/**
+				 * The standard I/O streams to supply to the subprocess. If unspecified, or if any properties are unspecified,
+				 * defaults will be used. The defaults are:
+				 *
+				 * * `input`: `null`
+				 * * `output`: This process's standard output stream
+				 * * `error`: This process's standard error stream
+				 */
+				stdio?: slime.jrunscript.shell.older.Invocation["stdio"]
+			}
+		}
+	}
+
+	export namespace exports {
+		export interface Invocation {
+			from: {
+				argument: (p: invocation.Argument) => run.old.Invocation
+			}
+
+			/** @deprecated Use `from.argument`. */
+			create: Invocation["from"]["argument"]
+
+			//	TODO	probably should be conditional based on presence of sudo tool
+			/**
+			 * Given settings for `sudo`, converts an invocation into an equivalent invocation that will be run under `sudo`.
+			 *
+			 * @param settings A set of `sudo` settings.
+			 * @returns An invocation that will run the given invocation under `sudo`.
+			 */
+			sudo: (settings: sudo.Settings) => slime.$api.fp.Transform<run.old.Invocation>
+
+			handler: {
+				stdio: {
+					/**
+					 * Creates an event handler that automatically buffers trailing blank lines, so that blank lines created by the
+					 * end of a stream do not produce calls to the event handler.
+					 */
+					line: slime.$api.fp.Transform<slime.$api.event.Handler<slime.jrunscript.shell.run.Line>>
+				}
+			}
+		}
+	}
+
+	export interface Exports {
+		invocation: {
+			 /**
+			  * Creates the code for a `bash` script from a single Invocation-like object and returns it as a string.
+			  */
+			 toBashScript: () => (p: {
+				/**
+				 * The command to execute.
+				 */
+				command: string | slime.jrunscript.file.File
+
+				/**
+				 * Arguments to be sent to the command. If omitted, no arguments will be sent.
+				 */
+				arguments?: string[]
+
+				/**
+				 * The working directory to be used when executing the command. If omitted, the shell's current working directory
+				 * will be used.
+				 */
+				directory?: string | slime.jrunscript.file.Directory
+
+				/**
+				 * Configuration of the environment for the command. If omitted, the command will inherit the environment of the
+				 * invoking shell.
+				 */
+				environment?: {
+					/**
+					 * Whether to include the environment of the invoking shell. If `true`, the command's environment will include
+					 * the environment of the invoking shell. Defaults to `true`.
+					 */
+					inherit?: boolean
+
+					/**
+					 * Environment variables to be provided to the command, or to be removed from the environment of the command.
+					 * Properties with string values represent variables to be provided to the command (potentially overriding
+					 * values from the parent shell). Properties with `null` values represent variables to be **removed** from
+					 * the command's environment (even if they are present in the parent shell). Properties that are undefined
+					 * will have no effect.
+					 */
+					values: {
+						[x: string]: string | null
+					}
+				}
+			 }) => string
+		}
+	}
+
+	export namespace older {
+		/**
+		 * A fully-specified invocation of a command to be run in an external process.
+		 */
+		export interface Invocation {
+			/**
+			 * The command to run.
+			 */
+			command: string
+
+			/**
+			 * The arguments to pass to the command.
+			 */
+			arguments: string[]
+
+			/**
+			 * The environment to pass to the command.
+			 */
+			environment: invocation.Argument["environment"]
+
+			/**
+			 * The working directory to use when running the command.
+			 */
+			directory: slime.jrunscript.file.Directory
+
+			stdio: invocation.old.Stdio
+		}
+	}
+}
+
+namespace slime.jrunscript.shell.internal.invocation {
+	export type Configuration = Pick<slime.jrunscript.shell.invocation.old.Argument, "command" | "arguments">
+
+	export type StdioWithInputFixed = {
+		input: slime.jrunscript.runtime.io.InputStream
+		output: slime.jrunscript.shell.invocation.old.Stdio["output"]
+		error: slime.jrunscript.shell.invocation.old.Stdio["error"]
+	}
+
+	export interface Export {
+		exports: (defaults: shell.run.Parent) => slime.jrunscript.shell.exports.Invocation
+
+		internal: {
+			/**
+			 * Interface that is exposed because the old `run` interface uses it; see `run-old.js`.
+			 */
+			old: {
+				error: {
+					BadCommandToken: slime.$api.error.old.Type<"ArgumentError",{}>
+				}
+				updateForStringInput: (p: slime.jrunscript.shell.invocation.old.Stdio) => slime.jrunscript.shell.internal.invocation.StdioWithInputFixed
+
+				toStdioConfiguration: (declaration: slime.jrunscript.shell.internal.invocation.StdioWithInputFixed) => slime.jrunscript.shell.run.StdioConfiguration
+
+				parseCommandToken: {
+					(arg: slime.jrunscript.shell.invocation.old.Token, index?: number, ...args: any[]): string;
+					Error: slime.$api.error.old.Type<"ArgumentError", {}>;
+				}
+
+				isLineListener: (p: slime.jrunscript.shell.invocation.old.OutputStreamConfiguration) => p is slime.jrunscript.shell.invocation.old.OutputStreamToLines
+			}
+		}
+
+		invocation: slime.jrunscript.shell.Exports["invocation"]
+	}
+
+	export type Script = slime.loader.Script<Context,Export>
 }
--- rhino/shell/module.js
+++ rhino/shell/module.js
@@ -89,10 +89,284 @@
 			/** @type { slime.jrunscript.shell.internal.run.Script } */
 			run: $loader.script("run.js"),
 			/** @type { slime.jrunscript.shell.internal.run.old.Script } */
-			run_old: $loader.script("run-old.js"),
-			/** @type { slime.jrunscript.shell.internal.invocation.Script } */
-			invocation: $loader.script("invocation.js")
+			run_old: $loader.script("run-old.js")
 		};
+
+		/** @type { slime.jrunscript.shell.internal.invocation.Export } */
+		var invocation = (
+			function($api,$context) {
+				var parseCommandToken = (
+					function() {
+						var ArgumentError = $api.Error.old.Type({ name: "ArgumentError", extends: TypeError });
+
+						/**
+						 *
+						 * @param { slime.jrunscript.shell.invocation.old.Token } arg
+						 * @param { number } [index]
+						 * @returns { string }
+						 */
+						var rv = function(arg,index) {
+							if (arguments.length == 1) index = null;
+							var label = (typeof(index) == "number") ? "property 'arguments[" + String(index) + "]'" : "property 'command'";
+							if (typeof(arg) == "undefined") {
+								throw new ArgumentError(label + " cannot be undefined");
+							}
+							if (arg === null) throw new ArgumentError(label + " must not be null");
+							if (arg && typeof(arg) == "object") return String(arg);
+							//	TODO	the below check does not allow the empty string to be a token
+							if (arg && typeof(arg) == "string") return arg;
+							throw new ArgumentError(label + " is not a string nor an object that can be converted to string.");
+						}
+						rv.Error = ArgumentError;
+						return rv;
+					}
+				)();
+
+				/**
+				 *
+				 * @param { slime.jrunscript.shell.invocation.Input } p
+				 * @return { slime.jrunscript.runtime.io.InputStream }
+				 */
+				var toInputStream = function(p) {
+					if (typeof(p) == "string") {
+						var buffer = new $context.library.io.Buffer();
+						buffer.writeText().write(p);
+						buffer.close();
+						return buffer.readBinary();
+					} else {
+						return p;
+					}
+				}
+
+				/**
+				 * @param { slime.jrunscript.shell.invocation.old.Stdio } p
+				 * @return { slime.jrunscript.shell.internal.invocation.StdioWithInputFixed }
+				 */
+				var updateForStringInput = function(p) {
+					/** @type { slime.jrunscript.shell.run.StdioConfiguration } */
+					return {
+						input: toInputStream(p.input),
+						output: p.output,
+						error: p.error
+					};
+				}
+
+				/**
+				 * @param { slime.jrunscript.shell.invocation.old.OutputStreamConfiguration } configuration
+				 * @return { configuration is slime.jrunscript.shell.invocation.old.OutputStreamToLines }
+				 */
+				var isLineListener = function(configuration) {
+					return configuration && Object.prototype.hasOwnProperty.call(configuration, "line");
+				}
+
+				/**
+				 * @param { slime.jrunscript.shell.invocation.old.OutputStreamConfiguration } configuration
+				 * @return { configuration is slime.jrunscript.shell.invocation.old.OutputStreamToString }
+				 */
+				var isString = function(configuration) {
+					return configuration === String
+				};
+
+				/**
+				 * @param { slime.jrunscript.shell.invocation.old.OutputStreamConfiguration } configuration
+				 * @return { configuration is slime.jrunscript.shell.invocation.OutputStreamToStream }
+				 */
+				var isRaw = function(configuration) {
+					return true;
+				}
+
+				/** @type { (configuration: slime.jrunscript.shell.invocation.old.OutputStreamConfiguration) => slime.jrunscript.shell.run.OutputCapture } */
+				var toCapture = function(configuration) {
+					if (isLineListener(configuration)) {
+						return "line";
+					} else if (isString(configuration)) {
+						return "string";
+					} else {
+						return configuration;
+					}
+				}
+
+				/**
+				 *
+				 * @param { slime.jrunscript.shell.internal.invocation.StdioWithInputFixed } declaration
+				 * @return { slime.jrunscript.shell.run.StdioConfiguration }
+				 */
+				function toStdioConfiguration(declaration) {
+					return {
+						input: declaration.input,
+						output: toCapture(declaration.output),
+						error: toCapture(declaration.error)
+					};
+				}
+
+				/**
+				 *
+				 * @param { slime.jrunscript.host.Environment } environment
+				 * @param { slime.jrunscript.shell.sudo.Settings } settings
+				 * @returns { slime.jrunscript.host.Environment }
+				 */
+				var getEnvironmentToSudo = function(environment, settings) {
+					return $api.Object.compose(
+						environment,
+						(settings && settings.askpass) ? { SUDO_ASKPASS: settings.askpass } : {}
+					)
+				}
+
+				/**
+				 *
+				 * @param { string } command
+				 * @param { string[] } args
+				 * @param { slime.jrunscript.shell.sudo.Settings } settings
+				 * @returns { string[] }
+				 */
+				var getArgumentsToSudo = function(command, args, settings) {
+					return $api.Array.build(function(array) {
+						if (settings && settings.askpass) array.push("--askpass");
+						if (settings && settings.nocache) array.push("--reset-timestamp")
+						array.push(command);
+						array.push.apply(array, args);
+					});
+				}
+
+				/** @type { (defaults: slime.jrunscript.shell.run.Parent) => slime.jrunscript.shell.exports.Invocation["from"]["argument"] } */
+				var create = function(defaults) {
+					return function(p) {
+						return {
+							context: {
+								environment: (p.environment) ? p.environment : defaults.environment(),
+								directory: (p.directory) ? p.directory.toString() : defaults.directory(),
+								stdio: {
+									input: (function() {
+										if (p.stdio && p.stdio.input) return toInputStream(p.stdio.input);
+										return null;
+									})(),
+									output: (p.stdio && p.stdio.output) ? p.stdio.output : defaults.stdio().output,
+									error: (p.stdio && p.stdio.error) ? p.stdio.error : defaults.stdio().error,
+								}
+							},
+							configuration: {
+								command: String(p.command),
+								arguments: (p.arguments) ? p.arguments.map(String) : []
+							}
+						}
+					}
+				};
+
+				/** @type { slime.jrunscript.shell.internal.invocation.Export["internal"]["old"] } */
+				var internal = (
+					function() {
+						return {
+							error: {
+								BadCommandToken: parseCommandToken.Error
+							},
+							updateForStringInput: updateForStringInput,
+							toStdioConfiguration: toStdioConfiguration,
+							parseCommandToken: parseCommandToken,
+							isLineListener: isLineListener
+						}
+					}
+				)();
+
+				return {
+					exports: function(defaults) {
+						//	TODO	this being undefined is just for testing at the moment, should think through how to make this less kludgy
+						var withDefaults = (defaults) ? create(defaults) : void(0);
+						return {
+							from: {
+								argument: withDefaults
+							},
+							create: (withDefaults) ? $api.deprecate(withDefaults) : void(0),
+							//	TODO	sudo has preserve-env and preserver-env= flags. Should make the relationship more explicit
+							//			between that and the environment provided normally, e.g., how could we pass an explicit environment
+							//			to sudo? Maybe by transforming the command into an `env` command?
+							sudo: function(settings) {
+								return function(invocation) {
+									return {
+										context: {
+											environment: getEnvironmentToSudo(invocation.context.environment, settings),
+											directory: invocation.context.directory,
+											stdio: invocation.context.stdio
+										},
+										configuration: {
+											command: "sudo",
+											arguments: getArgumentsToSudo(invocation.configuration.command, invocation.configuration.arguments, settings)
+										}
+									}
+								}
+							},
+							handler: {
+								stdio: {
+									line: function(f) {
+										var lastBlank = null;
+
+										return function(e) {
+											if (lastBlank) {
+												f(lastBlank);
+												lastBlank = null;
+											}
+											if (e.detail.line == "") {
+												lastBlank = e;
+											} else {
+												f(e);
+											}
+										}
+									}
+								}
+							}
+						}
+					},
+					invocation: {
+						//	The returned function is wrapped in this function because one could envision this function someday having
+						//	arguments containing some sort of information about how the script should be authored, maybe the path
+						//	to bash (which is different on FreeBSD), and so forth.
+						toBashScript: function() {
+							/** @type { ReturnType<slime.jrunscript.shell.Exports["invocation"]["toBashScript"]> } */
+							var toScriptCode = function(invocation) {
+								return $api.Array.build(function(script) {
+									script.push("#!/bin/bash");
+
+									if (invocation.directory) {
+										if (typeof(invocation.directory) == "string") {
+											script.push("cd " + invocation.directory);
+										} else {
+											script.push("cd " + invocation.directory.pathname.toString());
+										}
+									}
+
+									/** @type { Parameters<ReturnType<slime.jrunscript.shell.Exports["invocation"]["toBashScript"]>>[0]["environment"]} */
+									var environment = (invocation.environment) || { inherit: void(0), values: {} };
+									var inherit = (typeof(environment.inherit) == "undefined") ? true : environment.inherit;
+									var values = (typeof(environment.values) == "undefined") ? {} : environment.values;
+									var set = Object.entries(values).filter(function(entry) {
+										return typeof(entry[1]) == "string";
+									});
+									var unset = Object.entries(values).filter(function(entry) {
+										return entry[1] === null;
+									});
+
+									script.push($api.Array.build(function(rv) {
+										if (!inherit || set.length || unset.length) rv.push("env");
+										unset.forEach(function(entry) {
+											rv.push("-u", entry[0]);
+										});
+										set.forEach(function(entry) {
+											rv.push(entry[0] + "=" + "\"" + entry[1] + "\"");
+										});
+										rv.push(invocation.command);
+										if (invocation.arguments) rv.push.apply(rv, invocation.arguments);
+									}).join(" "))
+								}).join("\n");
+							};
+
+							return toScriptCode;
+						}
+					},
+					internal: {
+						old: internal
+					}
+				};
+			}
+		)($api,{ library: { io: $context.api.io }})
 
 		var scripts = (function() {
 			var run = code.run({
@@ -102,11 +376,6 @@
 					file: $context.api.file
 				},
 				spi: ($context.world && $context.world.subprocess)
-			});
-			var invocation = code.invocation({
-				library: {
-					io: $context.api.io
-				}
 			});
 			return {
 				run: run,
@@ -129,14 +398,13 @@
 						invocation: invocation
 					},
 					stdio: $context.stdio
-				}),
-				invocation: invocation
+				})
 			}
 		})();
 
 		$exports.run = scripts.run_old.run;
 
-		$exports.invocation = scripts.invocation.invocation;
+		$exports.invocation = invocation.invocation;
 
 		var embed = $api.events.Function(
 			/**
@@ -512,7 +780,7 @@
 			})
 		}
 
-		$exports.Invocation = scripts.invocation.exports(defaults);
+		$exports.Invocation = invocation.exports(defaults);
 
 		$exports.world = scripts.run;
 
@@ -558,7 +826,10 @@
 			Tell: $exports.Tell,
 			run: $exports.run,
 			PATH: $exports.PATH,
-			browser: $exports.browser
+			browser: $exports.browser,
+			test: {
+				invocation: invocation
+			}
 		}
 
 		$export(x);
