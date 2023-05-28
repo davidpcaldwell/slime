770b6c3dc1f4c3a4fa2a6b1c21bb544ca9fb3484
David P. Caldwell <david@davidpcaldwell.com>
2023-05-28 16:20:44 -0400

    Fix failing JSAPI definition

--- jrunscript/jsh/script/test/loader.jsh.js
+++ jrunscript/jsh/script/test/loader.jsh.js
@@ -6,7 +6,7 @@
 
 (
 	function() {
-		var loader = new jsh.script.Loader("../../..");
+		var loader = new jsh.script.Loader("../../../..");
 		jsh.shell.console("loader = " + loader);
 		var a = loader.module("loader/test/data/b/");
 		jsh.shell.echo(JSON.stringify({
