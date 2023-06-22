a0663b63dbbec37ea0f263b56f09a5d4d594f408
David P. Caldwell <david@davidpcaldwell.com>
2023-06-21 21:32:27 -0400

    Try to split rhino/shell from jsh.shell

---
+++ jrunscript/jsh/shell/plugin.jsh.js
@@ -0,0 +1,167 @@
+//	LICENSE
+//	This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+//	distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//	END LICENSE
+
+//@ts-check
+(
+	/**
+	 *
+	 * @param { slime.jrunscript.Packages } Packages
+	 * @param { slime.$api.Global } $api
+	 * @param { slime.jsh.plugin.Scope["plugins"] } plugins
+	 * @param { slime.jsh.Global } jsh
+	 * @param { slime.jsh.plugin.$slime } $slime
+	 * @param { slime.Loader } $loader
+	 * @param { slime.jsh.plugin.plugin } plugin
+	 */
+	function(Packages,$api,plugins,jsh,$slime,$loader,plugin) {
+		plugin({
+			isReady: function() {
+				return Boolean(plugins.shell && plugins.stdio && jsh.script);
+			},
+			load: function() {
+				/**
+				 * @type { slime.jsh.shell.internal.Context }
+				 */
+				var context = {
+					api: {
+						js: jsh.js
+						,java: jsh.java
+						,io: jsh.io
+						,file: jsh.file
+						,script: jsh.script
+					},
+					stdio: plugins.stdio,
+					_getSystemProperties: function() {
+						return $slime.getSystemProperties();
+					},
+					exit: function(code) {
+						$slime.exit(code);
+					},
+					jsh: function(configuration,script,args) {
+						var _invocation = $slime.getInterface().invocation(
+							script.pathname.java.adapt(),
+							jsh.java.toJavaArray(args,Packages.java.lang.String,function(s) {
+								return new Packages.java.lang.String(s);
+							})
+						);
+						return $slime.jsh(configuration,_invocation)
+					},
+					module: plugins.shell
+				};
+
+				/** @type { slime.jsh.shell.internal.Script } */
+				var script = $loader.script("jsh.js");
+
+				jsh.shell = script(
+					context
+				);
+			}
+		});
+
+		plugin({
+			isReady: function() {
+				return Boolean(jsh.file && jsh.shell);
+			},
+			load: function() {
+				var typescriptInstalled = jsh.shell.jsh.lib && jsh.shell.jsh.lib.getFile("node/bin/tsc");
+
+				if (typescriptInstalled) {
+					/** @type { slime.jsh.shell.internal.tsc.Script } */
+					var script = $loader.script("tsc.js");
+					var api = script({
+						node: jsh.shell.jsh.lib.getRelativePath("node").toString(),
+						tsc: jsh.shell.jsh.lib.getRelativePath("node/bin/tsc").toString(),
+						library: {
+							file: jsh.file,
+							shell: jsh.shell
+						}
+					});
+
+					/** @type { slime.runtime.loader.Compiler } */
+					var subprocess = $api.compiler.getTranspiler({
+						accept: $api.compiler.isMimeType("application/x.typescript"),
+						compile: api.compile
+					});
+
+					$slime.compiler.update(
+						function(was) {
+							return $api.fp.switch([
+								subprocess,
+								was
+							])
+						}
+					)
+				}
+			}
+		});
+
+		plugin({
+			isReady: function() {
+				return Boolean(jsh.shell && jsh.ui && jsh.shell.os);
+			},
+			load: function() {
+				if (jsh.shell.os.sudo) jsh.shell.os.sudo.gui = function(p) {
+					if (!p) p = { prompt: void(0) };
+					if (!p.prompt) p.prompt = "Account password for " + jsh.shell.environment.USER + ":";
+					return function() {
+						return jsh.ui.askpass.gui({ prompt: p.prompt });
+					};
+				}
+				if (jsh.shell.os.inject) jsh.shell.os.inject({ ui: jsh.ui });
+			}
+		});
+
+		plugin({
+			isReady: function() {
+				return Boolean(jsh.shell && jsh.httpd);
+			},
+			load: function() {
+				jsh.shell.browser.inject({ httpd: jsh.httpd });
+			}
+		});
+
+		plugin({
+			isReady: function() {
+				return Boolean(jsh.js && jsh.shell && jsh.shell.jsh && jsh.script)
+			},
+			load: function() {
+				jsh.shell.jsh.debug = function(p) {
+					var isRhino = (function() {
+						//	TODO	probably a way to get this from rhino/jrunscript/api.js
+						if (!jsh.java.getClass("org.mozilla.javascript.Context")) return false;
+						if (!Packages.org.mozilla.javascript.Context.getCurrentContext()) return false;
+						return true;
+					})();
+
+					//	TODO	probably want to build these arguments better so that other jsh.shell.jsh arguments like stdio and
+					//			environment can also be used and still work
+
+					var evaluate = function(result) {
+						jsh.shell.exit(result.status);
+					};
+
+					if (isRhino) {
+						jsh.shell.jsh({
+							script: jsh.script.file,
+							arguments: jsh.script.arguments,
+							environment: jsh.js.Object.set({}, jsh.shell.environment, {
+								JSH_DEBUG_SCRIPT: "rhino"
+							}),
+							evaluate: evaluate
+						})
+					} else {
+						jsh.shell.jsh({
+							script: jsh.shell.jsh.src.getFile("jsh/tools/ncdbg.jsh.js"),
+							arguments: [jsh.script.file.toString()].concat(jsh.script.arguments),
+							evaluate: evaluate
+						});
+					}
+				}
+			}
+		});
+	}
+//@ts-ignore
+)(Packages,$api,plugins,jsh,$slime,$loader,plugin);
--- rhino/shell/plugin.jsh.js
+++ rhino/shell/plugin.jsh.js
@@ -8,7 +8,6 @@
 (
 	/**
 	 *
-	 * @param { slime.jrunscript.Packages } Packages
 	 * @param { slime.$api.Global } $api
 	 * @param { slime.jsh.plugin.Scope["plugins"] } plugins
 	 * @param { slime.jsh.Global } jsh
@@ -16,14 +15,7 @@
 	 * @param { slime.Loader } $loader
 	 * @param { slime.jsh.plugin.plugin } plugin
 	 */
-	function(Packages,$api,plugins,jsh,$slime,$loader,plugin) {
-		var code = {
-			/** @type { slime.jrunscript.shell.Script } */
-			module: $loader.script("module.js"),
-			/** @type { slime.jsh.shell.internal.Script } */
-			jsh: $loader.script("jsh.js")
-		}
-
+	function($api,plugins,jsh,$slime,$loader,plugin) {
 		plugin({
 			isReady: function() {
 				return Boolean(jsh.js && jsh.document && jsh.js.document && jsh.web && jsh.java && jsh.io && jsh.file);
@@ -79,7 +71,10 @@
 					stdio: stdio
 				};
 
-				var module = code.module(mContext);
+				/** @type { slime.jrunscript.shell.Script } */
+				var script = $loader.script("module.js");
+
+				var module = script(mContext);
 
 				if (!module.properties) throw new TypeError();
 
@@ -88,149 +83,6 @@
 				plugins.stdio = stdio;
 			}
 		});
-
-		plugin({
-			isReady: function() {
-				return Boolean(plugins.shell && plugins.stdio && jsh.script);
-			},
-			load: function() {
-				/**
-				 * @type { slime.jsh.shell.internal.Context }
-				 */
-				var context = {
-					api: {
-						js: jsh.js
-						,java: jsh.java
-						,io: jsh.io
-						,file: jsh.file
-						,script: jsh.script
-					},
-					stdio: plugins.stdio,
-					_getSystemProperties: function() {
-						return $slime.getSystemProperties();
-					},
-					exit: function(code) {
-						$slime.exit(code);
-					},
-					jsh: function(configuration,script,args) {
-						var _invocation = $slime.getInterface().invocation(
-							script.pathname.java.adapt(),
-							jsh.java.toJavaArray(args,Packages.java.lang.String,function(s) {
-								return new Packages.java.lang.String(s);
-							})
-						);
-						return $slime.jsh(configuration,_invocation)
-					},
-					module: plugins.shell
-				};
-
-				jsh.shell = code.jsh(
-					context
-				);
-			}
-		});
-
-		plugin({
-			isReady: function() {
-				return Boolean(jsh.file && jsh.shell);
-			},
-			load: function() {
-				var typescriptInstalled = jsh.shell.jsh.lib && jsh.shell.jsh.lib.getFile("node/bin/tsc");
-
-				if (typescriptInstalled) {
-					/** @type { slime.jsh.shell.internal.tsc.Script } */
-					var script = $loader.script("tsc.js");
-					var api = script({
-						node: jsh.shell.jsh.lib.getRelativePath("node").toString(),
-						tsc: jsh.shell.jsh.lib.getRelativePath("node/bin/tsc").toString(),
-						library: {
-							file: jsh.file,
-							shell: jsh.shell
-						}
-					});
-
-					/** @type { slime.runtime.loader.Compiler } */
-					var subprocess = $api.compiler.getTranspiler({
-						accept: $api.compiler.isMimeType("application/x.typescript"),
-						compile: api.compile
-					});
-
-					$slime.compiler.update(
-						function(was) {
-							return $api.fp.switch([
-								subprocess,
-								was
-							])
-						}
-					)
-				}
-			}
-		});
-
-		plugin({
-			isReady: function() {
-				return Boolean(jsh.shell && jsh.ui && jsh.shell.os);
-			},
-			load: function() {
-				if (jsh.shell.os.sudo) jsh.shell.os.sudo.gui = function(p) {
-					if (!p) p = { prompt: void(0) };
-					if (!p.prompt) p.prompt = "Account password for " + jsh.shell.environment.USER + ":";
-					return function() {
-						return jsh.ui.askpass.gui({ prompt: p.prompt });
-					};
-				}
-				if (jsh.shell.os.inject) jsh.shell.os.inject({ ui: jsh.ui });
-			}
-		});
-
-		plugin({
-			isReady: function() {
-				return Boolean(jsh.shell && jsh.httpd);
-			},
-			load: function() {
-				jsh.shell.browser.inject({ httpd: jsh.httpd });
-			}
-		});
-
-		plugin({
-			isReady: function() {
-				return Boolean(jsh.js && jsh.shell && jsh.shell.jsh && jsh.script)
-			},
-			load: function() {
-				jsh.shell.jsh.debug = function(p) {
-					var isRhino = (function() {
-						//	TODO	probably a way to get this from rhino/jrunscript/api.js
-						if (!jsh.java.getClass("org.mozilla.javascript.Context")) return false;
-						if (!Packages.org.mozilla.javascript.Context.getCurrentContext()) return false;
-						return true;
-					})();
-
-					//	TODO	probably want to build these arguments better so that other jsh.shell.jsh arguments like stdio and
-					//			environment can also be used and still work
-
-					var evaluate = function(result) {
-						jsh.shell.exit(result.status);
-					};
-
-					if (isRhino) {
-						jsh.shell.jsh({
-							script: jsh.script.file,
-							arguments: jsh.script.arguments,
-							environment: jsh.js.Object.set({}, jsh.shell.environment, {
-								JSH_DEBUG_SCRIPT: "rhino"
-							}),
-							evaluate: evaluate
-						})
-					} else {
-						jsh.shell.jsh({
-							script: jsh.shell.jsh.src.getFile("jsh/tools/ncdbg.jsh.js"),
-							arguments: [jsh.script.file.toString()].concat(jsh.script.arguments),
-							evaluate: evaluate
-						});
-					}
-				}
-			}
-		});
 	}
 //@ts-ignore
-)(Packages,$api,plugins,jsh,$slime,$loader,plugin);
+)($api,plugins,jsh,$slime,$loader,plugin);
