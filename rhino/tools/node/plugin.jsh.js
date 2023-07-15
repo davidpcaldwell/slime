8a27049b73638f9bdf9204c05b14b5e8c3cdcf59
David P. Caldwell <david@davidpcaldwell.com>
2023-07-14 21:26:39 -0400

    Simplify structure of Node.js jsh plugin

---
+++ rhino/tools/node/plugin.jsh.js
@@ -0,0 +1,31 @@
+//	LICENSE
+//	This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+//	distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//	END LICENSE
+
+//@ts-check
+(
+	/**
+	 * @param { slime.jsh.plugin.plugins } plugins
+	 * @param { slime.Loader } $loader
+	 * @param { slime.jsh.plugin.plugin } plugin
+	 */
+	function(plugins,$loader,plugin) {
+		plugin({
+			isReady: function() {
+				return true;
+			},
+			load: function() {
+				plugins.node = {
+					module: function(p) {
+						/** @type { slime.jrunscript.node.Script } */
+						var script = $loader.script("module.js");
+						return script(p.context);
+					}
+				};
+			}
+		});
+	}
+//@ts-ignore
+)(plugins,$loader,plugin);
