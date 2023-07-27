07d519c55db9bf57c184a49df5c8cc617b3d4ee1
David P. Caldwell <david@davidpcaldwell.com>
2023-07-27 07:35:51 -0400

    Begin migrating Fifty internal docmentation

---
+++ tools/fifty/internal.fifty.ts
@@ -0,0 +1,16 @@
+//	LICENSE
+//	This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+//	distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//	END LICENSE
+
+/**
+ * ## Launcher
+ * * `./fifty`
+ * * `./tools/fifty.jsh.js`
+ *     * `view`
+ *     * `test.jsh`
+ *     * `test.browser`
+ */
+namespace slime.fifty.internal {
+}
