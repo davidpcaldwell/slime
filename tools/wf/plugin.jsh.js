32b1f79a475bb3f8a1e9804a9758a1a4aa134159
David P. Caldwell <david@davidpcaldwell.com>
2023-08-02 09:01:17 -0400

    Fix typescript.require to relaunch jsh on install

--- tools/wf/plugin.jsh.js
+++ tools/wf/plugin.jsh.js
@@ -494,11 +494,41 @@
 					return {
 						require: function(p) {
 							var project = (p && p.project) ? p.project : inputs.base();
-							$api.fp.world.execute(jsh.shell.tools.node.require());
-							jsh.shell.tools.node.installed.modules.require({
-								name: "typescript",
-								version: library.module.Project.getTypescriptVersion({ base: project.toString() })
-							});
+							$api.fp.world.now.tell(jsh.shell.tools.node.require());
+							var installation = jsh.shell.tools.node.installation;
+							var version = library.module.Project.getTypescriptVersion({ base: project.toString() });
+							//	We use jsh.shell.jsh.require to make sure shell relaunches, so that TypeScript can be
+							//	loaded by SLIME in the resulting shell.
+							//	TODO	use newer jsh.shell APIs
+							$api.fp.world.now.tell(
+								jsh.shell.jsh.require({
+									satisfied: function() {
+										var installed = $api.fp.world.now.question(
+											jsh.shell.tools.node.Installation.modules.installed("typescript"),
+											installation
+										);
+										if (installed.present) {
+											return installed.value.version == version
+										} else {
+											return false;
+										}
+									},
+									install: function() {
+										jsh.shell.tools.node.installed.modules.require({
+											name: "typescript",
+											version: version
+										});
+									}
+								}),
+								{
+									installing: function(e) {
+										jsh.shell.console("Installing TypeScript ...");
+									},
+									installed: function(e) {
+										jsh.shell.console("Installed TypeScript " + version + ".");
+									}
+								}
+							);
 						},
 						tsc: function(p) {
 							var project = (p && p.project) ? p.project : inputs.base();
