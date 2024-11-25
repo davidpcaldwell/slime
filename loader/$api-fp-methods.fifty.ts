2adf28df37996a27b0a0ae9b5f893bae138f110d
David P. Caldwell <david@davidpcaldwell.com>
2024-11-24 22:25:28 -0500

    Finish reasonable definitions of FP method analogues

--- loader/$api-fp-methods.fifty.ts
+++ loader/$api-fp-methods.fifty.ts
@@ -11,12 +11,16 @@ namespace slime.$api.fp.methods {
 	export interface Exports {
 	}
 
-	export interface Methods<T> {
+	export interface Operations<T> {
 		[x: string]: (t: T) => any
 	}
 
-	export type Specified<C,M extends Methods<C>> = {
-		[Property in keyof M]: ReturnType<M[Property]>
+	export type Specified<T,O extends Operations<T>> = {
+		[Property in keyof O]: () => ReturnType<O[Property]>
+	}
+
+	export type Flattened<T,O extends Operations<T>> = {
+		[Property in keyof O]: ReturnType<O[Property]>
 	}
 
 	export namespace fixture {
@@ -46,8 +50,15 @@ namespace slime.$api.fp.methods {
 			fifty: slime.fifty.test.Kit
 		) {
 			const { verify } = fifty;
+			const { $api, jsh } = fifty.global;
 
-			var specify = function<C>(c: C): <M extends Methods<C>> (methods: M) => Specified<C,M> {
+			var console = (fifty.global.jsh) ? function(message) {
+				jsh.shell.console(message);
+			} : function(message) {
+				//	do nothing in browser for now
+			}
+
+			var specify = function<T>(t: T): <O extends Operations<T>> (operations: O) => Specified<T,O> {
 				return function(methods) {
 					return Object.fromEntries(
 						Object.entries(methods).map(function(entry) {
@@ -55,16 +66,41 @@ namespace slime.$api.fp.methods {
 								entry[0],
 								//	TODO	would be nice to defer implementation of this, but might require use of
 								//			Object.defineProperty
-								entry[1](c)
+								function() {
+									return entry[1](t);
+								}
 							]
 						})
-					) as Specified<C,typeof methods>;
+					) as Specified<T,typeof methods>;
+				}
+			};
+
+			var flatten = function<T,O extends Operations<T>>(specified: Specified<T,O>): Flattened<T,O> {
+				var rv: slime.external.lib.typescript.Partial<Flattened<T,O>> = {};
+				Object.entries(specified).forEach(function(entry) {
+					rv = $api.Object.defineProperty({
+						name: entry[0],
+						descriptor: {
+							enumerable: true,
+							get: function() {
+								return entry[1]();
+							}
+						}
+					})(rv);
+				})
+				return rv as Flattened<T,O>;
+			}
+
+			var pin = function<T>(t: T): <O extends Operations<T>> (operations: O) => Flattened<T,O> {
+				return function(operations) {
+					return flatten(specify(t)(operations));
 				}
 			};
 
 			var implementation: fixture.Module["Bar"] = {
 				asString: function(context) {
 					return function(bar) {
+						console("asString: value=" + bar.value + " scale=" + context.scale);
 						return String(bar.value * context.scale);
 					}
 				},
@@ -84,16 +120,32 @@ namespace slime.$api.fp.methods {
 				var contextualized = specify({ scale: 2 });
 
 				var configured = contextualized(implementation);
+				console("configured");
 				const bar: fixture.Bar = { value: 2 };
-				var s = configured.asString(bar);
+				var s = configured.asString()(bar);
 				verify(s).is("4");
-				var n = configured.asNumber(bar);
+				var n = configured.asNumber()(bar);
 				verify(n).is(4);
 
-				var targeted = specify(bar)(configured);
-				verify(targeted).asString.is("4");
-				verify(targeted).asNumber.is(4);
-				verify(targeted).squared.is(16);
+				var flattened = flatten(configured);
+				console("flattened");
+				var fs = flattened.asString(bar);
+				verify(fs).is("4");
+				var fn = flattened.asNumber(bar);
+				verify(fn).is(4);
+
+				var pinContext = pin({ scale: 2 });
+				var pinned = pinContext(implementation);
+				console("pinned");
+				verify(pinned).asString(bar).is("4");
+				verify(pinned).asNumber(bar).is(4);
+				verify(pinned).squared(bar).is(16);
+
+				var targeted = specify(bar)(flattened);
+				console("targeted");
+				verify(targeted).asString().is("4");
+				verify(targeted).asNumber().is(4);
+				verify(targeted).squared().is(16);
 			}
 		}
 	//@ts-ignore
