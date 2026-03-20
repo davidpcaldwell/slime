3ed73d308f741a58c4a3cdd95d54a0397ad43f4f
David P. Caldwell <david@davidpcaldwell.com>
2026-03-19 21:26:32 -0400

    Simplify servlet implementation

--- rhino/http/servlet/api.js
+++ rhino/http/servlet/api.js
@@ -95,6 +95,99 @@
 				} else if (isScript($host)) {
 					return $host.parameters;
 				}
+			};
+
+			/** @type { ($host: slime.servlet.internal.$host) => slime.servlet.internal.Loaders } */
+			var getLoaders = function(/** @type { slime.servlet.internal.$host } */$host) {
+				if (isJava($host)) {
+					/**
+					 * @type { (p: any, prefix?: any) => slime.old.Loader }
+					 */
+					var Loader = (function() {
+						var Loader = function(p,prefix) {
+							var getMimeType = function(path) {
+								return $host.getServletContext().getMimeType(path);
+							};
+							/** @type { slime.old.loader.Source } */
+							var source = {
+								get: function(path) {
+									var pp = {};
+									pp._source = (prefix) ? p._source.child(prefix) : p._source;
+									pp.type = function(path) {
+										var _type = getMimeType(path);
+										if (/\.css$/.test(path)) {
+											_type = new Packages.java.lang.String("text/css");
+										}
+										if (_type) return api.io.mime.Type.parse(String(_type));
+										return null;
+									};
+
+									var delegate = new api.io.Loader(pp);
+									var delegated = delegate.source.get(path);
+									if (!delegated) return null;
+									return api.$api.Object.compose(delegated, {
+										type: pp.type(path)
+									});
+								},
+								list: function(path) {
+									var full = prefix + path;
+									return api.loader.paths(full).map(function(string) {
+										if (string.substring(string.length-1) == "/") {
+											return {
+												path: string.substring(0,string.length-1),
+												loader: true,
+												resource: false
+											}
+										} else {
+											return {
+												path: string,
+												loader: false,
+												resource: true
+											}
+										}
+									});
+								}
+							}
+							// var rv = new bootstrap.io.Loader(pp);
+							var rv = new api.io.Loader(source);
+							//	TODO	Below failed TypeScript and didn't seem to make sense; remove when safe
+							// rv.list = function(m) {
+							// 	var path = prefix + m.path;
+							// 	var rv = bootstrap.loader.paths(path);
+							// 	return rv;
+							// }
+							return rv;
+						}
+
+						return Loader;
+					})();
+
+					//	servlet container, determine webapp path and load relative to that
+					var prefix = (
+						function() {
+							var path = String($servlet.path);
+							var tokens = path.split("/");
+							var prefix = tokens.slice(0,tokens.length-1).join("/") + "/";
+							return prefix;
+						}
+					)();
+					var loader = Loader({
+						_source: $servlet.resources
+					});
+					return {
+						script: Loader({
+							_source: $servlet.resources
+						},prefix),
+						container: Loader({
+							_source: $servlet.resources
+						},""),
+						api: loader.Child("WEB-INF/slime/rhino/http/servlet/server/")
+					};
+				} else if (isScript($host)) {
+					return $host.loaders;
+				} else {
+					throw new Error("Cannot instantiate loaders: $slime = " + $slime + " $servlet = " + $servlet + " $host = " + $host);
+				}
 			}
 
 			/**
@@ -184,97 +277,7 @@
 				}
 			})();
 
-			var loaders = (
-				/**
-				 * @return { slime.servlet.internal.Loaders }
-				 */
-				function() {
-					if (isJava($host)) {
-						/**
-						 * @type { (p: any, prefix?: any) => slime.old.Loader }
-						 */
-						var Loader = (function() {
-							var Loader = function(p,prefix) {
-								var getMimeType = function(path) {
-									return $host.getServletContext().getMimeType(path);
-								};
-								/** @type { slime.old.loader.Source } */
-								var source = {
-									get: function(path) {
-										var pp = {};
-										pp._source = (prefix) ? p._source.child(prefix) : p._source;
-										pp.type = function(path) {
-											var _type = getMimeType(path);
-											if (/\.css$/.test(path)) {
-												_type = new Packages.java.lang.String("text/css");
-											}
-											if (_type) return api.io.mime.Type.parse(String(_type));
-											return null;
-										};
-
-										var delegate = new api.io.Loader(pp);
-										var delegated = delegate.source.get(path);
-										if (!delegated) return null;
-										return api.$api.Object.compose(delegated, {
-											type: pp.type(path)
-										});
-									},
-									list: function(path) {
-										var full = prefix + path;
-										return api.loader.paths(full).map(function(string) {
-											if (string.substring(string.length-1) == "/") {
-												return {
-													path: string.substring(0,string.length-1),
-													loader: true,
-													resource: false
-												}
-											} else {
-												return {
-													path: string,
-													loader: false,
-													resource: true
-												}
-											}
-										});
-									}
-								}
-								// var rv = new bootstrap.io.Loader(pp);
-								var rv = new api.io.Loader(source);
-								//	TODO	Below failed TypeScript and didn't seem to make sense; remove when safe
-								// rv.list = function(m) {
-								// 	var path = prefix + m.path;
-								// 	var rv = bootstrap.loader.paths(path);
-								// 	return rv;
-								// }
-								return rv;
-							}
-
-							return Loader;
-						})();
-
-						//	servlet container, determine webapp path and load relative to that
-						var path = String($servlet.path);
-						var tokens = path.split("/");
-						var prefix = tokens.slice(0,tokens.length-1).join("/") + "/";
-						var loader = Loader({
-							_source: $servlet.resources
-						});
-						return {
-							script: Loader({
-								_source: $servlet.resources
-							},prefix),
-							container: Loader({
-								_source: $servlet.resources
-							},""),
-							api: loader.Child("WEB-INF/slime/rhino/http/servlet/server/")
-						};
-					} else if (isScript($host)) {
-						return $host.loaders;
-					} else {
-						throw new Error("Cannot instantiate loaders: $slime = " + $slime + " $servlet = " + $servlet + " $host = " + $host);
-					}
-				}
-			)();
+			var loaders = getLoaders($host);
 
 			var context = (
 				/**
