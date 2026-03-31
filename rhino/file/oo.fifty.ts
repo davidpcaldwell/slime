723b57e5400020d323a996386c21379eebc6df3c
David P. Caldwell <david@davidpcaldwell.com>
2026-03-30 21:33:28 -0400

    Reduce surface area of older code loading constructs

--- rhino/file/oo.fifty.ts
+++ rhino/file/oo.fifty.ts
@@ -337,5 +337,5 @@ namespace slime.jrunscript.file.internal.oo {
 	//@ts-ignore
 	)(fifty);
 
-	export type Script = slime.loader.Script<Context,Exports>
+	export type Script = slime.runtime.loader.Module<Context,Exports>
 }
--- rhino/file/wo-filesystem.fifty.ts
+++ rhino/file/wo-filesystem.fifty.ts
@@ -228,5 +228,5 @@ namespace slime.jrunscript.file.internal.wo.filesystem {
 	//@ts-ignore
 	)(fifty);
 
-	export type Script = slime.loader.Script<Context,Exports>
+	export type Script = slime.runtime.loader.Module<Context,Exports>
 }
