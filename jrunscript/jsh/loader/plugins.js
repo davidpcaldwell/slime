bf33afe09c41fcb4dddbc00246ee8365cb45ff22
David P. Caldwell <david@davidpcaldwell.com>
2026-04-25 08:01:47 -0400

    #2329: Copilot review feedback

--- jrunscript/jsh/loader/plugins.js
+++ jrunscript/jsh/loader/plugins.js
@@ -329,7 +329,8 @@
 					new $slime.Loader({ _file: p._file })
 				);
 			} else if (isSynchronousLoaderPlugins(p)) {
-				$slime.$api.loader.old.old.loader.source.object
+				//	Copilot suggested removing next line as a no-op, which seems right, but what was it for?
+				//$slime.$api.loader.old.old.loader.source.object
 				content = getPluginsContent(
 					scope,
 					$slime.$api.loader.old.old.loader.from.synchronous(p.synchronous)
